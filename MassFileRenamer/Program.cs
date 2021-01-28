c24582c1a01c34ec537839f1554d4090cf014352
Brian Hart <brian.hart@xylogix.net>
2021-01-28 07:18:17 -0500

    Print count of files

--- MassFileRenamer/Program.cs
+++ MassFileRenamer/Program.cs
@@ -11,12 +11,27 @@ namespace MassFileRenamer
          {
             const string rootDir = @"C:\Users\Administrator\source\repos\astrohart\PortfolioMonitor";
 
-            new FileRenamer(rootDir).ProcessAll("PortfolioMonitor.Loaders.Config", "PortfolioMonitor.Serializers.Config");
+
+            var renamer = new FileRenamer(rootDir);
+
+            renamer.FilesCounted += OnFileRenamerCountedFiles;
+
+            renamer.ProcessAll("PortfolioMonitor.Loaders.Config", "PortfolioMonitor.Serializers.Config");
             Console.ReadKey();
          }
          catch (Exception e)
          {
             Console.WriteLine(e);
+         }
+      }
+
+      private static void OnFileRenamerCountedFiles(object sender, FilesCountedEventArgs e)
+      {
+         switch (e.OperationType)
+         {
+            case OperationType.ReplaceTextInFiles:
+               Console.WriteLine($"Replace Text in Files: {e.Count} files to be processed.");
+               break;
          }
       }
    }
