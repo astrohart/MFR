e6966eeac37cf6953fd78854ae889000f90b32ae
Brian Hart <brian.hart@xylogix.net>
2021-01-28 07:15:32 -0500

    Pull event up

--- MassFileRenamer.Objects/IFileRenamer.cs
+++ MassFileRenamer.Objects/IFileRenamer.cs
@@ -4,17 +4,25 @@
    {
       string RootDirectoryPath { get; }
 
-      void ProcessAll(string findWhat, string replaceWith);
-
-      void RenameFilesInFolder(string rootFolderPath, string findWhat, string replaceWith);
-
-      void RenameSubFoldersOf(string rootFolderPath, string findWhat, string replaceWith);
-
-      void ReplaceTextInFiles(string rootFolderPath, string findWhat, string replaceWith);
-
       /// <summary>
       /// Occurs when files to be processed have been counted.
       /// </summary>
       event FilesCountedEventHandler FilesCounted;
+
+      /// <summary>
+      /// Occurs when an operation is about to be processed for a file or a folder.
+      /// </summary>
+      event ProcessingOperationEventHandler ProcessingOperation;
+
+      void ProcessAll(string findWhat, string replaceWith);
+
+      void RenameFilesInFolder(string rootFolderPath, string findWhat,
+         string replaceWith);
+
+      void RenameSubFoldersOf(string rootFolderPath, string findWhat,
+         string replaceWith);
+
+      void ReplaceTextInFiles(string rootFolderPath, string findWhat,
+         string replaceWith);
    }
 }
