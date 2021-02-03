e3d418f9db7dd2f09dfa060f33b4031c42a822b1
Brian Hart <brian.hart@xylogix.net>
2021-02-03 17:41:54 -0500

    Add history lists

--- MassFileRenamer.GUI/Configuration.cs
+++ MassFileRenamer.GUI/Configuration.cs
@@ -1,22 +1,29 @@
 ﻿using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace MassFileRenamer.GUI
 {
    public class Configuration : IConfiguration
    {
+      [JsonProperty("findWhat")] public string FindWhat { get; set; }
+
+      [JsonProperty("findWhatHistory")]
+      public List<string> FindWhatHistory { get; set; } = new List<string>();
+
+      [JsonProperty("matchCase")] public bool MatchCase { get; set; }
+
+      [JsonProperty("matchWholeWord")] public bool MatchWholeWord { get; set; }
+
+      [JsonProperty("replaceWith")] public string ReplaceWith { get; set; }
+
+      [JsonProperty("replaceWithHistory")]
+      public List<string> ReplaceWithHistory { get; set; } = new List<string>();
+
       [JsonProperty("startingFolder")]
       public string StartingFolder { get; set; }
 
-      [JsonProperty("findWhat")]
-      public string FindWhat { get; set; }
-
-      [JsonProperty("replaceWith")]
-      public string ReplaceWith { get; set; }
-
-      [JsonProperty("matchCase")]
-      public bool MatchCase { get; set; }
-
-      [JsonProperty("matchWholeWord")]
-      public bool MatchWholeWord { get; set; }
+      [JsonProperty("startingFolderHistory")]
+      public List<string> StartingFolderHistory { get; set; } =
+         new List<string>();
    }
 }
--- MassFileRenamer.GUI/IConfiguration.cs
+++ MassFileRenamer.GUI/IConfiguration.cs
@@ -1,11 +1,16 @@
-﻿namespace MassFileRenamer.GUI
+﻿using System.Collections.Generic;
+
+namespace MassFileRenamer.GUI
 {
    public interface IConfiguration
    {
       string FindWhat { get; set; }
+      List<string> FindWhatHistory { get; set; }
       bool MatchCase { get; set; }
       bool MatchWholeWord { get; set; }
       string ReplaceWith { get; set; }
+      List<string> ReplaceWithHistory { get; set; }
       string StartingFolder { get; set; }
+      List<string> StartingFolderHistory { get; set; }
    }
 }
