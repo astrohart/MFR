47d4a97445a2878894165b4e3eee8c4be57237bf
Brian Hart <brian.hart@xylogix.net>
2021-02-03 10:09:14 -0500

    Add controls for 'find what' and; 'replace with'

--- MassFileRenamer.GUI/MainWindow.Designer.cs
+++ MassFileRenamer.GUI/MainWindow.Designer.cs
@@ -33,6 +33,10 @@ namespace MassFileRenamer.GUI
          this.startingFolderTextBox = new System.Windows.Forms.TextBox();
          this.startingFolderLabel = new System.Windows.Forms.Label();
          this.startingFolderBrowseButton = new System.Windows.Forms.Button();
+         this.findWhatLabel = new System.Windows.Forms.Label();
+         this.findWhatTextBox = new System.Windows.Forms.TextBox();
+         this.replaceWithLabel = new System.Windows.Forms.Label();
+         this.replaceWithTextBox = new System.Windows.Forms.TextBox();
          this.SuspendLayout();
          // 
          // startingFolderTextBox
@@ -60,11 +64,47 @@ namespace MassFileRenamer.GUI
          this.startingFolderBrowseButton.Text = "...";
          this.startingFolderBrowseButton.UseVisualStyleBackColor = true;
          // 
+         // findWhatLabel
+         // 
+         this.findWhatLabel.AutoSize = true;
+         this.findWhatLabel.Location = new System.Drawing.Point(9, 65);
+         this.findWhatLabel.Name = "findWhatLabel";
+         this.findWhatLabel.Size = new System.Drawing.Size(109, 15);
+         this.findWhatLabel.TabIndex = 3;
+         this.findWhatLabel.Text = "&Text to be replaced:";
+         // 
+         // findWhatTextBox
+         // 
+         this.findWhatTextBox.Location = new System.Drawing.Point(9, 84);
+         this.findWhatTextBox.Name = "findWhatTextBox";
+         this.findWhatTextBox.Size = new System.Drawing.Size(542, 23);
+         this.findWhatTextBox.TabIndex = 4;
+         // 
+         // replaceWithLabel
+         // 
+         this.replaceWithLabel.AutoSize = true;
+         this.replaceWithLabel.Location = new System.Drawing.Point(9, 119);
+         this.replaceWithLabel.Name = "replaceWithLabel";
+         this.replaceWithLabel.Size = new System.Drawing.Size(64, 15);
+         this.replaceWithLabel.TabIndex = 5;
+         this.replaceWithLabel.Text = "&With what:";
+         // 
+         // replaceWithTextBox
+         // 
+         this.replaceWithTextBox.Location = new System.Drawing.Point(9, 138);
+         this.replaceWithTextBox.Name = "replaceWithTextBox";
+         this.replaceWithTextBox.Size = new System.Drawing.Size(542, 23);
+         this.replaceWithTextBox.TabIndex = 6;
+         // 
          // MainWindow
          // 
          this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
          this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
          this.ClientSize = new System.Drawing.Size(596, 230);
+         this.Controls.Add(this.replaceWithLabel);
+         this.Controls.Add(this.replaceWithTextBox);
+         this.Controls.Add(this.findWhatLabel);
+         this.Controls.Add(this.findWhatTextBox);
          this.Controls.Add(this.startingFolderBrowseButton);
          this.Controls.Add(this.startingFolderLabel);
          this.Controls.Add(this.startingFolderTextBox);
@@ -85,6 +125,10 @@ namespace MassFileRenamer.GUI
       private System.Windows.Forms.TextBox startingFolderTextBox;
       private System.Windows.Forms.Label startingFolderLabel;
       private System.Windows.Forms.Button startingFolderBrowseButton;
+      private System.Windows.Forms.Label findWhatLabel;
+      private System.Windows.Forms.TextBox findWhatTextBox;
+      private System.Windows.Forms.Label replaceWithLabel;
+      private System.Windows.Forms.TextBox replaceWithTextBox;
    }
 }
 
