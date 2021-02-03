e54045a82885ce64f3876f55b028600966dd8256
Brian Hart <brian.hart@xylogix.net>
2021-02-03 09:30:53 -0500

    Override OnLoad

--- MassFileRenamer.GUI/MainWindow.cs
+++ MassFileRenamer.GUI/MainWindow.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Windows.Forms;
 
 namespace MassFileRenamer.GUI
 {
@@ -7,6 +8,15 @@ namespace MassFileRenamer.GUI
       public MainWindow()
       {
          InitializeComponent();
+      }
+
+      /// <summary>Raises the <see cref="E:System.Windows.Forms.Form.Load" /> event.</summary>
+      /// <param name="e">An <see cref="T:System.EventArgs" /> that contains the event data.</param>
+      protected override void OnLoad(EventArgs e)
+      {
+         base.OnLoad(e);
+
+         Text = $"{Application.ProductName} {Application.ProductVersion}";
       }
    }
 }
