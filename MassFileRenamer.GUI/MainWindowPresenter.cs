7b2d46662155de1ca73d2f574e50b3621ce21955
Brian Hart <brian.hart@xylogix.net>
2021-02-03 10:27:44 -0500

    Add XML docs

--- MassFileRenamer.GUI/MainWindowPresenter.cs
+++ MassFileRenamer.GUI/MainWindowPresenter.cs
@@ -1,4 +1,6 @@
-﻿namespace MassFileRenamer.GUI
+﻿using System;
+
+namespace MassFileRenamer.GUI
 {
    /// <summary>
    /// Presenter for the main application window.
@@ -15,5 +17,26 @@
       /// This object provides the functionality of the main window of the application.
       /// </remarks>
       private IMainWindow _mainWindow;
+
+      /// <summary>
+      /// Constructs a new instance of
+      /// <see
+      ///    cref="T:MassFileRenamer.GUI.MainWindowPresenter" />
+      /// and returns a
+      /// reference to it.
+      /// </summary>
+      /// <param name="mainWindow">
+      /// (Required.) Reference to an instance of an object that implements the
+      /// <see cref="T:MassFileRenamer.GUI.IMainWindow" /> interface.
+      /// </param>
+      /// <exception cref="T:System.ArgumentNullException">
+      /// Thrown if the required <paramref name="mainWindow" /> parameter has a
+      /// <c>null</c> value.
+      /// </exception>
+      public MainWindowPresenter(IMainWindow mainWindow)
+      {
+         _mainWindow = mainWindow ??
+                       throw new ArgumentNullException(nameof(mainWindow));
+      }
    }
 }
