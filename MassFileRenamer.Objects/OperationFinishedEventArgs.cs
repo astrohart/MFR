6f6d2895fe9001e5cacc13a6d852e5a55d474a31
Brian Hart <brian.hart@xylogix.net>
2021-02-03 09:58:09 -0500

    Create OperationFinishedEventArgs

---
+++ MassFileRenamer.Objects/OperationFinishedEventArgs.cs
@@ -0,0 +1,24 @@
+﻿namespace MassFileRenamer.Objects
+{
+   /// <summary>
+   /// Contains the data for a OperationFinished event.
+   /// </summary>
+   public class OperationFinishedEventArgs : OperationEventArgs
+   {
+      /// <summary>
+      /// Constructs a new instance of
+      /// <see
+      ///    cref="T:MassFileRenamer.Objects.OperationEventArgs" />
+      /// and returns a
+      /// reference to it.
+      /// </summary>
+      /// <param name="type">
+      /// (Required.) One of the
+      /// <see
+      ///    cref="T:MassFileRenamer.Objects.OperationType" />
+      /// values that specifies
+      /// the operation being performed.
+      /// </param>
+      public OperationFinishedEventArgs(OperationType type) : base(type) { }
+   }
+}
