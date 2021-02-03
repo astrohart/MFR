65fc66d1edbec00fe676a51dfdc2b3e265728b90
Brian Hart <brian.hart@xylogix.net>
2021-02-03 11:13:14 -0500

    Pull member up

--- MassFileRenamer.GUI/IMainWindowPresenter.cs
+++ MassFileRenamer.GUI/IMainWindowPresenter.cs
@@ -4,5 +4,11 @@
    /// Defines the public-exposed methods and properties of the presenter object
    /// for the main application window..
    /// </summary>
-   public interface IMainWindowPresenter { }
+   public interface IMainWindowPresenter
+   {
+      /// <summary>
+      /// Begins the rename operation.
+      /// </summary>
+      void Process();
+   }
 }
