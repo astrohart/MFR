1a9f06e8cce40dabf5b944c1e2a769239f6ea5d9
Brian Hart <brian.hart@xylogix.net>
2021-02-03 17:28:15 -0500

    Add XML docs

--- MassFileRenamer.GUI/ConvertConfiguration.cs
+++ MassFileRenamer.GUI/ConvertConfiguration.cs
@@ -6,6 +6,10 @@ namespace MassFileRenamer.GUI
 {
    public static class ConvertConfiguration
    {
+      /// <summary>
+      /// Reference to a <see cref="T:Newtonsoft.Json.JsonSerializerSettings" />
+      /// that specifies how to serialize objects.
+      /// </summary>
       public static readonly JsonSerializerSettings Settings =
          new JsonSerializerSettings
          {
@@ -20,10 +24,47 @@ namespace MassFileRenamer.GUI
             }
          };
 
+      /// <summary>
+      /// Converts a string containing JSON-formatted text into an instance of
+      /// an object that implements the
+      /// <see
+      ///    cref="T:MassFileRenamer.GUI.IConfiguration" />
+      /// interface and returns a
+      /// reference to it.
+      /// </summary>
+      /// <param name="json">
+      /// (Required.) String containing the JSON to convert.
+      /// </param>
+      /// <returns>
+      /// reference to an instance of an object that implements the
+      /// <see
+      ///    cref="T:MassFileRenamer.GUI.IConfiguration" />
+      /// object whose properties
+      /// are initialized to the data values read in from the JSON text.
+      /// </returns>
       public static IConfiguration FromJson(string json)
          => JsonConvert.DeserializeObject<Configuration>(json, Settings);
 
+      /// <summary>
+      /// Converts an instance of an object that implements the
+      /// <see
+      ///    cref="T:MassFileRenamer.GUI.IConfiguration" />
+      /// interface into a
+      /// JSON-formatted string and returns the resultant string.
+      /// </summary>
+      /// <param name="configuration">
+      /// (Required.) Reference to an instance of an object that implements the
+      /// <see cref="T:MassFileRenamer.GUI.IConfiguration" /> interface.
+      /// </param>
+      /// <returns>
+      /// String containing the JSON equivalent of the
+      /// <paramref
+      ///    name="configuration" />
+      /// object fed in.
+      /// </returns>
       public static string ToJson(IConfiguration configuration)
-         => JsonConvert.SerializeObject(configuration as Configuration, Settings);
+         => JsonConvert.SerializeObject(
+            configuration as Configuration, Settings
+         );
    }
 }
