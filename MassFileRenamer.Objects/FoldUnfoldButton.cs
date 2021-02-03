77aea46b75c41067c2767aa0fa06ad3e9a114b36
Brian Hart <brian.hart@xylogix.net>
2021-02-03 16:55:44 -0500

    Create FoldUnfoldButton class

---
+++ MassFileRenamer.Objects/FoldUnfoldButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Forms;
+
+namespace MassFileRenamer.Objects
+{
+   /// <summary>
+   /// Button that shows "More &gt;&gt;" or "Less &lt;&lt;" depending on whether a
+   /// form is folded or unfolded.
+   /// </summary>
+   public class FoldUnfoldButton : Button
+   {
+      
+   }
+}
