d98995a82f994f82013e5b5857801a749fb59fed
Brian Hart <brian.hart@xylogix.net>
2021-02-13 16:27:06 -0500

    Add Cancel button to Prorgess Dialog

--- MassFileRenamer.GUI/ProgressDialog.Designer.cs
+++ MassFileRenamer.GUI/ProgressDialog.Designer.cs
@@ -29,68 +29,69 @@ namespace MassFileRenamer.GUI
       /// </summary>
       private void InitializeComponent()
       {
-         System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ProgressDialog));
-         this.detailedStatusLabel = new System.Windows.Forms.Label();
-         this.progressBar = new System.Windows.Forms.ProgressBar();
-         this.statusLabel = new System.Windows.Forms.Label();
-         this.cancelButton = new System.Windows.Forms.Button();
-         this.SuspendLayout();
-         // 
-         // detailedStatusLabel
-         // 
-         this.detailedStatusLabel.Location = new System.Drawing.Point(12, 46);
-         this.detailedStatusLabel.Name = "detailedStatusLabel";
-         this.detailedStatusLabel.Size = new System.Drawing.Size(392, 46);
-         this.detailedStatusLabel.TabIndex = 1;
-         // 
-         // progressBar
-         // 
-         this.progressBar.Location = new System.Drawing.Point(12, 100);
-         this.progressBar.MarqueeAnimationSpeed = 30;
-         this.progressBar.Name = "progressBar";
-         this.progressBar.Size = new System.Drawing.Size(300, 23);
-         this.progressBar.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
-         this.progressBar.TabIndex = 2;
-         // 
-         // statusLabel
-         // 
-         this.statusLabel.Location = new System.Drawing.Point(13, 14);
-         this.statusLabel.Name = "statusLabel";
-         this.statusLabel.Size = new System.Drawing.Size(391, 24);
-         this.statusLabel.TabIndex = 0;
-         // 
-         // cancelButton
-         // 
-         this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-         this.cancelButton.Location = new System.Drawing.Point(318, 99);
-         this.cancelButton.Name = "cancelButton";
-         this.cancelButton.Size = new System.Drawing.Size(87, 27);
-         this.cancelButton.TabIndex = 3;
-         this.cancelButton.Text = "Cancel";
-         this.cancelButton.UseVisualStyleBackColor = true;
-         // 
-         // ProgressDialog
-         // 
-         this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
-         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-         this.CancelButton = this.cancelButton;
-         this.ClientSize = new System.Drawing.Size(417, 138);
-         this.ControlBox = false;
-         this.Controls.Add(this.cancelButton);
-         this.Controls.Add(this.statusLabel);
-         this.Controls.Add(this.progressBar);
-         this.Controls.Add(this.detailedStatusLabel);
-         this.Font = new System.Drawing.Font("Segoe UI", 9F);
-         this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-         this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-         this.MaximizeBox = false;
-         this.MinimizeBox = false;
-         this.Name = "ProgressDialog";
-         this.ShowIcon = false;
-         this.ShowInTaskbar = false;
-         this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-         this.Text = "[AppTitle]";
-         this.ResumeLayout(false);
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ProgressDialog));
+            this.detailedStatusLabel = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // detailedStatusLabel
+            // 
+            this.detailedStatusLabel.Location = new System.Drawing.Point(12, 46);
+            this.detailedStatusLabel.Name = "detailedStatusLabel";
+            this.detailedStatusLabel.Size = new System.Drawing.Size(392, 46);
+            this.detailedStatusLabel.TabIndex = 1;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 100);
+            this.progressBar.MarqueeAnimationSpeed = 30;
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(300, 23);
+            this.progressBar.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.progressBar.TabIndex = 2;
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.Location = new System.Drawing.Point(13, 14);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(391, 24);
+            this.statusLabel.TabIndex = 0;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancelButton.Location = new System.Drawing.Point(318, 99);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(87, 27);
+            this.cancelButton.TabIndex = 3;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.OnCancel);
+            // 
+            // ProgressDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.CancelButton = this.cancelButton;
+            this.ClientSize = new System.Drawing.Size(417, 138);
+            this.ControlBox = false;
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.detailedStatusLabel);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ProgressDialog";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "[AppTitle]";
+            this.ResumeLayout(false);
 
       }
 
--- MassFileRenamer.GUI/ProgressDialog.cs
+++ MassFileRenamer.GUI/ProgressDialog.cs
@@ -5,129 +5,178 @@ using xyLOGIX.Core.Extensions;
 
 namespace MassFileRenamer.GUI
 {
-   /// <summary>
-   /// Window that displays progress of an operation.
-   /// </summary>
-   public partial class ProgressDialog : Form, IProgressDialog
-   {
-      /// <summary>
-      /// Constructs a new instance of
-      /// <see
-      ///    cref="T:MassFileRenamer.GUI.ProgressDialog" />
-      /// and returns a reference
-      /// to it.
-      /// </summary>
-      public ProgressDialog()
-      {
-         InitializeComponent();
-      }
+    /// <summary>
+    /// Window that displays progress of an operation.
+    /// </summary>
+    public partial class ProgressDialog : Form, IProgressDialog
+    {
+        /// <summary>
+        /// Constructs a new instance of
+        /// <see
+        ///     cref="T:MassFileRenamer.GUI.ProgressDialog" />
+        /// and returns a
+        /// reference to it.
+        /// </summary>
+        public ProgressDialog()
+        {
+            InitializeComponent();
+        }
 
-      /// <summary>
-      /// Gets or sets the maximum value of the range of the progress bar control.
-      /// </summary>
-      public int ProgressBarMaximum
-      {
-         [DebuggerStepThrough] get => progressBar.Maximum;
-         set => progressBar.InvokeIfRequired(() => progressBar.Maximum = value);
-      }
+        /// <summary>
+        /// Raised when the user clicks the Cancel button in order to request
+        /// that the operation be aborted.
+        /// </summary>
+        public event EventHandler CancelRequested;
 
-      /// <summary>
-      /// Get or sets the minimum value of the range of the progress bar control.
-      /// </summary>
-      public int ProgressBarMinimum
-      {
-         [DebuggerStepThrough] get => progressBar.Minimum;
-         set => progressBar.InvokeIfRequired(() => progressBar.Minimum = value);
-      }
+        /// <summary>
+        /// Gets or sets a string containing the new detailed status.
+        /// </summary>
+        public string DetailedStatus
+        {
+            [DebuggerStepThrough] get => detailedStatusLabel.Text;
+            set {
+                detailedStatusLabel.InvokeIfRequired(
+                    () => detailedStatusLabel.Text = value
+                );
+            }
+        }
 
-      /// <summary>
-      /// Gets or sets the manner in which progress should
-      /// </summary>
-      public ProgressBarStyle ProgressBarStyle
-      {
-         [DebuggerStepThrough] get => progressBar.Style;
-         set => progressBar.InvokeIfRequired(() => progressBar.Style = value);
-      }
+        /// <summary>
+        /// Gets a reference to the progress bar control.
+        /// </summary>
+        public ProgressBar ProgressBar
+        {
+            [DebuggerStepThrough] get => progressBar;
+        }
 
-      /// <summary>
-      /// Gets or sets the current position of the progress bar.
-      /// </summary>
-      public int ProgressBarValue
-      {
-         [DebuggerStepThrough] get => progressBar.Value;
-         set => progressBar.InvokeIfRequired(() => progressBar.Value = value);
-      }
+        /// <summary>
+        /// Gets or sets the maximum value of the range of the progress bar control.
+        /// </summary>
+        public int ProgressBarMaximum
+        {
+            [DebuggerStepThrough] get => progressBar.Maximum;
+            set
+                => progressBar.InvokeIfRequired(
+                    () => progressBar.Maximum = value
+                );
+        }
 
-      /// <summary>
-      /// Gets or sets a string containing the new detailed status.
-      /// </summary>
-      public string DetailedStatus
-      {
-         [DebuggerStepThrough] get => detailedStatusLabel.Text;
-         set
-         {
-            detailedStatusLabel.InvokeIfRequired(
-               () => detailedStatusLabel.Text = value
-            );
-         }
-      }
+        /// <summary>
+        /// Get or sets the minimum value of the range of the progress bar control.
+        /// </summary>
+        public int ProgressBarMinimum
+        {
+            [DebuggerStepThrough] get => progressBar.Minimum;
+            set
+                => progressBar.InvokeIfRequired(
+                    () => progressBar.Minimum = value
+                );
+        }
 
-      /// <summary>
-      /// Gets a reference to the progress bar control.
-      /// </summary>
-      public ProgressBar ProgressBar
-      {
-         [DebuggerStepThrough] get => progressBar;
-      }
+        /// <summary>
+        /// Gets or sets the manner in which progress should
+        /// </summary>
+        public ProgressBarStyle ProgressBarStyle
+        {
+            [DebuggerStepThrough] get => progressBar.Style;
+            set
+                => progressBar.InvokeIfRequired(() => progressBar.Style = value)
+            ;
+        }
 
-      /// <summary>
-      /// Gets or sets a string containing the new status.
-      /// </summary>
-      public string Status
-      {
-         [DebuggerStepThrough] get => statusLabel.Text;
-         set { statusLabel.InvokeIfRequired(() => statusLabel.Text = value); }
-      }
+        /// <summary>
+        /// Gets or sets the current position of the progress bar.
+        /// </summary>
+        public int ProgressBarValue
+        {
+            [DebuggerStepThrough] get => progressBar.Value;
+            set
+                => progressBar.InvokeIfRequired(() => progressBar.Value = value)
+            ;
+        }
 
-      /// <summary>
-      /// Clears the status text and progress bar.
-      /// </summary>
-      public void Reset()
-      {
-         ProgressBarStyle = ProgressBarStyle.Continuous;
-         ProgressBarMinimum = 0;
-         ProgressBarMaximum = 100;
-         ProgressBarValue = 0;
+        /// <summary>
+        /// Gets or sets a string containing the new status.
+        /// </summary>
+        public string Status
+        {
+            [DebuggerStepThrough] get => statusLabel.Text;
+            set {
+                statusLabel.InvokeIfRequired(() => statusLabel.Text = value);
+            }
+        }
 
-         Status = DetailedStatus = string.Empty;
+        /// <summary>
+        /// Clears the status text and progress bar.
+        /// </summary>
+        public void Reset()
+        {
+            ProgressBarStyle = ProgressBarStyle.Continuous;
+            ProgressBarMinimum = 0;
+            ProgressBarMaximum = 100;
+            ProgressBarValue = 0;
 
-         this.InvokeIfRequired(RedrawDisplay);
-      }
+            Status = DetailedStatus = string.Empty;
 
-      private void RedrawDisplay()
-      {
-         Update();
-         Refresh();
-         Application.DoEvents();
-      }
+            this.InvokeIfRequired(RedrawDisplay);
+        }
 
-      /// <summary>
-      /// Raises the <see cref="E:System.Windows.Forms.Form.Load" /> event.
-      /// </summary>
-      /// <param name="e">
-      /// An <see cref="T:System.EventArgs" /> that contains the event data.
-      /// </param>
-      /// <remarks>
-      /// This method sets the title of the dialog to match the application's name.
-      /// </remarks>
-      protected override void OnLoad(EventArgs e)
-      {
-         base.OnLoad(e);
+        /// <summary>
+        /// Raises the
+        /// <see
+        ///     cref="E:MassFileRenamer.GUI.ProgressDialog.CancelRequested" />
+        /// event.
+        /// </summary>
+        protected virtual void OnCancelRequested()
+            => CancelRequested?.Invoke(this, EventArgs.Empty);
 
-         Status = "Calculating files to be processed...";
-         ProgressBar.MarqueeAnimationSpeed = 30;
-         ProgressBar.Style = ProgressBarStyle.Marquee;
-         Text = Application.ProductName;
-      }
-   }
+        /// <summary>
+        /// Raises the <see cref="E:System.Windows.Forms.Form.Load" /> event.
+        /// </summary>
+        /// <param name="e">
+        /// An <see cref="T:System.EventArgs" /> that contains the event data.
+        /// </param>
+        /// <remarks>
+        /// This method sets the title of the dialog to match the application's name.
+        /// </remarks>
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            Status = "Calculating files to be processed...";
+            ProgressBar.MarqueeAnimationSpeed = 30;
+            ProgressBar.Style = ProgressBarStyle.Marquee;
+            Text = Application.ProductName;
+        }
+
+        /// <summary>
+        /// Handles the <see cref="E:System.Windows.Forms.Control.Click" /> event
+        /// for the Cancel button.
+        /// </summary>
+        /// <param name="sender">
+        /// Reference to an instance of the object that raised the event.
+        /// </param>
+        /// <param name="e">
+        /// An <see cref="T:System.EventArgs" /> that contains the event data.
+        /// </param>
+        /// <remarks>
+        /// This method is called when the user chooses the Cancel button or
+        /// presses the ESCAPE key. This method responds merely by raising the
+        /// <see cref="E:MassFileRenamer.GUI.ProgressDialog.CancelRequested" />
+        /// event. This event directs the caller of this dialog box to attempt
+        /// to abort the operation and close down this dialog box from its GUI thread.
+        /// </remarks>
+        private void OnCancel(object sender, EventArgs e)
+            => OnCancelRequested();
+
+        /// <summary>
+        /// Directs the application to update the user's display.
+        /// </summary>
+        private void RedrawDisplay()
+        {
+            Update();
+            Refresh();
+            Application.DoEvents();
+        }
+    }
 }
