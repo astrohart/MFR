bd6004a1b57a108b277a891a151f4b46464261f5
Brian Hart <brian.hart@xylogix.net>
2021-02-13 14:37:19 -0500

    Code cleanup

--- MassFileRenamer.GUI/IMainWindowPresenter.cs
+++ MassFileRenamer.GUI/IMainWindowPresenter.cs
@@ -2,66 +2,69 @@
 
 namespace MassFileRenamer.GUI
 {
-   /// <summary>
-   /// Defines the public-exposed methods and properties of the presenter object
-   /// for the main application window..
-   /// </summary>
-   public interface IMainWindowPresenter
-   {
-      /// <summary>
-      /// Gets or sets a reference to an object that implements the
-      /// <see
-      ///    cref="T:MassFileRenamer.GUI.IConfiguration" />
-      /// interface that contains
-      /// the configuration details.
-      /// </summary>
-      IConfiguration Configuration { get; }
+    /// <summary>
+    /// Defines the public-exposed methods and properties of the presenter
+    /// object for the main application window..
+    /// </summary>
+    public interface IMainWindowPresenter
+    {
+        /// <summary>
+        /// Gets or sets a reference to an object that implements the
+        /// <see
+        ///     cref="T:MassFileRenamer.GUI.IConfiguration" />
+        /// interface that
+        /// contains the configuration details.
+        /// </summary>
+        IConfiguration Configuration
+        {
+            get;
+        }
 
-      /// <summary>
-      /// Occurs when the processing is done.
-      /// </summary>
-      event EventHandler Finished;
+        /// <summary>
+        /// Occurs when the configuration has been updated, say, by an import process.
+        /// </summary>
+        event EventHandler ConfigurationImported;
 
-      /// <summary>
-      /// Occurs when the processing has started.
-      /// </summary>
-      event EventHandler Started;
+        /// <summary>
+        /// Occurs when the processing is done.
+        /// </summary>
+        event EventHandler Finished;
 
-      /// <summary>
-      /// Dismisses the progress dialog.
-      /// </summary>
-      void CloseProgressDialog();
+        /// <summary>
+        /// Occurs when the processing has started.
+        /// </summary>
+        event EventHandler Started;
 
-      /// <summary>
-      /// Begins the rename operation.
-      /// </summary>
-      void Process();
+        /// <summary>
+        /// Dismisses the progress dialog.
+        /// </summary>
+        void CloseProgressDialog();
 
-      /// <summary>
-      /// Saves the configuration to disk.
-      /// </summary>
-      void SaveConfiguration();
+        /// <summary>
+        /// Imports configuration from a JSON file located on the disk.
+        /// </summary>
+        /// <param name="pathname">
+        /// (Required.) String containing the path to the file.
+        /// </param>
+        /// <remarks>
+        /// This method does nothing if the <paramref name="pathname" />
+        /// parameter is blank or the file does not exist.
+        /// </remarks>
+        void ImportConfiguration(string pathname);
 
-      /// <summary>
-      /// Shows the progress window.
-      /// </summary>
-      void ShowProgressDialog();
+        /// <summary>
+        /// Begins the rename operation.
+        /// </summary>
+        void Process();
 
-      /// <summary>
-      /// Occurs when the configuration has been updated, say, by an import process.
-      /// </summary>
-      event EventHandler ConfigurationImported;
+        /// <summary>
+        /// Saves the configuration to disk.
+        /// </summary>
+        void SaveConfiguration();
 
-      /// <summary>
-      /// Imports configuration from a JSON file located on the disk.
-      /// </summary>
-      /// <param name="pathname">
-      /// (Required.) String containing the path to the file.
-      /// </param>
-      /// <remarks>
-      /// This method does nothing if the <paramref name="pathname" />
-      /// parameter is blank or the file does not exist.
-      /// </remarks>
-      void ImportConfiguration(string pathname);
-   }
+        /// <summary>
+        /// Shows the progress window.
+        /// </summary>
+        void ShowProgressDialog();
+    }
 }
