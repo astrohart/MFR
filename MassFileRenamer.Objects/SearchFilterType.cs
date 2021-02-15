3f14b57dce5cff51cff8b1aac89ecd2cae65b71b
Brian Hart <brian.hart@xylogix.net>
2021-02-15 15:46:50 -0500

    Beginning code of search filtering mechanism

---
+++ MassFileRenamer.Objects/SearchFilterType.cs
@@ -0,0 +1,10 @@
+﻿namespace MassFileRenamer.Objects
+{
+    /// <summary>
+    /// Describes the kind of search filter we have.
+    /// </summary>
+    public enum SearchFilterType
+    {
+        Folder, File, Glob
+    }
+}
