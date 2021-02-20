bc38ef75450a971f67ba1ded59e57fc07f37109d
Brian Hart <brian.hart@xylogix.net>
2021-02-20 14:31:49 -0500

    Fix project file corruption

--- MassFileRenamer/Program.cs
+++ MassFileRenamer/Program.cs
@@ -1,94 +1,94 @@
 ﻿using MassFileRenamer.Objects;
 using System;
 
-namespace MassFileRenamer
+namespace MassFileRenamer.Console
 {
-   /// <summary>
-   /// Provides the application-level functionality.
-   /// </summary>
-   public static class Program
-   {
-       /// <summary>
-      /// Application entry point.
-      /// </summary>
-      public static void Main()
-      {
-         try
-         {
-            const string rootDir =
-               @"C:\Users\Administrator\source\repos\astrohart\PortfolioMonitor";
+    /// <summary>
+    /// Provides the application-level functionality.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// Application entry point.
+        /// </summary>
+        public static void Main()
+        {
+            try
+            {
+                const string rootDir =
+                   @"C:\Users\Administrator\source\repos\astrohart\PortfolioMonitor";
 
-            var renamer = new FileRenamer(rootDir);
+                var renamer = new FileRenamer(rootDir);
 
-            renamer.FilesToHaveTextReplacedCounted +=
-               OnFilesToHaveTextReplacedCounted;
-            renamer.ProcessingOperation += OnFileRenamerProcessingOperation;
-            renamer.StatusUpdate += OnFileRenamerStatusUpdated;
+                renamer.FilesToHaveTextReplacedCounted +=
+                   OnFilesToHaveTextReplacedCounted;
+                renamer.ProcessingOperation += OnFileRenamerProcessingOperation;
+                renamer.StatusUpdate += OnFileRenamerStatusUpdated;
 
-            renamer.ProcessAll(
-               @"MassFileRenamer",
-               @"PortfolioMonitor.Products"
-            );
-            Console.ReadKey();
-         }
-         catch (Exception e)
-         {
-            Console.WriteLine(e);
-         }
-      }
+                renamer.ProcessAll(
+                   @"MassFileRenamer",
+                   @"PortfolioMonitor.Products"
+                );
+                System.Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                System.Console.WriteLine(e);
+            }
+        }
 
-      private static void OnFileRenamerProcessingOperation(object sender,
-         ProcessingOperationEventArgs e)
-      {
-         switch (e.OperationType)
-         {
+        private static void OnFileRenamerProcessingOperation(object sender,
+           ProcessingOperationEventArgs e)
+        {
+            switch (e.OperationType)
+            {
             case OperationType.ReplaceTextInFiles:
-               Console.WriteLine(
-                  $"Replace Text in Files: Processing '{e.Pathname}'..."
-               );
-               break;
+                System.Console.WriteLine(
+                   $"Replace Text in Files: Processing '{e.Pathname}'..."
+                );
+                break;
 
             case OperationType.RenameFilesInFolder:
-               Console.WriteLine($"Rename Files: Processing '{e.Pathname}'...");
-               break;
+                System.Console.WriteLine($"Rename Files: Processing '{e.Pathname}'...");
+                break;
 
             case OperationType.RenameSubFolders:
-               Console.WriteLine(
-                  $"Rename Subfolders: Processing '{e.Pathname}'..."
-               );
-               break;
-         }
-      }
+                System.Console.WriteLine(
+                   $"Rename Subfolders: Processing '{e.Pathname}'..."
+                );
+                break;
+            }
+        }
 
-      private static void OnFileRenamerStatusUpdated(object sender,
-         StatusUpdateEventArgs e)
-      {
-         Console.WriteLine(e.Text);
-      }
+        private static void OnFileRenamerStatusUpdated(object sender,
+           StatusUpdateEventArgs e)
+        {
+            System.Console.WriteLine(e.Text);
+        }
 
-      private static void OnFilesToHaveTextReplacedCounted(object sender,
-         FilesOrFoldersCountedEventArgs e)
-      {
-         switch (e.OperationType)
-         {
+        private static void OnFilesToHaveTextReplacedCounted(object sender,
+           FilesOrFoldersCountedEventArgs e)
+        {
+            switch (e.OperationType)
+            {
             case OperationType.ReplaceTextInFiles:
-               Console.WriteLine(
-                  $"Replace Text in Files: {e.Count} files are to be processed."
-               );
-               break;
+                System.Console.WriteLine(
+                   $"Replace Text in Files: {e.Count} files are to be processed."
+                );
+                break;
 
             case OperationType.RenameFilesInFolder:
-               Console.WriteLine(
-                  $"Rename Files: {e.Count} files are to be processed."
-               );
-               break;
+                System.Console.WriteLine(
+                   $"Rename Files: {e.Count} files are to be processed."
+                );
+                break;
 
             case OperationType.RenameSubFolders:
-               Console.WriteLine(
-                  $"Rename Subfolders: {e.Count} folders are to be processed."
-               );
-               break;
-         }
-      }
-   }
+                System.Console.WriteLine(
+                   $"Rename Subfolders: {e.Count} folders are to be processed."
+                );
+                break;
+            }
+        }
+    }
 }
