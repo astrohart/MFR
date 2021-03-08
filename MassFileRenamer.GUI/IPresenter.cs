28d9a99963664aabca988495e76d008915680cb6
Brian Hart <brian.hart@xylogix.net>
2021-03-08 09:12:06 -0500

    Create IPresenter interface
    Defines those interfaces, methods, and properties that are common to the presenters of all the windows in the application.

---
+++ MassFileRenamer.GUI/IPresenter.cs
@@ -0,0 +1,36 @@
+﻿namespace MassFileRenamer.GUI
+{
+    /// <summary>
+    /// Defines the publicly-exposed methods and properties of any object that
+    /// has functionality common to all presenters.
+    /// </summary>
+    public interface IPresenter
+    {
+        /// <summary>
+        /// Dismisses the progress dialog.
+        /// </summary>
+        void CloseProgressDialog();
+
+        /// <summary>
+        /// Shows the progress window.
+        /// </summary>
+        void ShowProgressDialog();
+
+        /// <summary>
+        /// Updates data. Moves data from the screen to the model (
+        /// <paramref
+        ///     name="bSavingAndValidating" />
+        /// equals <c>true</c>) or from the model
+        /// to the screen ( <paramref name="bSavingAndValidating" /> equals <c>false</c>).
+        /// </summary>
+        /// <param name="bSavingAndValidating">
+        /// Set to <c>true</c> to move data from the screen to the model;
+        /// <c>false</c> to move data from the model to the screen.
+        /// </param>
+        /// <remarks>
+        /// Note that whatever operations this method performs may potentially
+        /// throw exceptions.
+        /// </remarks>
+        void UpdateData(bool bSavingAndValidating = true);
+    }
+}
