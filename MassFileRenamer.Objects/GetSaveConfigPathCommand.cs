2a4d4a714c53aa22e6b4a7518e00cdccc6cc5986
Brian Hart <brian.hart@xylogix.net>
2021-03-11 08:34:13 -0500

    Cleanup code

--- MassFileRenamer.Objects/GetSaveConfigPathCommand.cs
+++ MassFileRenamer.Objects/GetSaveConfigPathCommand.cs
@@ -1,24 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MassFileRenamer.Objects
+﻿namespace MassFileRenamer.Objects
 {
+    /// <summary>
+    /// Gets a reference to an instance of an object that implements the
+    /// <see
+    ///     cref="T:MassFileRenamer.Objects.ICommand" />
+    /// interface and provides
+    /// access to the operation of saving the path of a configuration file to
+    /// the system Registry.
+    /// </summary>
     public static class GetSaveConfigPathCommand
     {
-        public static ICommand<IRegOperationMetadata<string>>
-            ForPath(string value)
+        /// <summary>
+        /// Provides access to the
+        /// <see
+        ///     cref="T:MassFileRenamer.Objects.ICommand" />
+        /// -derived object that
+        /// allows saving the pathname of a configuration file to the system Registry.
+        /// </summary>
+        /// <param name="value">
+        /// (Required.) Fully-qualified pathname that is to be saved in the
+        /// system Registry. This value can, optionally, be surrounded by double
+        /// quotation marks, which become mandatory in the case that the path
+        /// contains spaces.
+        /// </param>
+        /// <returns>
+        /// </returns>
+        public static ICommand<IRegOperationMetadata<string>> ForPath(
+            string value)
             => GetCommand
                .For<IRegOperationMetadata<string>>(
                    MessageType.SaveStringToRegistry
                )
                .WithInput(
                    MakeNewRegOperationMetadata.FromScatch<string>()
-                                              .ForKeyPath(ConfigurationProvider.ConfigurationKeyName)
+                                              .ForKeyPath(
+                                                  ConfigurationProvider
+                                                      .ConfigurationKeyName
+                                              )
                                               .AndValueName(
-                                                  ConfigurationProvider.ConfigurationPathValueName
+                                                  ConfigurationProvider
+                                                      .ConfigurationPathValueName
                                               )
                                               .WithValue(value)
                );
