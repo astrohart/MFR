ca54ca1a2938ca4e60b627296d80fe3162b5a486
Brian Hart <brian.hart@xylogix.net>
2021-03-25 21:57:51 -0400

    Create VisualStudioManagerTests fixture

---
+++ MassFileRenamer.Objects.Tests/VisualStudioManagerTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace MassFileRenamer.Objects.Tests
+{
+    /// <summary>
+    /// Provides unit tests for the methods, properties, and events of the <see cref="T:MassFileRenamer.Objects.VisualStudioManager"/> class.
+    /// </summary>
+    [TestFixture]
+    public class VisualStudioManagerTests
+    {
+        // TODO: Add test fixture implementation here
+    }
+}
