9f908d4de59a03fede8e4895294537342ed53bef
Brian Hart <brian.hart@xylogix.net>
2021-03-28 10:03:38 -0400

    Update XML docs

--- MassFileRenamer.Objects/IStringMatcher.cs
+++ MassFileRenamer.Objects/IStringMatcher.cs
@@ -1,39 +1,48 @@
 ﻿namespace MassFileRenamer.Objects
 {
     /// <summary>
-    /// Defines the publicly-exposed methods and properties of an object that performs matching on strings.
+    /// Defines the publicly-exposed methods and properties of an object that
+    /// performs matching on strings.
     /// </summary>
+    /// <remarks>
+    /// The type of matching performed varies by file- and folder-renaming
+    /// operation type, as well as the specific configuration of matching (Match
+    /// Case, Match Exact Word etc.).
+    /// </remarks>
     public interface IStringMatcher :
         IFixedTextMatchingConfigurationSpecificObject,
         IFixedOperationTypeSpecificObject
     {
         /// <summary>
-        /// Carries out the matching operation using the values
-        /// specified by the provided <paramref name="expression" />.
-        /// Returns a string
+        /// Determines whether a <paramref name="value" /> string is a match
+        /// against a <paramref name="findWhat" />, according to how the
+        /// application is configured.
         /// </summary>
-        /// <param name="source">
-        /// (Required.)
-        /// String containing the data upon which the matching
-        /// operation is to be carried out.
+        /// <param name="value">
+        /// (Required.) String containing the value to check for matches.
         /// </param>
-        /// <param
-        ///     name="pattern">
-        /// (Required.) String containing the pattern that
-        /// allows us to match the data to be replaced.
+        /// <param name="findWhat">
+        /// (Optional.) String containing the pattern that specifies the search criteria.
         /// </param>
-        /// <param
-        ///     name="dest">
-        /// (Optional.) Default is blank. String containing
-        /// the new data that should be substituted for the replaced text.
+        /// <param name="replaceWith">
+        /// (Required.) String containing the data that all instances of
+        /// <paramref name="findWhat" /> in <paramref name="value" /> are to be
+        /// replaced with.
         /// </param>
-        /// <returns> String containing the new data. </returns>
         /// <exception cref="T:System.ArgumentException">
-        /// Thrown if either
-        /// of the required parameters, <paramref name="source" /> ,
-        /// <paramref name="pattern" />, or <paramref name="dest" /> , are
-        /// passed blank or <see langword="null" /> string for values.
+        /// Thrown if either of the required parameters,
+        /// <paramref
+        ///     name="value" />
+        /// or <paramref name="findWhat" />, are passed blank or
+        /// <see langword="null" /> string for values.
         /// </exception>
-        string Replace(string source, string pattern, string dest = "");
+        /// <returns>
+        /// Returns <see langword="true" /> if the <paramref name="value" /> is a
+        /// match against the provided <paramref name="findWhat" />;
+        /// <see
+        ///     langword="false" />
+        /// if no matches are found.
+        /// </returns>
+        bool IsMatch(string value, string findWhat, string replaceWith = "");
     }
 }
