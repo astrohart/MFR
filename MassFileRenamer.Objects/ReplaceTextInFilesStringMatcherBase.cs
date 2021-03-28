9bb7e78161d5b8fb19d5a0b320a9a00521fd316f
Brian Hart <brian.hart@xylogix.net>
2021-03-28 10:09:34 -0400

    Create 'ReplaceTextInFilesStringMatcherBase' class

---
+++ MassFileRenamer.Objects/ReplaceTextInFilesStringMatcherBase.cs
@@ -0,0 +1,69 @@
+﻿using PostSharp.Patterns.Diagnostics;
+
+namespace MassFileRenamer.Objects
+{
+    /// <summary>
+    /// Defines the methods, properties, and events that all String Matcher
+    /// objects for the Replace Text in Files operation type have in common.
+    /// </summary>
+    public abstract class ReplaceTextInFilesStringMatcherBase : IStringMatcher
+    {
+        /// <summary>
+        /// Gets one of the
+        /// <see
+        ///     cref="T:MassFileRenamer.Objects.OperationType" />
+        /// values that
+        /// corresponds to the type of operation being performed.
+        /// </summary>
+        [Log(AttributeExclude = true)]
+        public OperationType OperationType
+        {
+            get;
+        } = OperationType.ReplaceTextInFiles;
+
+        /// <summary>
+        /// Determines whether a <paramref name="value" /> string is a match
+        /// against a <paramref name="findWhat" />, according to how the
+        /// application is configured.
+        /// </summary>
+        /// <param name="value">
+        /// (Required.) String containing the value to check for matches.
+        /// </param>
+        /// <param name="findWhat">
+        /// (Optional.) String containing the pattern that specifies the search criteria.
+        /// </param>
+        /// <param name="replaceWith">
+        /// (Required.) String containing the data that all instances of
+        /// <paramref name="findWhat" /> in <paramref name="value" /> are to be
+        /// replaced with.
+        /// </param>
+        /// <exception cref="T:System.ArgumentException">
+        /// Thrown if either of the required parameters,
+        /// <paramref
+        ///     name="value" />
+        /// or <paramref name="findWhat" />, are passed blank or
+        /// <see langword="null" /> string for values.
+        /// </exception>
+        /// <returns>
+        /// Returns <see langword="true" /> if the <paramref name="value" /> is a
+        /// match against the provided <paramref name="findWhat" />;
+        /// <see
+        ///     langword="false" />
+        /// if no matches are found.
+        /// </returns>
+        public abstract bool IsMatch(string value, string findWhat, string replaceWith = "");
+
+        /// <summary>
+        /// Gets one of the
+        /// <see
+        ///     cref="T:MassFileRenamer.Objects.TextMatchingConfiguration" />
+        /// values
+        /// that corresponds to the type of operation being performed.
+        /// </summary>
+        [Log(AttributeExclude = true)]
+        public abstract TextMatchingConfiguration TextMatchingConfiguration
+        {
+            get;
+        }
+    }
+}
