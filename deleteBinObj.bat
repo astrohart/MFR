707711e4757458e91c76685a8fe1ab97a5020887
Brian Hart <brian.hart@xylogix.net>
2021-04-10 07:51:37 -0400

    Update deleteBinObj.bat to show progress

--- deleteBinObj.bat
+++ deleteBinObj.bat
@@ -1,4 +1,10 @@
 @echo off
 CD /D "%~dp0" >NUL 2>NUL
-FOR /F "tokens=*" %%G IN ('DIR /B /AD /S bin') DO RMDIR /S /Q "%%G"
-FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
+FOR /F "tokens=*" %%G IN ('DIR /B /AD /S bin') DO (
+    ECHO Deleting '%%G'...
+    RMDIR /S /Q "%%G"
+)
+FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO (
+    ECHO Deleting '%%G'...
+    RMDIR /S /Q "%%G"
+)
