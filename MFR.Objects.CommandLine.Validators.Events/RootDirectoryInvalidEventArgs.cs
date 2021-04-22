9896d5bce9467ee8a17e57d17eab82557153b676
Brian Hart <brian.hart@xylogix.net>
2021-04-22 08:47:54 -0400

    Create RootDirectoryInvalid event

---
+++ MFR.Objects.CommandLine.Validators.Events/RootDirectoryInvalidEventArgs.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MFR.Objects.CommandLine.Validators.Events
+{
+    /// <summary>
+    /// Provides information for RootDirectoryInvalid event handlers.
+    /// </summary>
+    public class RootDirectoryInvalidEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Constructs a new instance of
+        /// <see
+        ///     cref="T:MFR.Objects.CommandLine.Validators.Events.RootDirectoryInvalidEventArgs" />
+        /// and returns a reference to it.
+        /// </summary>
+        /// <param name="rootDirectory">
+        /// (Required.) String containing the value of the root directory
+        /// specified by the user on the command line.
+        /// </param>
+        public RootDirectoryInvalidEventArgs(string rootDirectory)
+        {
+            RootDirectory = rootDirectory;
+        }
+
+        /// <summary>
+        /// Gets the string containing the root directory that was specified by
+        /// the user on the command line.
+        /// </summary>
+        public string RootDirectory
+        {
+            get;
+        }
+    }
+}
---
+++ MFR.Objects.CommandLine.Validators.Events/RootDirectoryInvalidEventHandler.cs
@@ -0,0 +1,21 @@
+﻿namespace MFR.Objects.CommandLine.Validators.Events
+{
+    /// <summary>
+    /// Represents a handler for a RootDirectoryInvalid event.
+    /// </summary>
+    /// <param name="sender">
+    /// Reference to the instance of the object that raised the event.
+    /// </param>
+    /// <param name="e">
+    /// A
+    /// <see
+    ///     cref="T:MFR.Objects.CommandLine.Validators.Events.RootDirectoryInvalidEventArgs" />
+    /// that contains the event data.
+    /// </param>
+    /// <remarks>
+    /// This delegate merely specifies the signature of all methods that handle
+    /// the RootDirectoryInvalid event.
+    /// </remarks>
+    public delegate void RootDirectoryInvalidEventHandler(object sender,
+        RootDirectoryInvalidEventArgs e);
+}
