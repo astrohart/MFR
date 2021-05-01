31009b1f60d36db8233c2f03dbf4e469fd70ebc6
Brian Hart <brian.hart@xylogix.net>
2021-05-01 16:41:20 -0400

    FInished fleshing out the 'IVisualStudioSolutionManager' interface body

---
+++ MFR.Objects.Managers.Solutions.Interfaces/IVisualStudioSolutionManager.cs
@@ -0,0 +1,90 @@
+﻿using MFR.Objects.Solutions.Interfaces;
+using System.Collections.Generic;
+
+namespace MFR.Objects.Managers.Solutions.Interfaces
+{
+    /// <summary>
+    /// Defines the publicly-exposed methods and properties of a <c> Solution
+    /// Manager </c> object.
+    /// </summary>
+    /// <remarks>
+    /// <code>Solution Manager</code>
+    /// objects allow management of Visual Studio Solution (*.sln) objects, such
+    /// as scanning for them, detecting if they're loading, and loading /
+    /// unloading them in open instances of Visual Studio.
+    /// </remarks>
+    public interface IVisualStudioSolutionManager
+    {
+        /// <summary>
+        /// Determines whether the folder having path passed in the <paramref
+        /// name="folder"/> parameter contains any *.sln files that represent
+        /// solutions currently loaded by running instances of Visual Studio.
+        /// </summary>
+        /// <param name="folder">
+        /// (Required.) String containing the fully-qualified pathname of a
+        /// folder that should be scanned for *.sln files.
+        /// </param>
+        /// <returns>
+        /// <see langword="true"/> if the specified <paramref name="folder"/>
+        /// contains *.sln files that are currently loaded by running instances
+        /// of Visual Studio; <see langword="false"/> otherwise.
+        /// </returns>
+        bool ContainsLoadedSolutions(string folder);
+
+        /// <summary>
+        /// Scans the folder whose path is provided in the <paramref
+        /// name="folder"/> parameter for *.sln files, and, for each file
+        /// located, determines whether a running instance of Visual Studio has
+        /// it loaded.
+        /// <para/>
+        /// If this is so, then information about the loaded solution is
+        /// returned as an element of a collection of any other such solutions.
+        /// </summary>
+        /// <param name="folder">
+        /// (Required.) String containing the fully-qualified pathname of a
+        /// folder that should be scanned for *.sln files.
+        /// </param>
+        /// <returns>
+        /// Read-only list of references to instances of objects that implement
+        /// the <see
+        /// cref="T:MFR.Objects.Solutions.Interfaces.IVisualStudioSolution"/>
+        /// interface for each instance of Visual Studio that has that solution loaded.
+        /// </returns>
+        /// <exception cref="T:System.ArgumentException">
+        /// Thrown if the required parameter, <paramref name="folder"/>, is
+        /// passed a blank or <see langword="null"/> string for a value.
+        /// </exception>
+        /// <exception cref="T:DirectoryNotFoundException">
+        /// Thrown if the directory whose fully-qualified path is passed in the
+        /// <paramref name="folder"/> parameter cannot be found on the disk.
+        /// </exception>
+        IReadOnlyList<IVisualStudioSolution> GetLoadedSolutionsInFolder(
+            string folder);
+
+        /// <summary>
+        /// Instructs the appropriate currently-running instances of Visual
+        /// Studio who had solutions in the specified collection open at one
+        /// time, to load them again.
+        /// </summary>
+        /// <param name="solutions">
+        /// (Required.) Reference to a collection of references to instances of
+        /// objects that implement the <see
+        /// cref="T:MFR.Objects.Solutions.Interfaces.IVisualStudioSolution"/>
+        /// interface that represent the solution(s) to be loaded.
+        /// </param>
+        void LoadAll(IReadOnlyList<IVisualStudioSolution> solutions);
+
+        /// <summary>
+        /// Instructs the appropriate currently-running instances of Visual
+        /// Studio who had solutions in the specified collection not loaded at
+        /// one time, to unload them again.
+        /// </summary>
+        /// <param name="solutions">
+        /// (Required.) Reference to a collection of references to instances of
+        /// objects that implement the <see
+        /// cref="T:MFR.Objects.Solutions.Interfaces.IVisualStudioSolution"/>
+        /// interface which represent the solution(s) to be unloaded.
+        /// </param>
+        void UnloadAll(IReadOnlyList<IVisualStudioSolution> solutions);
+    }
+}
