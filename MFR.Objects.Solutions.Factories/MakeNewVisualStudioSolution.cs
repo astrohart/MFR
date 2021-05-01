1cfef5af04eb3b7d6233b114747497c4b14b510a
Brian Hart <brian.hart@xylogix.net>
2021-05-01 15:41:48 -0400

    Add back undone changes

---
+++ MFR.Objects.Solutions.Factories/MakeNewVisualStudioSolution.cs
@@ -0,0 +1,15 @@
+﻿namespace MFR.Objects.Solutions.Factories
+{
+    /// <summary>
+    /// Creates new instances of objects that implement the <see cref="T:EnvDTE.DTE" /> interface, and returns references to them.
+    /// </summary>
+    public static class MakeNewVisualStudioSolution
+    {
+        /// <summary>
+        /// Creates a new instance of an object that implements the <see cref="T:EnvDTE.DTE" /> interface and returns a reference to it.
+        /// </summary>
+        /// <returns>Reference to an instance of an object that implements the <see cref="T:EnvDTE.DTE" /> interface.</returns>
+        public static IVisualStudioSolution FromScratch()
+            => new VisualStudioSolution();
+    }
+}
