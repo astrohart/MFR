01382e16cf42fddaeed2eaf7c3ebfc8ca97ab4d4
Brian Hart <brian.hart@xylogix.net>
2021-05-01 15:42:36 -0400

    Create class 'MakeNewVisualStudioSolution'

--- MFR.Objects.Solutions.Factories/MakeNewVisualStudioSolution.cs
+++ MFR.Objects.Solutions.Factories/MakeNewVisualStudioSolution.cs
@@ -1,14 +1,16 @@
-﻿namespace MFR.Objects.Solutions.Factories
+﻿using MFR.Objects.Solutions.Interfaces;
+
+namespace MFR.Objects.Solutions.Factories
 {
     /// <summary>
-    /// Creates new instances of objects that implement the <see cref="T:EnvDTE.DTE" /> interface, and returns references to them.
+    /// Creates new instances of objects that implement the <see cref="T:MFR.Objects.Solutions.Interfaces.IVisualStudioSolution" /> interface, and returns references to them.
     /// </summary>
     public static class MakeNewVisualStudioSolution
     {
         /// <summary>
-        /// Creates a new instance of an object that implements the <see cref="T:EnvDTE.DTE" /> interface and returns a reference to it.
+        /// Creates a new instance of an object that implements the <see cref="T:MFR.Objects.Solutions.Interfaces.IVisualStudioSolution" /> interface and returns a reference to it.
         /// </summary>
-        /// <returns>Reference to an instance of an object that implements the <see cref="T:EnvDTE.DTE" /> interface.</returns>
+        /// <returns>Reference to an instance of an object that implements the <see cref="T:MFR.Objects.Solutions.Interfaces.IVisualStudioSolution" /> interface.</returns>
         public static IVisualStudioSolution FromScratch()
             => new VisualStudioSolution();
     }
