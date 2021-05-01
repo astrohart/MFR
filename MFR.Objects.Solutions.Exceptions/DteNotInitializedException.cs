199bfe43ae34faf6f3c12f2f50d7d4a3ac065614
Brian Hart <brian.hart@xylogix.net>
2021-05-01 15:25:23 -0400

    Create DteNotInitializedException class

---
+++ MFR.Objects.Solutions.Exceptions/DteNotInitializedException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MFR.Objects.Solutions.Exceptions
+{
+    /// <summary>
+    /// Thrown when a variable designed to hold a reference to an instance of an
+    /// object that implements the <see cref="T:EnvDTE.DTE" /> interface and
+    /// which is needed for the current operation has not yet been initialized.
+    /// </summary>
+    public class DteNotInitializedException : Exception
+    {
+        /// <summary>
+        /// Constructs a new instance of
+        /// <see
+        ///     cref="T:MFR.Objects.Solutions.Exceptions.DteNotInitializedException" />
+        /// and returns a reference to it.
+        /// </summary>
+        public DteNotInitializedException() : base(
+            "Please ensure the required DTE variable has been initialized."
+        ) { }
+    }
+}
