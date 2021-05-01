3593d8b7b47a611c1c2f2133b1993762bee07382
Brian Hart <brian.hart@xylogix.net>
2021-05-01 16:28:04 -0400

    Pull members up

--- MFR.Objects.Solutions.Interfaces/IVisualStudioSolution.cs
+++ MFR.Objects.Solutions.Interfaces/IVisualStudioSolution.cs
@@ -38,6 +38,13 @@
         }
 
         /// <summary>
+        /// Gets or sets a value that specifies whether the solution should be
+        /// closed and then reopened during an operation. <see langword="true" />
+        /// by default.
+        /// </summary>
+        bool ShouldReopen { get; set; }
+
+        /// <summary>
         /// Forces either a currently-running instance of Visual Studio to load
         /// the solution, or opens the solution in a new instance of <c>devenv.exe</c>.
         /// </summary>
