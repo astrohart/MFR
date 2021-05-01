f01e3dbce9cc92430e7c93782d6f944936e2ff21
Brian Hart <brian.hart@xylogix.net>
2021-05-01 15:44:26 -0400

    Rename class

---
+++ MFR.Objects.Solutions.Factories/GetNewVisualStudioSolutionObject.cs
@@ -0,0 +1,10 @@
+﻿namespace MFR.Objects.Solutions.Factories
+{
+    /// <summary>
+    /// Creates new instances of objects that implement the
+    /// <see
+    ///     cref="T:MFR.Objects.Solutions.Interfaces.IVisualStudioSolution" />
+    /// interface, and returns references to them.
+    /// </summary>
+    public static class GetNewVisualStudioSolutionObject { }
+}
