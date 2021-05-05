96a5c0bd598fb130cc8d459673ed11a62b2a4f94
Brian Hart <brian.hart@xylogix.net>
2021-05-05 11:49:27 -0400

    Add unit tests and rename files to match type name

---
+++ MFR.Objects.Managers.Solutions.Tests/VisualStudioSolutionServiceTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace MFR.Objects.Managers.Solutions.Tests
+{
+    /// <summary>
+    /// Provides unit tests for the methods, properties, and events of the
+    /// <see
+    ///     cref="T:MFR.Objects.Managers.Solutions.VisualStudioSolutionService" />
+    /// class.
+    /// </summary>
+    [TestFixture]
+    public class VisualStudioSolutionServiceTests
+    {
+        /// <summary>
+        /// TODO: Add unit test documentation here
+        /// </summary>
+        [Test]
+        public void Test_() { }
+    }
+}
