617ae7d93bfd5c955937e05420f5c3fc1bd04ef5
Brian Hart <brian.hart@xylogix.net>
2021-05-07 10:24:11 -0400

    Create VisualStudioSolutionServiceAction class

---
+++ MFR.Objects.Managers.Solutions.Actions.Constants/VisualStudioSolutionServiceAction.cs
@@ -0,0 +1,26 @@
+﻿using MFR.Objects.Messages.Constants;
+using System;
+
+namespace MFR.Objects.Managers.Solutions.Actions.Constants
+{
+    /// <summary>
+    /// Defines names for the types of actions that can be performed on
+    /// <c>Visual Studio Solution Service</c> objects.
+    /// </summary>
+    public class VisualStudioSolutionServiceAction : ActionType
+    {
+        /// <summary>
+        /// Constructs a new instance of
+        /// <see
+        ///     cref="T:MFR.Objects.Managers.Solutions.Actions.Constants.VisualStudioSolutionAction" />
+        /// and returns a reference to it.
+        /// </summary>
+        /// <param name="messageId">
+        /// (Required.) Reference to an instance of <see cref="T:System.Guid" />
+        /// that provides a unique identifier to be associated with this instance.
+        /// </param>
+        public VisualStudioSolutionServiceAction(Guid messageId) : base(
+            messageId
+        ) { }
+    }
+}
