79b6338b3ddd27e4518fe6697f241eb495bac464
Brian Hart <brian.hart@xylogix.net>
2021-07-08 22:26:55 -0400

    Create ProfileCollection and IProfileCollection

---
+++ MFR.Profiles.Collections.Interfaces/IProfileCollection.cs
@@ -0,0 +1,13 @@
+﻿using MFR.Profiles.Interfaces;
+using System.Collections.Generic;
+
+namespace MFR.Profiles.Collections.Interfaces
+{
+    /// <summary>
+    /// Defines the publicly-exposed methods and properties of a <c>Profile Collection</c> object.
+    /// </summary>
+    /// <remarks>A <c>Profile Collection</c> object is what it sounds like -- a container of profiles.</remarks>
+    public interface IProfileCollection : ICollection<IProfile>
+    {
+    }
+}
---
+++ MFR.Profiles.Collections/ProfileCollection.cs
@@ -0,0 +1,16 @@
+﻿using MFR.Profiles.Collections.Interfaces;
+using MFR.Profiles.Interfaces;
+using System.Collections.ObjectModel;
+
+namespace MFR.Profiles.Collections
+{
+    /// <summary>
+    /// Implements the methods and properties of a <c>Profile Collection</c> object.
+    /// </summary>
+    /// <remarks>
+    /// A <c>Profile Collection</c> object is just that -- a container for
+    /// <c>Profile</c> s.
+    /// </remarks>
+    public class
+        ProfileCollection : Collection<IProfile>, IProfileCollection { }
+}
