dc046293abc6102634ab38c98d6b6afce4fe71b7
Brian Hart <brian.hart@xylogix.net>
2021-07-12 08:22:26 -0400

    Make into a Singleton

--- MFR.Profiles.Providers/ProfileProvider.cs
+++ MFR.Profiles.Providers/ProfileProvider.cs
@@ -1,5 +1,6 @@
 ﻿using MFR.Profiles.Collections.Interfaces;
 using MFR.Profiles.Providers.Interfaces;
+using System;
 
 namespace MFR.Profiles.Providers
 {
@@ -8,6 +9,25 @@ namespace MFR.Profiles.Providers
     /// </summary>
     public class ProfileProvider : IProfileProvider
     {
+        /// <summary>
+        /// Empty, static constructor to prohibit direct allocation of this class.
+        /// </summary>
+        static ProfileProvider() { }
+
+        /// <summary>
+        /// Empty, protected constructor to prohibit direct allocation of this class.
+        /// </summary>
+        protected ProfileProvider() { }
+
+        /// <summary>
+        /// Gets a reference to the one and only instance of
+        /// <see cref="T:MFR.Profiles.Providers.ProfileProvider" />.
+        /// </summary>
+        public static ProfileProvider Instance
+        {
+            get;
+        } = new ProfileProvider();
+
         /// <summary>
         /// Gets the default folder for the configuration file.
         /// </summary>
@@ -49,7 +69,8 @@ namespace MFR.Profiles.Providers
         /// <summary>
         /// Gets a reference to an instance of an object that implements the
         /// <see
-        /// cref="T:MFR.Profiles.Collections.Interfaces.IProfileCollection"/> interface.
+        ///     cref="T:MFR.Profiles.Collections.Interfaces.IProfileCollection" />
+        /// interface.
         /// </summary>
         /// <remarks>
         /// The object to which a reference is returned by this properties
@@ -72,29 +93,35 @@ namespace MFR.Profiles.Providers
         /// configuration file, but a separate file.
         /// </remarks>
         /// <exception cref="T:System.IO.FileNotFoundException">
-        /// Thrown if the file whose pathname is passed in the <paramref
-        /// name="pathname"/> parameter cannot be found on the disk.
+        /// Thrown if the file whose pathname is passed in the
+        /// <paramref
+        ///     name="pathname" />
+        /// parameter cannot be found on the disk.
         /// </exception>
         public void Load(string pathname = "")
-            => throw new System.NotImplementedException();
+            => throw new NotImplementedException();
 
         /// <summary>
-        /// Saves profile list data to a file on the disk having path <paramref name="pathname"/>.
+        /// Saves profile list data to a file on the disk having path
+        /// <paramref name="pathname" />.
         /// </summary>
         /// <param name="pathname">
         /// (Optional.) String containing the path to where the data should be
         /// saved in JSON format.
-        /// <para/>
+        /// <para />
         /// If this parameter is blank, then the data is saved to the path that
-        /// is stored in the <see
-        /// cref="P:MFR.Profiles.Providers.Interfaces.IProfileProvider.ProfileListPath"/> property.
+        /// is stored in the
+        /// <see
+        ///     cref="P:MFR.Profiles.Providers.Interfaces.IProfileProvider.ProfileListPath" />
+        /// property.
         /// </param>
         /// <remarks>
-        /// If the <see
-        /// cref="P:MFR.Profiles.Providers.Interfaces.IProfileProvider.ProfileListPath"/>
+        /// If the
+        /// <see
+        ///     cref="P:MFR.Profiles.Providers.Interfaces.IProfileProvider.ProfileListPath" />
         /// property is blank, then this method does nothing.
         /// </remarks>
         public void Save(string pathname = "")
-            => throw new System.NotImplementedException();
+            => throw new NotImplementedException();
     }
 }
