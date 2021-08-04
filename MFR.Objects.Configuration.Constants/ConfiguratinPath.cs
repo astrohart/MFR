c2daef840526b0d680971841cd26bca8206381f9
Brian Hart <brian.hart@xylogix.net>
2021-08-04 14:26:05 -0400

    Create ConfiguraitonPath.cs

---
+++ MFR.Objects.Configuration.Constants/ConfiguratinPath.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MFR.Objects.Configuration.Constants
+{
+    class ConfiguratinPath
+    {
+    }
+}
