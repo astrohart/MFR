3b97eb7959450746fa8e53e7d359680f0d9d114d
Brian Hart <brian.hart@xylogix.net>
2021-08-22 14:12:38 -0500

    Update docs

--- MFR.Settings.Configuration.Factories/MakeNewConfiguration.cs
+++ MFR.Settings.Configuration.Factories/MakeNewConfiguration.cs
@@ -3,14 +3,14 @@
 namespace MFR.Settings.Configuration.Factories
 {
     /// <summary>
-    /// Creates new instances of objects that implement the <see cref="T:MFR.Settings.Configuration.Factories.csproj" /> interface, and returns references to them.
+    /// Creates new instances of objects that implement the <see cref="T:MFR.Settings.Configuration.Interfaces.IConfiguration" /> interface, and returns references to them.
     /// </summary>
     public static class MakeNewConfiguration
     {
         /// <summary>
-        /// Creates a new instance of an object that implements the <see cref="T:MFR.Settings.Configuration.Factories.csproj" /> interface and returns a reference to it.
+        /// Creates a new instance of an object that implements the <see cref="T:MFR.Settings.Configuration.Interfaces.IConfiguration" /> interface and returns a reference to it.
         /// </summary>
-        /// <returns>Reference to an instance of an object that implements the <see cref="T:MFR.Settings.Configuration.Factories.csproj" /> interface.</returns>
+        /// <returns>Reference to an instance of an object that implements the <see cref="T:MFR.Settings.Configuration.Interfaces.IConfiguration" /> interface.</returns>
         public static IConfiguration FromScratch()
             => new Configuration();
     }
