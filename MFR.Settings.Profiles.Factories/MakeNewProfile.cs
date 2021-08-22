cd178793b1376a59186250112fb27d3bedbe8f37
Brian Hart <brian.hart@xylogix.net>
2021-08-22 14:38:59 -0500

    Create MakeNewProfile class

---
+++ MFR.Settings.Profiles.Factories/MakeNewProfile.cs
@@ -0,0 +1,24 @@
+﻿using MFR.Settings.Profiles.Interfaces;
+
+namespace MFR.Settings.Profiles.Factories
+{
+    /// <summary>
+    /// Creates new instances of objects that implement the
+    /// <see cref="T:MFR.Settings.Profiles.Interfaces.IProfile" /> interface, and
+    /// returns references to them.
+    /// </summary>
+    public static class MakeNewProfile
+    {
+        /// <summary>
+        /// Creates a new instance of an object that implements the
+        /// <see cref="T:MFR.Settings.Profiles.Interfaces.IProfile" /> interface and
+        /// returns a reference to it.
+        /// </summary>
+        /// <returns>
+        /// Reference to an instance of an object that implements the
+        /// <see cref="T:MFR.Settings.Profiles.Interfaces.IProfile" /> interface.
+        /// </returns>
+        public static IProfile FromScratch()
+            => new Profile();
+    }
+}
