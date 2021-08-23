faf321d0f0c8c9f99b086d05e5dd295f711045b0
Brian Hart <brian.hart@xylogix.net>
2021-08-23 12:48:08 -0500

    (WIP) Create ProfileParamsValidator.cs
    Created the ProfileParamsValidator class and made it implement the System.Web.UI.IValidator interface.  Am going to be breaking off from this project for a quick second to actually define the same interface in a new 'xyLOGIX.Validators' library that will then be added to this solution -- for semantic consistency.

---
+++ MFR.Params.Profiles.Validators/ProfileParamsValidator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI;
+
+namespace MFR.Params.Profiles.Validators
+{
+    public class ProfileParamsValidator : IValidator
+    {
+        /// <summary>When implemented by a class, evaluates the condition it checks and updates the <see cref="P:System.Web.UI.IValidator.IsValid" /> property.</summary>
+        public void Validate()
+            => throw new NotImplementedException();
+
+        /// <summary>When implemented by a class, gets or sets a value indicating whether the user-entered content in the specified control passes validation.</summary>
+        /// <returns>
+        /// <see langword="true" /> if the content is valid; otherwise, <see langword="false" />.</returns>
+        public bool IsValid
+        {
+            get;
+            set;
+        }
+
+        /// <summary>When implemented by a class, gets or sets the error message text generated when the condition being validated fails.</summary>
+        /// <returns>The error message to generate.</returns>
+        public string ErrorMessage
+        {
+            get;
+            set;
+        }
+    }
+}
