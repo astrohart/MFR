b03d51a37a13925699d2b5dc2abb293281466b0a
Brian Hart <brian.hart@xylogix.net>
2021-08-23 17:02:57 -0500

    Implement ProfileParamsValidator

--- MFR.Params.Profiles.Validators/ProfileParamsValidator.cs
+++ MFR.Params.Profiles.Validators/ProfileParamsValidator.cs
@@ -1,33 +1,70 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web.UI;
+﻿using MFR.Params.Profiles.Interfaces;
+using MFR.Settings.Profiles.Providers.Factories;
+using xyLOGIX.Validators;
+using xyLOGIX.Validators.Events;
 
 namespace MFR.Params.Profiles.Validators
 {
-    public class ProfileParamsValidator : IValidator
+    /// <summary>
+    /// Validates the data in the properties of an instance of an object that
+    /// implements the <see cref="T:MFR.Params.Profiles.Interfaces.IProfileParams" />
+    /// interface.
+    /// </summary>
+    public class ProfileParamsValidator : ObjectValidatorBase<IProfileParams>
     {
-        /// <summary>When implemented by a class, evaluates the condition it checks and updates the <see cref="P:System.Web.UI.IValidator.IsValid" /> property.</summary>
-        public void Validate()
-            => throw new NotImplementedException();
+        /// <summary>
+        /// Constructs a new instance of
+        /// <see cref="T:xyLOGIX.Validators.ObjectValidatorBase" />
+        /// and returns a reference to it.
+        /// </summary>
+        /// <param name="objectToValidate">
+        /// (Required.) Reference to an instance of an object that implements the
+        /// <see cref="T:MFR.Params.Profiles.Interfaces.IProfileParams" /> interface that
+        /// has the data to be validated.
+        /// </param>
+        /// <exception cref="T:System.ArgumentNullException">
+        /// Thrown if the required
+        /// parameter, <paramref name="objectToValidate" />, is passed a
+        /// <see langword="null" /> value.
+        /// </exception>
+        public ProfileParamsValidator(IProfileParams objectToValidate) : base(
+            objectToValidate
+        ) { }
 
-        /// <summary>When implemented by a class, gets or sets a value indicating whether the user-entered content in the specified control passes validation.</summary>
-        /// <returns>
-        /// <see langword="true" /> if the content is valid; otherwise, <see langword="false" />.</returns>
-        public bool IsValid
+        /// <summary>
+        /// When implemented by a class, evaluates the condition it checks and
+        /// updates the <see cref="P:System.Web.UI.IValidator.IsValid" /> property.
+        /// </summary>
+        public override void Validate()
         {
-            get;
-            set;
-        }
+            base.Validate();
 
-        /// <summary>When implemented by a class, gets or sets the error message text generated when the condition being validated fails.</summary>
-        /// <returns>The error message to generate.</returns>
-        public string ErrorMessage
-        {
-            get;
-            set;
+            if (string.IsNullOrWhiteSpace(ObjectToValidate.Name))
+            {
+                OnValidationFailed(
+                    new ValidationFailedEventArgs(
+                        "Profile name cannot be blank."
+                    )
+                );
+                return;
+            }
+
+            // Check whether there is an existing profile with the name we are wanting to use.
+            // If so, then the user must be shown an error message to inform the user that the
+            // name cannot be reused.
+            if (GetProfileProvider.SoleInstance()
+                                  .ProfileCollection
+                                  .HasProfileNamed(ObjectToValidate.Name))
+            {
+                OnValidationFailed(
+                    new ValidationFailedEventArgs(
+                        $"A profile with the name '{ObjectToValidate.Name}' already exists."
+                    )
+                );
+                return;
+            }
+
+            OnValidationSucceeded();
         }
     }
 }
