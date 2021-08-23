0abec31e3927da9942a38d9bc9cc0468029087fe
Brian Hart <brian.hart@xylogix.net>
2021-08-23 11:13:12 -0500

    Create ProfileParams class

---
+++ MFR.Params.Profiles/ProfileParams.cs
@@ -0,0 +1,18 @@
+﻿namespace MFR.Params.Profiles
+{
+    /// <summary>
+    /// Defines properties that encapsulate information about a <c>Profile</c> object
+    /// to be used when initiating requests, e.g., to create profiles etc.
+    /// </summary>
+    public class ProfileParams
+    {
+        /// <summary>
+        /// Gets or sets a string that contains the name of the <c>Profile</c>.
+        /// </summary>
+        public string Name
+        {
+            get;
+            set;
+        }
+    }
+}
