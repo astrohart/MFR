bb87f144cd19fbe9e2d70912d21b00df797cc795
Brian Hart <brian.hart@xylogix.net>
2021-08-23 11:11:04 -0500

    Create unit test for MakeNewConfiguratoin class

---
+++ MFR.Settings.Configuration.Factories.Tests/MakeNewConfigurationTests.cs
@@ -0,0 +1,35 @@
+﻿using MFR.Settings.Configuration.Interfaces;
+using NUnit.Framework;
+
+namespace MFR.Settings.Configuration.Factories.Tests
+{
+    /// <summary>
+    /// Provides unit tests for the methods, properties, and events of the
+    /// <see cref="T:MFR.Settings.Configuration.Factories.MakeNewConfiguration" />
+    /// class.
+    /// </summary>
+    [TestFixture]
+    public class MakeNewConfigurationTests
+    {
+        /// <summary>
+        /// Asserts that the
+        /// <see
+        ///     cref="M:MFR.Settings.Configuration.Factories.MakeNewConfiguration.FromScratch" />
+        /// method returns a reference to an instance of an object that is (a) non-
+        /// <see langword="null" />, (b) an instance of the
+        /// <see cref="T:MFR.Settings.Configuration.Configuration" /> class, and (c)
+        /// implements the
+        /// <see cref="T:MFR.Settings.Configuration.Interfaces.IConfiguration" />
+        /// interface.
+        /// </summary>
+        [Test]
+        public void Test_MakeNewConfiguraton_FromScratch_Method_Works()
+        {
+            var result = MakeNewConfiguration.FromScratch();
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Is.AssignableFrom(typeof(Configuration)));
+            Assert.That(result, Is.AssignableTo(typeof(IConfiguration)));
+        }
+    }
+}
