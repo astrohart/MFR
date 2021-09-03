66cc6196ecb9ea13ad5446f9a04ace224a6cea65
Brian Hart <brian.hart@xylogix.net>
2021-09-03 10:57:49 -0400

    Delete batchBuildAll.bat

--- batchBuildAll.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-REM
-REM batchRebuildAll.bat - Batch rebuilds all platforms, all configurations of
-REM all projects in the current folder.
-REM
-:SETUP_VARIABLES
-SET "MSBUILD_PATH=%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\MSBuild.exe"
-ECHO MSBUILD_PATH = '%MSBUILD_PATH%'
-PAUSE
