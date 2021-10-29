d9e76f835e2c2464bcf78eaec786313bff913560
Brian Hart <brian.hart@xylogix.net>
2021-10-28 23:27:17 -0500

    Create IOperationTypeInfo.cs

---
+++ MFR.GUI.Models.Interfaces/IOperationTypeInfo.cs
@@ -0,0 +1,40 @@
+﻿using MFR.Operations.Constants;
+
+namespace MFR.GUI.Models.Interfaces
+{
+    /// <summary>
+    /// Defines the publicly-exposed methods and properties of a POCO that encodes
+    /// information about a type of operation.
+    /// </summary>
+    public interface IOperationTypeInfo
+    {
+        /// <summary>
+        /// Gets or sets a value that indicates whether the user wants to perform the
+        /// operation.
+        /// </summary>
+        bool Enabled
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the name of the operation type.
+        /// </summary>
+        string Name
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the <see cref="T:MFR.Operations.Constants.OperationType" /> value
+        /// that corresponds to the type of operation to perform.
+        /// </summary>
+        OperationType OperationType
+        {
+            get;
+            set;
+        }
+    }
+}
