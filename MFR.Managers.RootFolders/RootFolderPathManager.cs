a8b3907c6931564da664702df91d09628cc352bf
Brian Hart <brian.hart@xylogix.net>
2021-11-02 11:56:38 -0500

    Create RootFolderPathManager.cs
    This is going to be the main concrete object that provides root-folder-path-management services.

---
+++ MFR.Managers.RootFolders/RootFolderPathManager.cs
@@ -0,0 +1,33 @@
+﻿using PostSharp.Patterns.Diagnostics;
+
+namespace MFR.Managers.RootFolders
+{
+    /// <summary>
+    /// Class to manage a collection of root folder paths to iterate over, processing
+    /// the find/replace operations for all folders listed in the collection.
+    /// </summary>
+    public class RootFolderPathManager
+    {
+        /// <summary>
+        /// Empty, static constructor to prohibit direct allocation of this class.
+        /// </summary>
+        [Log(AttributeExclude = true)]
+        static RootFolderPathManager() { }
+
+        /// <summary>
+        /// Empty, protected constructor to prohibit direct allocation of this class.
+        /// </summary>
+        [Log(AttributeExclude = true)]
+        protected RootFolderPathManager() { }
+
+        /// <summary>
+        /// Gets a reference to the one and only instance of
+        /// <see cref="T:MFR.Managers.RootFolders.RootFolderPathManager" />.
+        /// </summary>
+        [Log(AttributeExclude = true)]
+        public static RootFolderPathManager Instance
+        {
+            get;
+        } = new RootFolderPathManager();
+    }
+}
