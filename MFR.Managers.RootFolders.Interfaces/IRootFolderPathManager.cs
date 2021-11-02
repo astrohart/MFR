c8968141cbf3915d287ea5b85f092cddcbaf30aa
Brian Hart <brian.hart@xylogix.net>
2021-11-02 15:22:19 -0500

    Remove RootFolders property from IRootFolderPathManager interface

--- MFR.Managers.RootFolders.Interfaces/IRootFolderPathManager.cs
+++ MFR.Managers.RootFolders.Interfaces/IRootFolderPathManager.cs
@@ -9,16 +9,5 @@ namespace MFR.Managers.RootFolders.Interfaces
     /// </summary>
     public interface IRootFolderPathManager
     {
-        /// <summary>
-        /// Gets a reference to a collection of instances of objects that implement the
-        /// <see cref="T:System.String" /> interface.
-        /// <para />
-        /// Each object plays the role of a folder from which the operation(s) that are
-        /// selected by the user start processing from.
-        /// </summary>
-        ICollection<string> RootFolders
-        {
-            get;
-        }
     }
 }
