6d4cfd9234d9a51548f6e01493cdea2d616d7e1e
Brian Hart <brian.hart@xylogix.net>
2021-11-04 13:34:05 -0500

    Create Enumerate.cs

---
+++ MFR.FileSystem.Enumerators/Enumerate.cs
@@ -0,0 +1,201 @@
+using System.Collections.Generic;
+using System.IO;
+using xyLOGIX.FileSystem.Enumerable.Factories;
+
+namespace MFR.FileSystem.Enumerators
+{
+    /// <summary>
+    /// Methods to search the file system.
+    /// </summary>
+    public static class Enumerate
+    {
+        /// <summary>
+        /// Enumerates the subfolders of the folder whose fully-qualified pathname is
+        /// specified by the <paramref name="path" /> parameter.
+        /// </summary>
+        /// <param name="path">
+        /// (Required.) String containing the fully-qualified pathname
+        /// of the folder where the search is to start.
+        /// </param>
+        /// <param name="searchPattern">
+        /// (Required.) String containing one or more wildcard
+        /// expressions for the search.
+        /// <para />
+        /// Each wildcard expression must be separated by a semicolon.
+        /// </param>
+        /// <param name="searchOption">
+        /// (Required.) A
+        /// <see cref="T:System.IO.SearchOption" /> value that specifies whether the search
+        /// should list results from the current folder only, or subfolders as well.
+        /// </param>
+        /// <returns>
+        /// Collection of strings that contains the fully-qualified pathnames of
+        /// the folders contained within the folder whose fully-qualified pathname is
+        /// passed in the <paramref name="path" />.
+        /// </returns>
+        public static IEnumerable<string> Directories(string path,
+            string searchPattern, SearchOption searchOption)
+            => MakeNewFileSystemEnumerable.FromScratch()
+                                          .StartingFromRootFolder(path)
+                                          .UsingSearchPattern(searchPattern)
+                                          .AndWithSearchOption(searchOption)
+                                          .ForDirectoriesOnly();
+
+        /// <summary>
+        /// Enumerates the subfolders of the folder whose fully-qualified pathname is
+        /// specified by the <paramref name="path" /> parameter.
+        /// </summary>
+        /// <param name="path">
+        /// (Required.) String containing the fully-qualified pathname
+        /// of the folder where the search is to start.
+        /// </param>
+        /// <returns>
+        /// Collection of strings that contains the fully-qualified pathnames of
+        /// the folders contained within the folder whose fully-qualified pathname is
+        /// passed in the <paramref name="path" />.
+        /// <para />
+        /// This method only returns those folders in the top level of the folder whose
+        /// fully-qualified pathname is specified for the <paramref name="path" />
+        /// parameter's value.
+        /// </returns>
+        public static IEnumerable<string> Directories(string path)
+            => MakeNewFileSystemEnumerable.FromScratch()
+                                          .StartingFromRootFolder(path)
+                                          .UsingSearchPattern("*")
+                                          .AndWithSearchOption(
+                                              SearchOption.TopDirectoryOnly
+                                          )
+                                          .ForDirectoriesOnly();
+
+        /// <summary>
+        /// Enumerates the subfolders of the folder whose fully-qualified pathname is
+        /// specified by the <paramref name="path" /> parameter.
+        /// </summary>
+        /// <param name="path">
+        /// (Required.) String containing the fully-qualified pathname
+        /// of the folder where the search is to start.
+        /// </param>
+        /// <param name="searchPattern">
+        /// (Required.) String containing one or more wildcard
+        /// expressions for the search.
+        /// <para />
+        /// Each wildcard expression must be separated by a semicolon.
+        /// </param>
+        /// <returns>
+        /// Collection of strings that contains the fully-qualified pathnames of
+        /// the folders contained within the folder whose fully-qualified pathname is
+        /// passed in the <paramref name="path" />.
+        /// <para />
+        /// This method only returns those folders in the top level of the folder whose
+        /// fully-qualified pathname is specified for the <paramref name="path" />
+        /// parameter's value.
+        /// </returns>
+        public static IEnumerable<string> Directories(string path,
+            string searchPattern)
+            => MakeNewFileSystemEnumerable.FromScratch()
+                                          .StartingFromRootFolder(path)
+                                          .UsingSearchPattern(searchPattern)
+                                          .AndWithSearchOption(
+                                              SearchOption.TopDirectoryOnly
+                                          )
+                                          .ForDirectoriesOnly();
+
+        /// <summary>
+        /// Enumerates the files in the top level of the folder whose fully-qualified
+        /// pathname is
+        /// specified by the <paramref name="path" /> parameter.
+        /// </summary>
+        /// <param name="path">
+        /// (Required.) String containing the fully-qualified pathname
+        /// of the folder where the search is to start.
+        /// </param>
+        /// <returns>
+        /// Collection of strings that contains the fully-qualified pathnames of
+        /// the files contained within the top level of the folder whose fully-qualified
+        /// pathname is passed in the <paramref name="path" />.
+        /// <para />
+        /// This method only returns those folders in the top level of the folder whose
+        /// fully-qualified pathname is specified for the <paramref name="path" />
+        /// parameter's value.
+        /// </returns>
+        public static IEnumerable<string> Files(string path)
+            => MakeNewFileSystemEnumerable.FromScratch()
+                                          .StartingFromRootFolder(path)
+                                          .UsingSearchPattern("*")
+                                          .AndWithSearchOption(
+                                              SearchOption.TopDirectoryOnly
+                                          )
+                                          .ForFilesOnly();
+
+        /// <summary>
+        /// Enumerates the files in the top level of the folder whose fully-qualified
+        /// pathname is
+        /// specified by the <paramref name="path" /> parameter.
+        /// </summary>
+        /// <param name="path">
+        /// (Required.) String containing the fully-qualified pathname
+        /// of the folder where the search is to start.
+        /// </param>
+        /// <param name="searchPattern">
+        /// (Required.) String containing one or more wildcard
+        /// expressions for the search.
+        /// <para />
+        /// Each wildcard expression must be separated by a semicolon.
+        /// </param>
+        /// <returns>
+        /// Collection of strings that contains the fully-qualified pathnames of
+        /// the files contained within the top level of the folder whose fully-qualified
+        /// pathname is passed in the <paramref name="path" />.
+        /// <para />
+        /// This method only returns those folders in the top level of the folder whose
+        /// fully-qualified pathname is specified for the <paramref name="path" />
+        /// parameter's value.
+        /// </returns>
+        public static IEnumerable<string> Files(string path,
+            string searchPattern)
+            => MakeNewFileSystemEnumerable.FromScratch()
+                                          .StartingFromRootFolder(path)
+                                          .UsingSearchPattern(searchPattern)
+                                          .AndWithSearchOption(
+                                              SearchOption.TopDirectoryOnly
+                                          )
+                                          .ForFilesOnly();
+
+        /// <summary>
+        /// Enumerates the files in the top level of the folder whose fully-qualified
+        /// pathname is
+        /// specified by the <paramref name="path" /> parameter.
+        /// </summary>
+        /// <param name="path">
+        /// (Required.) String containing the fully-qualified pathname
+        /// of the folder where the search is to start.
+        /// </param>
+        /// <param name="searchPattern">
+        /// (Required.) String containing one or more wildcard
+        /// expressions for the search.
+        /// <para />
+        /// Each wildcard expression must be separated by a semicolon.
+        /// </param>
+        /// <param name="searchOption">
+        /// (Required.) A
+        /// <see cref="T:System.IO.SearchOption" /> value that specifies whether the search
+        /// should list results from the current folder only, or subfolders as well.
+        /// </param>
+        /// <returns>
+        /// Collection of strings that contains the fully-qualified pathnames of
+        /// the files contained within the top level of the folder whose fully-qualified
+        /// pathname is passed in the <paramref name="path" />.
+        /// <para />
+        /// This method only returns those folders in the top level of the folder whose
+        /// fully-qualified pathname is specified for the <paramref name="path" />
+        /// parameter's value.
+        /// </returns>
+        public static IEnumerable<string> Files(string path,
+            string searchPattern, SearchOption searchOption)
+            => MakeNewFileSystemEnumerable.FromScratch()
+                                          .StartingFromRootFolder(path)
+                                          .UsingSearchPattern(searchPattern)
+                                          .AndWithSearchOption(searchOption)
+                                          .ForFilesOnly();
+    }
+}
