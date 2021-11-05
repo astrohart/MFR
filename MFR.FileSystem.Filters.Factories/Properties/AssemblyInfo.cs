599d0d28501e3e6eef6950d09f3d4db3537c0a50
Brian Hart <brian.hart@xylogix.net>
2021-11-05 07:03:55 -0500

    Create MFR.FileSystem.Fiilters module

---
+++ MFR.FileSystem.Filters.Factories/Properties/AssemblyInfo.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("xyLOGIX Factories Support Library for Project File Renamer's Filters for File System Searches Component")]
+[assembly: AssemblyDescription("Provides objects and functionality for objects that search the file system while maintaining fluent method-call semantics.  This particular library holds the factories for this component.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("xyLOGIX, LLC")]
+[assembly: AssemblyProduct("xyLOGIX Support Library for Project File Renamer")]
+[assembly:
+    AssemblyCopyright(
+        "Copyright © 2021 by xyLOGIX, LLC.  All rights reserved.")]
+[assembly:
+    AssemblyTrademark(
+        "This software is a component of xyLOGIX Project File Renamer.  xyLOGIX Project File Renamer is a trademark of xyLOGIX, LLC.  All rights reserved, especially those granted by international treaties.")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("68b8ca64-ebb3-4f54-8ec5-d32a511c1782")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
