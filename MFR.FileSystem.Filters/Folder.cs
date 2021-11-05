e72b32a3fc2fd2b4ec4d62d4b664e2b9924feb1b
Brian Hart <brian.hart@xylogix.net>
2021-11-05 06:53:45 -0500

    Make Folder a Singleton
    The Singleton instance property is called Contains.  Thsi is for semantic reasons.

--- MFR.FileSystem.Filters/Folder.cs
+++ MFR.FileSystem.Filters/Folder.cs
@@ -8,6 +8,22 @@ namespace MFR.FileSystem.Filters
 {
     public class Folder
     {
-        
+        /// <summary>
+        /// Gets a reference to the one and only instance of <see cref="T:MFR.FileSystem.Filters.Folder"/>.
+        /// </summary>
+        public static Folder Contains
+        {
+            get;
+        } = new Folder();
+
+        /// <summary>
+        /// Empty, static constructor to prohibit direct allocation of this class.
+        /// </summary>
+        static Folder() { }
+
+        /// <summary>
+        /// Empty, protected constructor to prohibit direct allocation of this class.
+        /// </summary>
+        protected Folder() { }
     }
 }
