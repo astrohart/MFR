7b45209bfa04889b608d558811e9a766765755a1
Brian Hart <brian.hart@xylogix.net>
2021-11-11 17:21:55 -0500

    Update ProfileManagementDialogBox.cs
    Add XML documentation.

--- MFR.GUI.Dialogs/ProfileManagementDialogBox.cs
+++ MFR.GUI.Dialogs/ProfileManagementDialogBox.cs
@@ -2,8 +2,16 @@
 
 namespace MFR.GUI.Dialogs
 {
+    /// <summary>
+    /// Dialog box to assist the user in managing configuration profiles.
+    /// </summary>
     public partial class ProfileManagementDialogBox : Form
     {
+        /// <summary>
+        /// Constructs a new instance of
+        /// <see cref="T:MFR.GUI.Dialogs.ProfileManagementDialogBox" /> and returns a
+        /// reference to it.
+        /// </summary>
         public ProfileManagementDialogBox()
         {
             InitializeComponent();
