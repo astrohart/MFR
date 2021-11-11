a75823529c68c4af7603ec26f3d2afa8b7ef2329
Brian Hart <brian.hart@xylogix.net>
2021-11-11 16:22:53 -0500

    Update IProfileCollection.cs
    Added an indexer.  Did cleanup and reformatting with CodeMaid.  Removed unused usings.

--- MFR.Settings.Profiles.Collections.Interfaces/IProfileCollection.cs
+++ MFR.Settings.Profiles.Collections.Interfaces/IProfileCollection.cs
@@ -1,25 +1,47 @@
 ﻿using MFR.Settings.Profiles.Interfaces;
-using System;
 using System.Collections.Generic;
 
 namespace MFR.Settings.Profiles.Collections.Interfaces
 {
     /// <summary>
-    /// Defines the publicly-exposed methods and properties of a <c>Profile Collection</c> object.
+    /// Defines the publicly-exposed methods and properties of a
+    /// <c>Profile Collection</c> object.
     /// </summary>
-    /// <remarks>A <c>Profile Collection</c> object is what it sounds like -- a container of profiles.</remarks>
+    /// <remarks>
+    /// A <c>Profile Collection</c> object is what it sounds like -- a
+    /// container of profiles.
+    /// </remarks>
     public interface IProfileCollection : ICollection<IProfile>
     {
         /// <summary>
-        ///     Determines whether the profile collection already has a profile with the
-        ///     name provided.
-        ///     <para />
-        ///     There can only be one profile with a given name, case-insensitive.
+        /// Gets the object implementing the
+        /// <see cref="T:MFR.Settings.Profiles.Interfaces.IProfile" /> interface at the
+        /// position <paramref name="i" />  in the collection.
+        /// </summary>
+        /// <param name="i">
+        /// (Required.) Integer, that is zero or greater, referring to the
+        /// index of the desired element.
+        /// </param>
+        /// <returns>
+        /// Reference to an instance of an object that implements the
+        /// <see cref="T:MFR.Settings.Profiles.Interfaces.IProfile" /> interface that
+        /// exists at the specified index.
+        /// </returns>
+        IProfile this[int i]
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Determines whether the profile collection already has a profile with the
+        /// name provided.
+        /// <para />
+        /// There can only be one profile with a given name, case-insensitive.
         /// </summary>
         /// <param name="name">(Required.) Name of the profile to be searched for.</param>
         /// <returns>
-        ///     <see langword="true" /> if a profile having the given name is found;
-        ///     <see langword="false" /> otherwise.
+        /// <see langword="true" /> if a profile having the given name is found;
+        /// <see langword="false" /> otherwise.
         /// </returns>
         bool HasProfileNamed(string name);
     }
