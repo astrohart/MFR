09f3b3b6e32005017be879acf440727cba86ef34
Brian Hart <brian.hart@xylogix.net>
2021-11-13 14:29:20 -0600

    Update CommandLineInfo.cs
    Extract the ICommandLineInfo interface

--- MFR.CommandLine/CommandLineInfo.cs
+++ MFR.CommandLine/CommandLineInfo.cs
@@ -5,13 +5,21 @@ using System.Linq;
 
 namespace MFR.CommandLine
 {
+    public interface ICommandLineInfo
+    {
+        /// <summary>
+        /// Gets or sets a string that contains the starting folder.
+        /// </summary>
+        string RootDirectory { get; set; }
+    }
+
     /// <summary>
     /// Encapsulates this application's command line arguments.
     /// </summary>
     /// <remarks>
     /// This class depends on the <c>FluentCommandLineParser</c> NuGet package.
     /// </remarks>
-    public class CommandLineInfo
+    public class CommandLineInfo : ICommandLineInfo
     {
         /// <summary>
         /// Gets or sets a string that contains the starting folder.
