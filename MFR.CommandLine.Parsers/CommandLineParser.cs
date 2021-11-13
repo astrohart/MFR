90540145be42924242d1580beb61ad7c64e260c4
Brian Hart <brian.hart@xylogix.net>
2021-11-13 14:26:04 -0600

    Create CommandLineParser.cs

---
+++ MFR.CommandLine.Parsers/CommandLineParser.cs
@@ -0,0 +1,13 @@
+﻿using PostSharp.Patterns.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MFR.CommandLine.Parsers
+{
+    public class CommandLineParser
+    {
+    }
+}
