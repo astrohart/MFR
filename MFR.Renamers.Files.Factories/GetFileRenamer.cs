ed4ea37ba886c63516e615030857cc77aa53b473
Brian Hart <brian.hart@xylogix.net>
2021-11-14 14:37:35 -0600

    Update MFR.Renamers.Files.Factories.csproj
    Turn the MakeNewFileRenamer class into a GetFileRenamer.SoleInstance singleton-accessor class.

---
+++ MFR.Renamers.Files.Factories/GetFileRenamer.cs
@@ -0,0 +1,23 @@
+﻿using MFR.Renamers.Files.Interfaces;
+
+namespace MFR.Renamers.Files.Factories
+{
+    /// <summary>
+    /// Provides access to the one and only instance of the object that implements the
+    /// <see cref="T:MFR.Renamers.Files.Interfaces.IFileRenamer" /> interface.
+    /// </summary>
+    public static class GetFileRenamer
+    {
+        /// <summary>
+        /// Obtains access to the sole instance of the object that implements the
+        /// <see cref="T:MFR.Renamers.Files.Interfaces.IFileRenamer" /> interface, and
+        /// returns a reference to it.
+        /// </summary>
+        /// <returns>
+        /// Reference to the one, and only, instance of the object that implements the
+        /// <see cref="T:MFR.Renamers.Files.Interfaces.IFileRenamer" /> interface.
+        /// </returns>
+        public static IFileRenamer SoleInstance()
+            => FileRenamer.Instance;
+    }
+}
