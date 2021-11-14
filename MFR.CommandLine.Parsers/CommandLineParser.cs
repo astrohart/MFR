fdc1a01cc54fa80e024546b851f651271aa7c644
Brian Hart <brian.hart@xylogix.net>
2021-11-13 18:41:53 -0600

    Update MFR.CommandLine.Parsers library
    - Moved the ParseCommandLine method to the CommandLineParser object
    - Installed the FluetCommandLineParser NuGet package

--- MFR.CommandLine.Parsers/CommandLineParser.cs
+++ MFR.CommandLine.Parsers/CommandLineParser.cs
@@ -1,9 +1,9 @@
-﻿using PostSharp.Patterns.Diagnostics;
+﻿using Fclp;
+using MFR.CommandLine.Constants;
+using MFR.CommandLine.Models.Interfaces;
+using PostSharp.Patterns.Diagnostics;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MFR.CommandLine.Parsers
 {
@@ -12,6 +12,22 @@ namespace MFR.CommandLine.Parsers
     /// </summary>
     public class CommandLineParser
     {
+        /// <summary>
+        /// Empty, static constructor to prohibit direct allocation of this class.
+        /// </summary>
+        [Log(AttributeExclude = true)]
+        static CommandLineParser()
+        {
+        }
+
+        /// <summary>
+        /// Empty, protected constructor to prohibit direct allocation of this class.
+        /// </summary>
+        [Log(AttributeExclude = true)]
+        protected CommandLineParser()
+        {
+        }
+
         /// <summary>
         /// Gets a reference to the one and only instance of <see cref="T:MFR.CommandLine.Parsers.CommandLineParser"/>.
         /// </summary>
@@ -22,15 +38,51 @@ namespace MFR.CommandLine.Parsers
         } = new CommandLineParser();
 
         /// <summary>
-        /// Empty, static constructor to prohibit direct allocation of this class.
+        /// Parses the application's command-line arguments and sets the properties
+        /// of a new instance of <see cref="T:UserQuery.CommandLineInfo" />
+        /// accordingly.
         /// </summary>
-        [Log(AttributeExclude = true)]
-        static CommandLineParser() { }
+        /// <param name="args">
+        /// (Required.) Array containing the command-line arguments passed to this
+        /// application.
+        /// </param>
+        public ICommandLineInfo ParseCommandLine(string[] args)
+        {
+            var result = new CommandLineInfo();
+            if (args == null || !args.Any())
+                return result;
 
-        /// <summary>
-        /// Empty, protected constructor to prohibit direct allocation of this class.
-        /// </summary>
-        [Log(AttributeExclude = true)]
-        protected CommandLineParser() { }
+            var p = new FluentCommandLineParser<CommandLineInfo>();
+
+            try
+            {
+                p.Setup(arg => arg.RootDirectory)
+                 .As('r', "root")
+                 .WithDescription(
+                     "Sets the directory that this application begins in."
+                 )
+                 .SetDefault(Directories.MyDocuments);
+
+                p.SetupHelp("?", "help")
+                 .Callback(text => Console.WriteLine(text));
+
+                var parsingResult = p.Parse(args);
+                if (parsingResult.HasErrors)
+                    throw new InvalidOperationException(
+                        "ERROR: Failed to parse command line."
+                    );
+
+                result = p.Object;
+            }
+            catch
+            {
+                if (p != null && p.Options.Any())
+                    p.HelpOption.ShowHelp(p.Options);
+
+                Environment.Exit(-1);
+            }
+
+            return result;
+        }
     }
 }
