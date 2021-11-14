4044560f70f4dfd8679d4e66e1673d0981233892
Brian Hart <brian.hart@xylogix.net>
2021-11-13 18:39:55 -0600

    Update MFR.CommandLine.Models project
    - Moved the ParseCommandLine method to the CommandLineParser object.
    - Uninstalled the FluentCommandLineParser NuGet package.

--- MFR.CommandLine.Models/CommandLineInfo.cs
+++ MFR.CommandLine.Models/CommandLineInfo.cs
@@ -1,4 +1,3 @@
-using Fclp;
 using MFR.CommandLine.Constants;
 using MFR.CommandLine.Models.Interfaces;
 using System;
@@ -22,53 +21,5 @@ namespace MFR.CommandLine.Models
             get;
             set;
         } = Directories.MyDocuments;
-
-        /// <summary>
-        /// Parses the application's command-line arguments and sets the properties
-        /// of a new instance of <see cref="T:UserQuery.CommandLineInfo" />
-        /// accordingly.
-        /// </summary>
-        /// <param name="args">
-        /// (Required.) Array containing the command-line arguments passed to this
-        /// application.
-        /// </param>
-        public static CommandLineInfo ParseCommandLine(string[] args)
-        {
-            var result = new CommandLineInfo();
-            if (args == null || !args.Any())
-                return result;
-
-            var p = new FluentCommandLineParser<CommandLineInfo>();
-
-            try
-            {
-                p.Setup(arg => arg.RootDirectory)
-                 .As('r', "root")
-                 .WithDescription(
-                     "Sets the directory that this application begins in."
-                 )
-                 .SetDefault(Directories.MyDocuments);
-
-                p.SetupHelp("?", "help")
-                 .Callback(text => Console.WriteLine(text));
-
-                var parsingResult = p.Parse(args);
-                if (parsingResult.HasErrors)
-                    throw new InvalidOperationException(
-                        "ERROR: Failed to parse command line."
-                    );
-
-                result = p.Object;
-            }
-            catch
-            {
-                if (p != null && p.Options.Any())
-                    p.HelpOption.ShowHelp(p.Options);
-
-                Environment.Exit(-1);
-            }
-
-            return result;
-        }
     }
 }
