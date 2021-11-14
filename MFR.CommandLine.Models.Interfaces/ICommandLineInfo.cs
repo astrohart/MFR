aeb1327b07c4818a02df1c379b683d66b6a4a5e2
Brian Hart <brian.hart@xylogix.net>
2021-11-14 13:42:59 -0600

    Update ICommandLineInfo.cs
    Added the FindWhat, MatchCase, MatchWholeWord, RenameFiles, RenameSubFolders, ReplaceInFiles, ReplaceWith properties so that the user can configure their search from the command line.

--- MFR.CommandLine.Models.Interfaces/ICommandLineInfo.cs
+++ MFR.CommandLine.Models.Interfaces/ICommandLineInfo.cs
@@ -7,6 +7,66 @@ namespace MFR.CommandLine.Models.Interfaces
     public interface ICommandLineInfo
     {
         /// <summary>
+        /// Gets or sets a string containing the content to be found in the file system
+        /// entries.
+        /// </summary>
+        string FindWhat
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether a case-sensitive search should be done.
+        /// </summary>
+        bool MatchCase
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether a whole-word search should be done.
+        /// </summary>
+        bool MatchWholeWord
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the files are to be renamed.
+        /// </summary>
+        bool RenameFiles
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether sub folders should be renamed.
+        /// </summary>
+        bool RenameSubFolders
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether text in files should be replaced.
+        /// </summary>
+        bool ReplaceinFiles
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// Gets or sets a string containing the content to be replaced in the file system
+        /// entries.
+        /// </summary>
+        string ReplaceWith
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
         /// Gets or sets a string that contains the starting folder.
         /// </summary>
         string RootDirectory
