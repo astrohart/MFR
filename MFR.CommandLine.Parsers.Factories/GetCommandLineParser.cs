361084dc016dd8b9ac274ce2b13a05d2a0c4100b
Brian Hart <brian.hart@xylogix.net>
2021-11-14 13:53:43 -0600

    Create GetCommandLineParser.cs
    Provides a way to access the CommandLineParser singleton without referring to the instance member in the concrete type.  Separates concerns from callers.

---
+++ MFR.CommandLine.Parsers.Factories/GetCommandLineParser.cs
@@ -0,0 +1,19 @@
+﻿using MFR.CommandLine.Parsers.Interfaces;
+
+namespace MFR.CommandLine.Parsers.Factories
+{
+    /// <summary>
+    /// Provides access to the one and only instance of the object that implements the <see cref="T:MFR.CommandLine.Parsers.Interfaces.ICommandLineParser" /> interface.
+    /// </summary>
+    public static class GetCommandLineParser
+    {
+        /// <summary>
+        /// Obtains access to the sole instance of the object that implements the <see cref="T:MFR.CommandLine.Parsers.Interfaces.ICommandLineParser" /> interface, and returns a reference to it.
+        /// </summary>
+        /// <returns>
+        /// Reference to the one, and only, instance of the object that implements the <see cref="T:MFR.CommandLine.Parsers.Interfaces.ICommandLineParser" /> interface.
+        /// </returns>
+        public static ICommandLineParser SoleInstance()
+            => CommandLineParser.Instance;
+    }
+}
