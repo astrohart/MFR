65b0409da80c52b22109cfaa5596a8413e790c7e
Brian Hart <brian.hart@xylogix.net>
2021-11-14 13:46:15 -0600

    Update CommandLineParser.cs
    - Extracted the ICommandLineParser interface
    - Made the Instance property be of type ICommandLineParser so that usage of the singleton isntance variable separates concerns from the concrete type
    - Renamed the ParseCommandLine method to Parse
    - In Parse method, replace new CommandLineInfo() with the more fluent MakeNewCommandLineInfo.FromScratch() call
    - Add method calls within the Parse() method to the FluentCommandLineParser library in order to set up new command line options for the new configuration properties in CommandLineInfo
    - Did a re-format and code cleanup with ReSharper and CodeMaid

--- MFR.CommandLine.Parsers/CommandLineParser.cs
+++ MFR.CommandLine.Parsers/CommandLineParser.cs
@@ -1,6 +1,9 @@
 ﻿using Fclp;
 using MFR.CommandLine.Constants;
+using MFR.CommandLine.Models;
+using MFR.CommandLine.Models.Factroies;
 using MFR.CommandLine.Models.Interfaces;
+using MFR.CommandLine.Parsers.Interfaces;
 using PostSharp.Patterns.Diagnostics;
 using System;
 using System.Linq;
@@ -10,45 +13,43 @@ namespace MFR.CommandLine.Parsers
     /// <summary>
     /// Manages the parsing and validation of the command line of the application.
     /// </summary>
-    public class CommandLineParser
+    public class CommandLineParser : ICommandLineParser
     {
         /// <summary>
         /// Empty, static constructor to prohibit direct allocation of this class.
         /// </summary>
         [Log(AttributeExclude = true)]
-        static CommandLineParser()
-        {
-        }
+        static CommandLineParser() { }
 
         /// <summary>
         /// Empty, protected constructor to prohibit direct allocation of this class.
         /// </summary>
         [Log(AttributeExclude = true)]
-        protected CommandLineParser()
-        {
-        }
+        protected CommandLineParser() { }
 
         /// <summary>
-        /// Gets a reference to the one and only instance of <see cref="T:MFR.CommandLine.Parsers.CommandLineParser"/>.
+        /// Gets a reference to the one and only instance of
+        /// <see cref="T:MFR.CommandLine.Parsers.CommandLineParser" />.
         /// </summary>
         [Log(AttributeExclude = true)]
-        public static CommandLineParser Instance
+        public static ICommandLineParser Instance
         {
             get;
         } = new CommandLineParser();
 
         /// <summary>
-        /// Parses the application's command-line arguments and sets the properties
-        /// of a new instance of <see cref="T:UserQuery.CommandLineInfo" />
+        /// Parses the application's command-line arguments and sets the properties of a
+        /// new instance of an object that implements the
+        /// <see cref="T:MFR.CommandLine.Models.Interfaces.ICommandLineInfo" /> interface
         /// accordingly.
         /// </summary>
         /// <param name="args">
         /// (Required.) Array containing the command-line arguments passed to this
         /// application.
         /// </param>
-        public ICommandLineInfo ParseCommandLine(string[] args)
+        public ICommandLineInfo Parse(string[] args)
         {
-            var result = new CommandLineInfo();
+            var result = MakeNewCommandLineInfo.FromScratch();
             if (args == null || !args.Any())
                 return result;
 
@@ -62,6 +63,45 @@ namespace MFR.CommandLine.Parsers
                      "Sets the directory that this application begins in."
                  )
                  .SetDefault(Directories.MyDocuments);
+
+                p.Setup(arg => arg.FindWhat)
+                 .As("findWhat")
+                 .WithDescription(
+                     "Sets the string to be found in file system entries."
+                 )
+                 .SetDefault(string.Empty);
+
+                p.Setup(arg => arg.ReplaceWith)
+                 .As("replaceWith")
+                 .WithDescription(
+                     "Sets the string to be substituted in file system entry names."
+                 )
+                 .SetDefault(string.Empty);
+
+                p.Setup(arg => arg.RenameFiles)
+                 .As("renameFiles")
+                 .WithDescription("Indicates that files should be renamed.")
+                 .SetDefault(true);
+
+                p.Setup(arg => arg.RenameSubFolders)
+                 .As("renameSubFolders")
+                 .WithDescription("Indicates that folders should be renamed.")
+                 .SetDefault(true);
+
+                p.Setup(arg => arg.ReplaceinFiles)
+                 .As("replaceInFiles")
+                 .WithDescription("Indicates that text should be replaced in files.")
+                 .SetDefault(true);
+
+                p.Setup(arg => arg.MatchCase)
+                 .As("replaceInFiles")
+                 .WithDescription("Indicates that a case-sensitive search should be performed.")
+                 .SetDefault(true);
+
+                p.Setup(arg => arg.MatchWholeWord)
+                 .As("replaceInFiles")
+                 .WithDescription("Indicates that a case-sensitive search should be performed.")
+                 .SetDefault(true);
 
                 p.SetupHelp("?", "help")
                  .Callback(text => Console.WriteLine(text));
