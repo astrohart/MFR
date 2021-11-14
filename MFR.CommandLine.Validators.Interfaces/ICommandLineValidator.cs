7a15397a9db825257374841ea9cd7335c04cef94
Brian Hart <brian.hart@xylogix.net>
2021-11-14 14:29:19 -0600

    Update ICommandLineInfoValidator.cs
    Update the XML documenrtation for the IsValid method

--- MFR.CommandLine.Validators.Interfaces/ICommandLineValidator.cs
+++ MFR.CommandLine.Validators.Interfaces/ICommandLineValidator.cs
@@ -36,8 +36,7 @@ namespace MFR.CommandLine.Validators.Interfaces
         /// </returns>
         /// <exception cref="T:System.ArgumentNullException">
         /// Thrown if the required parameter, <paramref name="rootDirectoryValidator" />,
-        /// is
-        /// passed a <see langword="null" /> value.
+        /// is passed a <see langword="null" /> value.
         /// </exception>
         ICommandLineValidator AssociateWithRootDirectoryValidator(
             IRootDirectoryValidator rootDirectoryValidator);
@@ -51,12 +50,33 @@ namespace MFR.CommandLine.Validators.Interfaces
         /// <param name="cmdInfo">
         /// (Required.) Reference to an instance of an object that implements the
         /// <see cref="T:MFR.CommandLine.Models.Interfaces.ICommandLineInfo" /> interface
-        /// that represents the object whose properties are to be validated.
+        /// that represents the object whose properties' values are to be validated.
         /// </param>
         /// <returns>
         /// <see langword="true" /> if the <paramref name="cmdInfo" /> object's
-        /// properties contain valid values.
+        /// properties contain valid values, according to the current validation rules.
         /// </returns>
+        /// <remarks>
+        /// If the <paramref name="cmdInfo" /> parameter is passed a
+        /// <see langword="null" /> reference, this method raises the
+        /// <see
+        ///     cref="E:MFR.CommandLine.Validators.Interfaces.ICommandLineValidator.CommandLineInfoInvalid" />
+        /// event and then increments the value of the
+        /// <see
+        ///     cref="P:MFR.CommandLine.Validators.Interfaces.IValidator.ValidationFailures" />
+        /// property, and then this method does no further processing.
+        /// <para />
+        /// Otherwise, the object whose reference is passed in the
+        /// <paramref name="cmdInfo" /> parameter is validated.  The
+        /// <see
+        ///     cref="P:MFR.CommandLine.Validators.Interfaces.IValidator.ValidationFailures" />
+        /// property is used to keep track of the number of validation failures that occur.
+        /// <para />
+        /// At the beginning of this method's execution, the
+        /// <see
+        ///     cref="P:MFR.CommandLine.Validators.Interfaces.IValidator.ValidationFailures" />
+        /// property's value is set to zero.
+        /// </remarks>
         bool IsValid(ICommandLineInfo cmdInfo);
     }
 }
