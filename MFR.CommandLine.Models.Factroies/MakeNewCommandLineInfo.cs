1efb2f45699c45970021944daa632c8d3f349f51
Brian Hart <brian.hart@xylogix.net>
2021-11-14 13:36:02 -0600

    Create MakeNewCommandLineInfo.cs
    Decouples the creation of an instance of CommandLineInfo from the caller of the constructor through the ICommandLineInfo interface.
    
    This separates concerns.

---
+++ MFR.CommandLine.Models.Factroies/MakeNewCommandLineInfo.cs
@@ -0,0 +1,24 @@
+﻿using MFR.CommandLine.Models.Interfaces;
+
+namespace MFR.CommandLine.Models.Factroies
+{
+    /// <summary>
+    /// Creates new instances of objects that implement the
+    /// <see cref="T:MFR.CommandLine.Models.Interfaces.ICommandLineInfo" /> interface,
+    /// and returns references to them.
+    /// </summary>
+    public static class MakeNewCommandLineInfo
+    {
+        /// <summary>
+        /// Creates a new instance of an object that implements the
+        /// <see cref="T:MFR.CommandLine.Models.Interfaces.ICommandLineInfo" /> interface
+        /// and returns a reference to it.
+        /// </summary>
+        /// <returns>
+        /// Reference to an instance of an object that implements the
+        /// <see cref="T:MFR.CommandLine.Models.Interfaces.ICommandLineInfo" /> interface.
+        /// </returns>
+        public static ICommandLineInfo FromScratch()
+            => new CommandLineInfo();
+    }
+}
