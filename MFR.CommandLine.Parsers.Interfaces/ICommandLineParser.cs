7d4a0ded1b84f303daea151c2078a96cfa7aca1d
Brian Hart <brian.hart@xylogix.net>
2021-11-14 13:54:32 -0600

    Create ICommandLineParser.cs
    Extract the ICommandLineParser interface from the CommandLineParser object.

---
+++ MFR.CommandLine.Parsers.Interfaces/ICommandLineParser.cs
@@ -0,0 +1,23 @@
+﻿using MFR.CommandLine.Models.Interfaces;
+
+namespace MFR.CommandLine.Parsers.Interfaces
+{
+    /// <summary>
+    /// Defines the publicly-exposed methods and properties of an object that parses
+    /// the command line of the application.
+    /// </summary>
+    public interface ICommandLineParser
+    {
+        /// <summary>
+        /// Parses the application's command-line arguments and sets the properties of a
+        /// new instance of an object that implements the
+        /// <see cref="T:MFR.CommandLine.Models.Interfaces.ICommandLineInfo" /> interface
+        /// accordingly.
+        /// </summary>
+        /// <param name="args">
+        /// (Required.) Array containing the command-line arguments passed to this
+        /// application.
+        /// </param>
+        ICommandLineInfo Parse(string[] args);
+    }
+}
