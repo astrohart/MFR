ff6af5c1e7a48f884df8b430fd8545a860027be0
Brian Hart <brian.hart@xylogix.net>
2021-11-19 13:44:45 -0600

    Update Register.cs
    Added XML docs as required by Vsxmd.

--- MFR.Common/Register.cs
+++ MFR.Common/Register.cs
@@ -3,8 +3,14 @@ using xyLOGIX.Core.Debug;
 
 namespace MFR.Common
 {
+    /// <summary>
+    /// Provides methods to register with the Windows operating system.
+    /// </summary>
     public static class Register
     {
+        /// <summary>
+        /// Registers an OLE Message Filter with the Windows operating system.
+        /// </summary>
         public static void WindowsMessageFilter()
         {
             try
