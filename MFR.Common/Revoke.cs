777ac7135ba179a582b72df427da20ce0cceb6c8
Brian Hart <brian.hart@xylogix.net>
2021-11-19 13:45:01 -0600

    Update Revoke.cs
    Added required XML documentation as per Vsxmd.

--- MFR.Common/Revoke.cs
+++ MFR.Common/Revoke.cs
@@ -3,8 +3,14 @@ using xyLOGIX.Core.Debug;
 
 namespace MFR.Common
 {
+    /// <summary>
+    /// Methods to revoke things from being in a registered status with the operating system.
+    /// </summary>
     public static class Revoke
     {
+        /// <summary>
+        /// Revokes the most-recently-registered OLE message filter.
+        /// </summary>
         public static void WindowsMessageFilter()
         {
             try
