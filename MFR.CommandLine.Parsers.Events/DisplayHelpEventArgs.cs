b30b21822859b0c1dc70d4f27a8808e7f7f45209
Brian Hart <brian.hart@xylogix.net>
2022-01-15 23:02:56 -0600

    Create DisplayEvent event handler and args

---
+++ MFR.CommandLine.Parsers.Events/DisplayHelpEventArgs.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MFR.CommandLine.Parsers.Events
+{
+    /// <summary>
+    /// Provides information for DisplayHelp event handlers.
+    /// </summary>
+    public class DisplayHelpEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Constructs a new instance of
+        /// <see cref="T:MFR.CommandLine.Parsers.Events.DisplayHelpEventArgs" /> and
+        /// returns a reference to it.
+        /// </summary>
+        /// <param name="helpText">
+        /// (Required.) String containing the Help text that is to be displayed.
+        /// </param>
+        public DisplayHelpEventArgs(string helpText)
+        {
+            HelpText = helpText;
+        }
+
+        /// <summary>
+        /// Gets a string that contains the Help text that is to be displayed.
+        /// </summary>
+        public string HelpText
+        {
+            get;
+        }
+    }
+}
