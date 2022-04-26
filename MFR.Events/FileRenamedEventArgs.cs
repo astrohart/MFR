73f527b7d24d115c10fb6cb4a6986873c4ec80ce
Brian Hart <brian.hart@xylogix.net>
2022-04-26 15:52:34 -0500

    Create FileRenamed event

---
+++ MFR.Events/FileRenamedEventArgs.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace MFR.Events
+{
+    /// <summary>
+    /// Provides information for FileRenamed event handlers.
+    /// </summary>
+    public class FileRenamedEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Constructs a new instance of <see cref="T:MFR.Events.FileRenamedEventArgs" />
+        /// and returns a reference to it.
+        /// </summary>
+        /// <param name="source">
+        /// (Required.) A <see cref="T:System.String" /> containing the fully-qualified
+        /// pathname of the file's former location (before the rename operation).
+        /// </param>
+        /// <param name="destination">
+        /// (Required.) A <see cref="T:System.String" /> containing the fully-qualified
+        /// pathname of the file's current location (after the rename operation).
+        /// </param>
+        /// <exception cref="T:System.ArgumentException">
+        /// Thrown if either of the required
+        /// parameters, <paramref name="source" /> or <paramref name="source" />, are
+        /// passed blank or <see langword="null" /> strings for values.
+        /// </exception>
+        public FileRenamedEventArgs(string source, string destination)
+        {
+            if (string.IsNullOrWhiteSpace(source))
+                throw new ArgumentException(
+                    "Value cannot be null or whitespace.", nameof(source)
+                );
+            if (string.IsNullOrWhiteSpace(destination))
+                throw new ArgumentException(
+                    "Value cannot be null or whitespace.", nameof(destination)
+                );
+            Source = source;
+            Destination = destination;
+        }
+
+        /// <summary>
+        /// Gets a <see cref="T:System.String" /> containing the fully-qualified pathname
+        /// of the file's current location.
+        /// </summary>
+        public string Destination
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Gets a <see cref="T:System.String" /> containing the fully-qualified pathname
+        /// of the file's former name.
+        /// </summary>
+        public string Source
+        {
+            get;
+        }
+    }
+}
---
+++ MFR.Events/FileRenamedEventHandler.cs
@@ -0,0 +1,19 @@
+﻿namespace MFR.Events
+{
+    /// <summary>
+    /// Represents a handler for a <c>FileRenamed</c> event.
+    /// </summary>
+    /// <param name="sender">
+    /// Reference to the instance of the object that raised the event.
+    /// </param>
+    /// <param name="e">
+    /// A <see cref="T:MFR.Events.FileRenamedEventArgs" /> that contains the event
+    /// data.
+    /// </param>
+    /// <remarks>
+    /// This delegate merely specifies the signature of all methods that handle the
+    /// <c>FileRenamed</c> event.
+    /// </remarks>
+    public delegate void FileRenamedEventHandler(object sender,
+        FileRenamedEventArgs e);
+}
