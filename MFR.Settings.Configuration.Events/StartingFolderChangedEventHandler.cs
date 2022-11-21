ad3f634f133f4f5857c8788dcc5b66e57f842d16
Brian Hart <brian.hart@xylogix.net>
2022-11-21 13:05:05 -0700

    Create StartingFolderChangedEventHandler.cs
    This is a delegate for an event that is to be raised when the StartingFolder property of the Configuration object is updated.

---
+++ MFR.Settings.Configuration.Events/StartingFolderChangedEventHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MFR.Settings.Configuration.Events
+{
+    /// <summary>
+    /// Represents a handler for a <c>StartingFolderChanged</c> event.
+    /// </summary>
+    /// <param name="sender">
+    /// Reference to the instance of the object that raised the event.
+    /// </param>
+    /// <param name="e">
+    /// A <see cref="T:System.EventArgs" /> that contains the event data.
+    /// </param>
+    /// <remarks>
+    /// This delegate merely specifies the signature of all methods that handle the
+    /// <c>StartingFolderChanged</c> event.
+    /// </remarks>
+    public delegate void StartingFolderChangedEventHandler(object sender,
+        EventArgs e);
+}
