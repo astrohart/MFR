e3d5fdf52121281b8680f5e317f46ee31f7c4cef
Brian Hart <brian.hart@xylogix.net>
2022-12-26 09:11:15 -0700

    Update CancellableProgressDialog.cs
    * Make the setters for the ProgressBarMinimum, ProgressBarMaximum, and ProgressBarValue properties more robust and well-documented.

--- MFR.GUI.Dialogs/CancellableProgressDialog.cs
+++ MFR.GUI.Dialogs/CancellableProgressDialog.cs
@@ -64,7 +64,7 @@ namespace MFR.GUI.Dialogs
             [DebuggerStepThrough] get => progressBar.Maximum;
             set
                 => progressBar.InvokeIfRequired(
-                    () => progressBar.Maximum = value
+                    () => DoSetProgressBarMaximum(value)
                 );
         }
 
@@ -76,7 +76,7 @@ namespace MFR.GUI.Dialogs
             [DebuggerStepThrough] get => progressBar.Minimum;
             set
                 => progressBar.InvokeIfRequired(
-                    () => progressBar.Minimum = value
+                    () => DoSetProgressBarMinimum(value)
                 );
         }
 
@@ -100,7 +100,7 @@ namespace MFR.GUI.Dialogs
             [DebuggerStepThrough] get => progressBar.Value;
             set
                 => progressBar.InvokeIfRequired(
-                    () => progressBar.Value = value
+                    () => DoSetProgressBarValue(value)
                 );
         }
 
@@ -187,6 +187,101 @@ namespace MFR.GUI.Dialogs
         }
 
         /// <summary>
+        /// Sets the value of the <see cref="P:System.Windows.Forms.ProgressBar.Maximum" />
+        /// property but only after first checking that the argument of the
+        /// <paramref name="value" /> parameter falls within a valid range.
+        /// </summary>
+        /// <param name="value">
+        /// (Required.) An <see cref="T:System.Int32" /> that specifies
+        /// the new value of the progress bar's maximum limit.
+        /// </param>
+        /// <remarks>
+        /// This method stops if (a) the argument of the <paramref name="value" />
+        /// parameter is negative (i.e., we do not know what a negative maximum value means
+        /// in the context of a progress bar), or (b) if the argument of the
+        /// <paramref name="value" /> parameter is less than, or equal to, the value of the
+        /// <see cref="P:MFR.GUI.Dialogs.CancellableProgressDialog.ProgressBarMinimum" />
+        /// property, or (c) if the argument of the <paramref name="value" /> parameter is
+        /// less than the value of the
+        /// <see cref="P:MFR.GUI.Dialogs.CancellableProgressDialog.ProgressBarValue" />
+        /// property.
+        /// </remarks>
+        private void DoSetProgressBarMaximum(int value)
+        {
+            if (value < 0) return;
+            if (value <= ProgressBarMinimum) return;
+            if (value < ProgressBarValue) return;
+
+            progressBar.Maximum = value;
+        }
+
+        /// <summary>
+        /// Sets the value of the <see cref="P:System.Windows.Forms.ProgressBar.Minimum" />
+        /// property but only after first checking that the argument of the
+        /// <paramref name="value" /> parameter falls within a valid range.
+        /// </summary>
+        /// <param name="value">
+        /// (Required.) An <see cref="T:System.Int32" /> that specifies the new value of
+        /// the
+        /// progress bar's minimum limit.
+        /// </param>
+        /// <remarks>
+        /// This method stops if (a) the argument of the <paramref name="value" />
+        /// parameter is negative (i.e., we do not know what a negative minimum value means
+        /// in the context of a progress bar), or (b) if the argument of the
+        /// <paramref name="value" /> parameter is greater than, or equal to, the value of
+        /// the <see cref="P:System.Windows.Forms.ProgressBar.Maximum" /> property.
+        /// </remarks>
+        private void DoSetProgressBarMinimum(int value)
+        {
+            /* range validation */
+            if (value < 0) return;
+            if (value >= ProgressBarMaximum) return;
+
+            progressBar.Minimum = value;
+        }
+
+        /// <summary>
+        /// Sets the value of the <see cref="P:System.Windows.Forms.ProgressBar.Value" />
+        /// property after first checking that the argument of the
+        /// <paramref name="value" /> parameter is in the proper range, i.e., between the
+        /// minimum and maximum, inclusive.
+        /// </summary>
+        /// <param name="value">
+        /// (Required.) An <see cref="T:System.Int32" /> value that
+        /// specifies the new value of the progress of an operation.
+        /// </param>
+        /// <remarks>
+        /// This method stops if any of the following are true: (a) if the argument of the
+        /// <paramref name="value" /> parameter is a negative number (we don't know how to
+        /// display negative progress), or (b) if the argument of the
+        /// <paramref name="value" /> parameter is less than the value of the
+        /// <see cref="P:MFR.GUI.Dialogs.CancellableProgressDialog.ProgressBarMinimum" />
+        /// property, or (c) if the argument of the <paramref name="value" /> parameter is
+        /// greater than the value of the
+        /// <see cref="P:MFR.GUI.Dialogs.CancellableProgressDialog.ProgressBarMaximum" />
+        /// property.
+        /// </remarks>
+        private void DoSetProgressBarValue(int value)
+        {
+            /*
+             * If the setter is trying to set the
+             * progress bar to a value outside the
+             * valid range, ignore it.
+             *
+             * The value for the progress bar must be
+             * between the Minimum and Maximum values,
+             * inclusive.
+             */
+
+            if (value < 0) return;
+            if (value < ProgressBarMinimum) return; // invalid value
+            if (value > ProgressBarMaximum) return; // invalid value
+
+            progressBar.Value = value;
+        }
+
+        /// <summary>
         /// Handles the <see cref="E:System.Windows.Forms.Control.Click" /> event
         /// for the Cancel button.
         /// </summary>
@@ -204,7 +299,11 @@ namespace MFR.GUI.Dialogs
         /// to abort the operation and close down this dialog box from its GUI thread.
         /// </remarks>
         private void OnCancel(object sender, EventArgs e)
-            => OnCancelRequested();
+        {
+            OnCancelRequested();
+            DialogResult = DialogResult.Cancel;
+            this.InvokeIfRequired(Close); // be sure to close this window
+        }
 
         /// <summary>
         /// Directs the application to update the user's display.
