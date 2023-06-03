f766f0c682b0b5ca8907070976757a5e2b63b234
Brian Hart <brian.hart@xylogix.net>
2023-06-03 17:50:56 -0600

    Latest updates 17:50 06/03/2023

--- MFR.Operations.Descriptions.Factories/GetRenameFilesInFolderOperationStartedDescription.cs
+++ MFR.Operations.Descriptions.Factories/GetRenameFilesInFolderOperationStartedDescription.cs
@@ -6,7 +6,9 @@ namespace MFR.Operations.Descriptions.Factories
     /// Provides access to the one and only instance of the object that implements the
     /// <see
     ///     cref="T:MFR.Operations.Descriptions.Interfaces.IOperationStartedDescription" />
-    /// interface.
+    /// interface that explains that the application is beginning the process of
+    /// renaming files in a folder according to the search-and-replace parameters that
+    /// the user has requested.
     /// </summary>
     public static class GetRenameFilesInFolderOperationStartedDescription
     {
@@ -20,7 +22,9 @@ namespace MFR.Operations.Descriptions.Factories
         /// Reference to the one, and only, instance of the object that implements the
         /// <see
         ///     cref="T:MFR.Operations.Descriptions.Interfaces.IOperationStartedDescription" />
-        /// interface.
+        /// interface that explains that the application is beginning the process of
+        /// renaming files in a folder according to the search-and-replace parameters that
+        /// the user has requested.
         /// </returns>
         public static IOperationStartedDescription SoleInstance()
             => RenameFilesInFolderOperationStartedDescription.Instance;
