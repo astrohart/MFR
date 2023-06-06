99013099250ad34b2355b9b56bb84807cca23b7f
Brian Hart <brian.hart@xylogix.net>
2023-06-05 22:42:42 -0600

    Latest updates 22:42 06/05/2023

---
+++ MFR.Engines.Replacement.Factories/MakeNewSolutionFolderNameReplacementEngine.cs
@@ -0,0 +1,30 @@
+﻿using MFR.Engines.Replacement.Intefaces;
+using PostSharp.Patterns.Diagnostics;
+
+namespace MFR.Engines.Replacement.Factories
+{
+    /// <summary>
+    /// Creates new instances of objects that implement the
+    /// <see cref="T:MFR.Engines.Replacement.Intefaces.ITextReplacementEngine" />
+    /// interface that renames solution folders according to the parameters supplied by
+    /// the user.
+    /// them.
+    /// </summary>
+    [Log(AttributeExclude = true)]
+    public static class MakeNewSolutionFolderNameReplacementEngine
+    {
+        /// <summary>
+        /// Creates a new instance of an object that implements the
+        /// <see cref="T:MFR.Engines.Replacement.Intefaces.ITextReplacementEngine" />
+        /// interface and returns a reference to it.
+        /// </summary>
+        /// <returns>
+        /// Reference to an instance of an object that implements the
+        /// <see cref="T:MFR.Engines.Replacement.Intefaces.ITextReplacementEngine" />
+        /// interface that renames solution folders according to the parameters supplied by
+        /// the user.
+        /// </returns>
+        public static ITextReplacementEngine FromScratch()
+            => new SolutionFolderNameReplacementEngine();
+    }
+}
