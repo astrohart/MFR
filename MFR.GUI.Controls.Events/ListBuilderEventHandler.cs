b35bb29b471f276c57d09c2b346b7da67ceb66cd
Brian Hart <brian.hart@xylogix.net>
2023-06-07 10:04:53 -0600

    Update ListBuilderEventHandler.cs

--- MFR.GUI.Controls.Events/ListBuilderEventHandler.cs
+++ MFR.GUI.Controls.Events/ListBuilderEventHandler.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace MFR.GUI.Controls.Events
+﻿namespace MFR.GUI.Controls.Events
 {
     /// <summary>
     /// Handler for an event that occurs with respect to either the left-hand side or
@@ -8,8 +6,12 @@ namespace MFR.GUI.Controls.Events
     /// </summary>
     /// <param name="sender">(Required.) Reference to the object that raised the event.</param>
     /// <param name="e">
-    /// (Required.) A <see cref="T:System.EventArgs" /> that contains the
-    /// event data.
+    /// (Required.) A <see cref="T:MFR.GUI.Controls.Events.ListBuidlerEventArgs" />
+    /// that contains the event data.
+    /// <para />
+    /// This object is utilized to explain to the handler of the event, to which side
+    /// (left or right) of the list-builder control that this event pertains to.
     /// </param>
-    public delegate void ListBuilderEventHandler(object sender, EventArgs e);
+    public delegate void ListBuilderEventHandler(object sender,
+        ListBuidlerEventArgs e);
 }
