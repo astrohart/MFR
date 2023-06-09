84a7a679365398f9fd86cde5874f4587ba1e422a
Brian Hart <brian.hart@xylogix.net>
2023-06-09 15:53:16 -0600

    Latest updates 15:53 06/09/2023

---
+++ MFR.Settings.Configuration.Providers.Events/ConfigurationLoadedEventArgs.cs
@@ -0,0 +1,32 @@
+﻿using PostSharp.Patterns.Diagnostics;
+using System;
+
+namespace MFR.Settings.Configuration.Providers.Events
+{
+    /// <summary>
+    /// Provides information for <c>ConfigurationLoaded</c> event handlers.
+    /// </summary>
+    [Log(AttributeExclude = true)]
+    public class ConfigurationLoadedEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Constructs a new instance of
+        /// <see
+        ///     cref="T:MFR.Settings.Configuration.Providers.Events.ConfigurationLoadedEventArgs" />
+        /// and returns a reference to it.
+        /// </summary>
+        /// <param name="pathname">
+        /// (Required.) A <see cref="T:System.String" /> that contains the fully-qualified
+        /// pathname of the configuration file that was most recently loaded.
+        /// </param>
+        public ConfigurationLoadedEventArgs(string pathname)
+        {
+            Pathname = pathname;
+        }
+
+        public string Pathname
+        {
+            get;
+        }
+    }
+}
---
+++ MFR.Settings.Configuration.Providers.Events/ConfigurationLoadedEventHandler.cs
@@ -0,0 +1,21 @@
+﻿namespace MFR.Settings.Configuration.Providers.Events
+{
+    /// <summary>
+    /// Represents a handler for a <c>ConfigurationLoaded</c> event.
+    /// </summary>
+    /// <param name="sender">
+    /// Reference to the instance of the object that raised the event.
+    /// </param>
+    /// <param name="e">
+    /// A
+    /// <see
+    ///     cref="T:MFR.Settings.Configuration.Providers.Events.ConfigurationLoadedEventArgs" />
+    /// that contains the event data.
+    /// </param>
+    /// <remarks>
+    /// This delegate merely specifies the signature of all methods that handle the
+    /// <c>ConfigurationLoaded</c> event.
+    /// </remarks>
+    public delegate void ConfigurationLoadedEventHandler(object sender,
+        ConfigurationLoadedEventArgs e);
+}
