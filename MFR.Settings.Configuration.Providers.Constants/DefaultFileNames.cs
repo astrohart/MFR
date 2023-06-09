321cdadc09d9080ac3f1456821274a62c79e6995
Brian Hart <brian.hart@xylogix.net>
2023-06-09 16:23:42 -0600

    Latest updates 16:23 06/09/2023

---
+++ MFR.Settings.Configuration.Providers.Constants/DefaultFileNames.cs
@@ -0,0 +1,14 @@
+﻿namespace MFR.Settings.Configuration.Providers.Constants
+{
+    /// <summary>
+    /// Exposes string constants for default filenames.
+    /// </summary>
+    public static class DefaultFileNames
+    {
+        /// <summary>
+        /// A <see cref="T:System.String" /> that contains the default value for the
+        /// filename of the application's configuration file, which is <c>config.json</c>..
+        /// </summary>
+        public const string ConfigurationFile = "config.json";
+    }
+}
