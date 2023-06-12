4b0a92d902cc289df8a8c08ea1436adb1c0fbd07
Brian Hart <brian.hart@xylogix.net>
2023-06-11 22:00:07 -0600

    Latest updates 22:00 06/11/2023

--- MFR.GUI.Constants/ProgramText.cs
+++ MFR.GUI.Constants/ProgramText.cs
@@ -8,10 +8,36 @@ namespace MFR.GUI.Constants
     public static class ProgramText
     {
         /// <summary>
+        /// Gets or sets a <see cref="T:System.String" /> that represents the company name
+        /// that is associated with the application.
+        /// </summary>
+        public static string CompanyName
+        {
+            get;
+            set;
+        } = Application.CompanyName;
+
+        /// <summary>
         /// Gets the title text that should be utilized for the main application window.
         /// </summary>
         public static string MainWindowTitle
             => $"{ShortCompanyName} {ProductNameWithoutCompany}";
+
+        /// <summary>
+        /// Gets or set a <see cref="T:System.String" /> that represents the product name
+        /// that is associated with the application.
+        /// </summary>
+        public static string ProductName
+        {
+            get;
+            set;
+        } = Application.ProductName;
+
+        /// <summary>
+        /// Gets the product name without the company name.
+        /// </summary>
+        private static string ProductNameWithoutCompany
+            => Application.ProductName.Replace(ShortCompanyName, string.Empty);
 
         /// <summary>
         /// Gets the short name (without prefixes or suffixes) of the company
@@ -19,11 +45,5 @@ namespace MFR.GUI.Constants
         /// </summary>
         public static string ShortCompanyName
             => Application.CompanyName.Replace(", LLC", string.Empty);
-
-        /// <summary>
-        /// Gets the product name without the company name.
-        /// </summary>
-        private static string ProductNameWithoutCompany
-            => Application.ProductName.Replace(ShortCompanyName, string.Empty);
     }
 }
