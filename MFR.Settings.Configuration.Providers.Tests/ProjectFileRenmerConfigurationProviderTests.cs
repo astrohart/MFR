9ee278eeabb1337ac2a2d178dbd19a8d2861debe
Brian Hart <brian.hart@xylogix.net>
2023-06-11 22:15:54 -0600

    Latest updates 22:15 06/11/2023

--- MFR.Settings.Configuration.Providers.Tests/ProjectFileRenmerConfigurationProviderTests.cs
+++ MFR.Settings.Configuration.Providers.Tests/ProjectFileRenmerConfigurationProviderTests.cs
@@ -1,3 +1,5 @@
+using MFR.Settings.Configuration.Providers.Factories;
+using MFR.Settings.Configuration.Providers.Interfaces;
 using NUnit.Framework;
 using System;
 
@@ -5,12 +7,12 @@ namespace MFR.Settings.Configuration.Providers.Tests
 {
     /// <summary>
     /// Provides unit tests for the methods, properties, and events of the
-    /// <see cref="T:MFR.Settings.Configuration.Providers.ProjectFileRenmerConfigurationProvider" />
+    /// <see
+    ///     cref="T:MFR.Settings.Configuration.Providers.ProjectFileRenmerConfigurationProvider" />
     /// class.
     /// </summary>
     [TestFixture]
-    public class
-        ProjectFileRenmerConfigurationProviderTests
+    public class ProjectFileRenmerConfigurationProviderTests
     {
         /// <summary>
         /// Path to a sample configuration file.
@@ -19,16 +21,36 @@ namespace MFR.Settings.Configuration.Providers.Tests
             @"C:\Users\Brian Hart\AppData\Local\xyLOGIX, LLC\Project File Renamer\Config\config.json";
 
         /// <summary>
-        /// TODO: Add unit test documentation here
+        /// Gets a reference to an instance of an object that implements the
+        /// <see
+        ///     cref="T:MFR.Settings.Configuration.Providers.Interfaces.IProjectFileRenamerConfigurationProvider" />
+        /// interface.
+        /// </summary>
+        private static IProjectFileRenamerConfigurationProvider
+            ProjectFileRenamerConfigurationProvider
+        {
+            get;
+        } = GetProjectFileRenamerConfigurationProvider.SoleInstance();
+
+        /// <summary>
+        /// Asserts that the
+        /// <see
+        ///     cref="M:MFR.Settings.Configuration.Providers.Interfaces.IProjectFileRenamerConfigurationProvider.Load" />
+        /// method works correctly.
         /// </summary>
         [Test]
         public void Test_Load_Works()
         {
             Assert.DoesNotThrow(
-                () => ProjectFileRenmerConfigurationProvider.Instance.Load(ConfigFilePath)
+                () => ProjectFileRenamerConfigurationProvider.Load(
+                    ConfigFilePath
+                )
             );
 
-            Console.WriteLine(ProjectFileRenmerConfigurationProvider.Instance.CurrentConfiguration.FindWhat);
+            Console.WriteLine(
+                ProjectFileRenamerConfigurationProvider.CurrentConfiguration
+                    .FindWhat
+            );
         }
     }
 }
