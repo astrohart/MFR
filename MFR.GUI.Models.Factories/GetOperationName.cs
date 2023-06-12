7f0f4d5b3f78b436facab621360fcf5ed56d26b6
Brian Hart <brian.hart@xylogix.net>
2023-06-11 21:58:14 -0600

    Latest updates 21:58 06/11/2023

--- MFR.GUI.Models.Factories/GetOperationName.cs
+++ MFR.GUI.Models.Factories/GetOperationName.cs
@@ -3,13 +3,32 @@ using MFR.Operations.Constants;
 
 namespace MFR.GUI.Models.Factories
 {
+    /// <summary>
+    /// Exposes methods to obtain the names of operations.
+    /// </summary>
     public static class GetOperationName
     {
         /// <summary>
-        /// Given the specified operation 
+        /// Given the specified operation <paramref name="type" />, retrieves a
+        /// <see cref="T:System.String" /> that contains the corresponding operation name.
+        /// <para />
+        /// This <see cref="T:System.String" /> should be used for displaying information
+        /// about the operation in UI/UX elements.
         /// </summary>
-        /// <param name="type"></param>
-        /// <returns></returns>
+        /// <param name="type">
+        /// (Required.) One of the
+        /// <see cref="T:MFR.Operations.Constants.OperationType" /> enumeration values that
+        /// specifies the operation for which to obtain the corresponding user-interface
+        /// text.
+        /// </param>
+        /// <returns>
+        /// If the specified
+        /// <see cref="T:MFR.Operations.Constants.OperationType" /> enumeration value is
+        /// supported, a <see cref="T:System.String" /> that corresponds to the operation
+        /// for use in displaying information about the operation in the user interface; if
+        /// the specified operation <paramref name="type" />  is not supported, then the
+        /// <see cref="F:System.String.Empty" /> value will be returned.
+        /// </returns>
         public static string For(OperationType type)
         {
             var result = string.Empty;
