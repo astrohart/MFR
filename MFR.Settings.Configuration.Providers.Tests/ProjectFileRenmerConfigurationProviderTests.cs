285238238c7257fc64967dadb04c80685bd6fb82
Brian Hart <brian.hart@xylogix.net>
2023-06-11 22:21:33 -0600

    Latest updates 22:21 06/11/2023

--- MFR.Settings.Configuration.Providers.Tests/ProjectFileRenmerConfigurationProviderTests.cs
+++ MFR.Settings.Configuration.Providers.Tests/ProjectFileRenmerConfigurationProviderTests.cs
@@ -1,7 +1,10 @@
+using Alphaleonis.Win32.Filesystem;
 using MFR.Settings.Configuration.Providers.Factories;
 using MFR.Settings.Configuration.Providers.Interfaces;
 using NUnit.Framework;
 using System;
+using xyLOGIX.Core.Assemblies.Info;
+using xyLOGIX.Core.Debug;
 
 namespace MFR.Settings.Configuration.Providers.Tests
 {
@@ -51,6 +54,96 @@ namespace MFR.Settings.Configuration.Providers.Tests
                 ProjectFileRenamerConfigurationProvider.CurrentConfiguration
                     .FindWhat
             );
+        }
+
+        /// <summary>
+        /// Exposes static methods to obtain data from various data sources.
+        /// </summary>
+        private static class Get
+        {
+            /// <summary>
+            /// A <see cref="T:System.String" /> containing the final piece of the path of the
+            /// log file.
+            /// </summary>
+            private static readonly string LOG_FILE_PATH_TERMINATOR =
+                $@"{AssemblyCompany}\{AssemblyProduct}\Logs\{AssemblyTitle}_log.txt";
+
+            /// <summary>
+            /// Gets a <see cref="T:System.String" /> that contains the product name defined
+            /// for this application.
+            /// </summary>
+            /// <remarks>
+            /// This property is really an alias for the
+            /// <see cref="P:AssemblyMetadata.AssemblyCompany" /> property.
+            /// </remarks>
+            private static string AssemblyCompany
+                => AssemblyMetadata.AssemblyCompany;
+
+            /// <summary>
+            /// Gets a <see cref="T:System.String" /> that contains the product name defined
+            /// for this application.
+            /// </summary>
+            /// <remarks>
+            /// This property is really an alias for the
+            /// <see cref="P:AssemblyMetadata.AssemblyProduct" /> property.
+            /// </remarks>
+            private static string AssemblyProduct
+                => AssemblyMetadata.AssemblyProduct.Replace(
+                    "xyLOGIX ", string.Empty
+                );
+
+            /// <summary>
+            /// Gets a <see cref="T:System.String" /> that contains the assembly title defined
+            /// for this application.
+            /// </summary>
+            /// <remarks>
+            /// This property is really an alias for the
+            /// <see cref="P:AssemblyMetadata.AssemblyTitle" /> property.
+            /// </remarks>
+            private static string AssemblyTitle
+                => AssemblyMetadata.AssemblyTitle.Replace(" ", "_");
+
+            /// <summary>
+            /// Gets a <see cref="T:System.String" /> that contains a user-friendly name for
+            /// the software product of which this application or class library is a part.
+            /// </summary>
+            /// <returns>
+            /// A <see cref="T:System.String" /> that contains a user-friendly name
+            /// for the software product of which this application or class library is a part.
+            /// </returns>
+            public static string ApplicationProductName()
+            {
+                string result;
+
+                try
+                {
+                    result = AssemblyProduct;
+                }
+                catch (Exception ex)
+                {
+                    // dump all the exception info to the log
+                    DebugUtils.LogException(ex);
+
+                    result = string.Empty;
+                }
+
+                return result;
+            }
+
+            /// <summary>
+            /// Obtains a <see cref="T:System.String" /> that contains the fully-qualified
+            /// pathname of the file that should be used for logging messages.
+            /// </summary>
+            /// <returns>
+            /// A <see cref="T:System.String" /> that contains the fully-qualified
+            /// pathname of the file that should be used for logging messages.
+            /// </returns>
+            public static string LogFilePath()
+                => Path.Combine(
+                    Environment.GetFolderPath(
+                        Environment.SpecialFolder.CommonApplicationData
+                    ), LOG_FILE_PATH_TERMINATOR
+                );
         }
     }
 }
