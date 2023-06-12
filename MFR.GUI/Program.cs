1770b867b51f71f0db543b5cd1929723ea538a4e
Brian Hart <brian.hart@xylogix.net>
2023-06-12 09:21:58 -0600

    Moved the SetUpLogging method
    * Moved the method from the ProjectFileRenamerApp class to the Program class and used the new PostSharp infrastrucure code.

--- MFR.GUI/Program.cs
+++ MFR.GUI/Program.cs
@@ -1,6 +1,9 @@
+using Alphaleonis.Win32.Filesystem;
 using MFR.GUI.Application.Factories;
 using MFR.GUI.Application.Interfaces;
 using System;
+using xyLOGIX.Core.Assemblies.Info;
+using xyLOGIX.Core.Debug;
 
 namespace MFR.GUI
 {
@@ -15,9 +18,7 @@ namespace MFR.GUI
         /// <see cref="T:MFR.GUI.Application.Interfaces.IWinApp" /> interface.
         /// </summary>
         private static IWinApp Application
-        {
-            get;
-        } = GetProjectFileRenamerApp.SoleInstance();
+            => GetProjectFileRenamerApp.SoleInstance();
 
         /// <summary>
         /// The main entry point for the application.
@@ -28,6 +29,111 @@ namespace MFR.GUI
         /// </param>
         [STAThread]
         public static void Main(string[] args)
-            => Application.WinInit(args);
+        {
+            SetUpLogging();
+
+            Application.WinInit(args);
+        }
+
+        /// <summary>
+        /// Configures the logging infrastructure.
+        /// </summary>
+        private static void SetUpLogging()
+            => LogFileManager.InitializeLogging(
+                true, muteConsole: false,
+                infrastructureType: LoggingInfrastructureType.PostSharp,
+                logFileName: Get.LogFilePath(),
+                applicationName: Get.ApplicationProductName()
+            );
+
+        /// <summary>
+        /// Exposes static methods to obtain data from various data sources.
+        /// </summary>
+        private static class Get
+        {
+            /// <summary>
+            /// A <see cref="T:System.String" /> containing the final piece of the path of the
+            /// log file.
+            /// </summary>
+            private static readonly string LOG_FILE_PATH_TERMINATOR =
+                $@"{AssemblyCompany}\{AssemblyProduct}\Logs\{AssemblyTitle}_log.txt";
+
+            /// <summary>
+            /// Gets a <see cref="T:System.String" /> that contains the product name defined
+            /// for this application.
+            /// </summary>
+            /// <remarks>
+            /// This property is really an alias for the
+            /// <see cref="P:AssemblyMetadata.AssemblyCompany" /> property.
+            /// </remarks>
+            private static string AssemblyCompany
+                => AssemblyMetadata.AssemblyCompany;
+
+            /// <summary>
+            /// Gets a <see cref="T:System.String" /> that contains the product name defined
+            /// for this application.
+            /// </summary>
+            /// <remarks>
+            /// This property is really an alias for the
+            /// <see cref="P:AssemblyMetadata.AssemblyProduct" /> property.
+            /// </remarks>
+            private static string AssemblyProduct
+                => AssemblyMetadata.AssemblyProduct.Replace(
+                    "xyLOGIX ", string.Empty
+                );
+
+            /// <summary>
+            /// Gets a <see cref="T:System.String" /> that contains the assembly title defined
+            /// for this application.
+            /// </summary>
+            /// <remarks>
+            /// This property is really an alias for the
+            /// <see cref="P:AssemblyMetadata.AssemblyTitle" /> property.
+            /// </remarks>
+            private static string AssemblyTitle
+                => AssemblyMetadata.AssemblyTitle.Replace(" ", "_");
+
+            /// <summary>
+            /// Gets a <see cref="T:System.String" /> that contains a user-friendly name for
+            /// the software product of which this application or class library is a part.
+            /// </summary>
+            /// <returns>
+            /// A <see cref="T:System.String" /> that contains a user-friendly name
+            /// for the software product of which this application or class library is a part.
+            /// </returns>
+            public static string ApplicationProductName()
+            {
+                string result;
+
+                try
+                {
+                    result = AssemblyProduct;
+                }
+                catch (Exception ex)
+                {
+                    // dump all the exception info to the log
+                    DebugUtils.LogException(ex);
+
+                    result = string.Empty;
+                }
+
+                return result;
+            }
+
+            /// <summary>
+            /// Obtains a <see cref="T:System.String" /> that contains the fully-qualified
+            /// pathname of the file that should be used for logging messages.
+            /// </summary>
+            /// <returns>
+            /// A <see cref="T:System.String" /> that contains the fully-qualified
+            /// pathname of the file that should be used for logging messages.
+            /// </returns>
+            public static string LogFilePath()
+                => Path.Combine(
+                    Environment.GetFolderPath(
+                        Environment.SpecialFolder.CommonApplicationData
+                    ), LOG_FILE_PATH_TERMINATOR
+                );
+        }
     }
 }
