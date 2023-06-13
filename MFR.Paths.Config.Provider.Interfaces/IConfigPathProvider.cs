38921c629bf574f1af79286f68475d9bbf9b970e
Brian Hart <brian.hart@xylogix.net>
2023-06-13 16:45:18 -0600

    Latest updates 16:45 06/13/2023

--- MFR.Paths.Config.Provider.Interfaces/IConfigPathProvider.cs
+++ MFR.Paths.Config.Provider.Interfaces/IConfigPathProvider.cs
@@ -2,26 +2,24 @@
 using System;
 using System.ComponentModel;
 
-namespace MFR.Paths.Profiles.Provider.Interfaces
+namespace MFR.Paths.Config.Provider.Interfaces
 {
     /// <summary>
     /// Defines the publicly-exposed methods and properties of an object that provides
-    /// access to the fully-qualified pathname of the list of the user's favorite
-    /// configuration-setting profiles.
+    /// access to the fully-qualified pathname of the application configuration file.
     /// </summary>
-    public interface IProfileListPathProvider
+    public interface IConfigPathProvider
     {
         /// <summary>
-        /// Gets a string whose value is the fully-qualified pathname of the profile list
-        /// file.
+        /// Gets a string whose value is the fully-qualified pathname of the application configuration file.
         /// </summary>
         /// <remarks>
         /// When this property's value is updated, the
         /// <see
-        ///     cref="E:MFR.Paths.Profiles.Provider.Interfaces.IProfileListPathProvider.ProfileCollectionFilePathChanged" />
+        ///     cref="E:MFR.Paths.Config.Provider.Interfaces.IConfigPathProvider.ConfigFilePathChanged" />
         /// event  is raised.
         /// </remarks>
-        string ProfileCollectionFilePath
+        string ConfigFilePath
         {
             get;
             set;
@@ -30,69 +28,69 @@ namespace MFR.Paths.Profiles.Provider.Interfaces
         /// <summary>
         /// Occurs when the process of loading the value of the
         /// <see
-        ///     cref="P:MFR.Paths.Profiles.Provider.Interfaces.IProfileListPathProvider.ProfileCollectionFilePath" />
+        ///     cref="P:MFR.Paths.Config.Provider.Interfaces.IConfigPathProvider.ConfigFilePath" />
         /// property from the system Registry is about to begin.
         /// </summary>
-        event CancelEventHandler LoadingProfileCollectionFilePath;
+        event CancelEventHandler LoadingConfigFilePath;
 
         /// <summary>
         /// Occurs when the value of the
         /// <see
-        ///     cref="P:MFR.Paths.Profiles.Provider.Interfaces.IProfileListPathProvider.ProfileCollectionFilePath" />
+        ///     cref="P:MFR.Paths.Config.Provider.Interfaces.IConfigPathProvider.ConfigFilePath" />
         /// property is updated.
         /// </summary>
-        event EventHandler ProfileCollectionFilePathChanged;
+        event EventHandler ConfigFilePathChanged;
 
         /// <summary>
         /// Occurs when the value of the
         /// <see
-        ///     cref="P:MFR.Paths.Profiles.Provider.Interfaces.IProfileListPathProvider.ProfileCollectionFilePath" />
+        ///     cref="P:MFR.Paths.Config.Provider.Interfaces.IConfigPathProvider.ConfigFilePath" />
         /// property has been loaded from the system Registry.
         /// </summary>
-        event EventHandler ProfileCollectionFilePathLoaded;
+        event EventHandler ConfigFilePathLoaded;
 
         /// <summary>
         /// Occurs when the process of loading the value of the
         /// <see
-        ///     cref="P:MFR.Paths.Profiles.Provider.Interfaces.IProfileListPathProvider.ProfileCollectionFilePath" />
+        ///     cref="P:MFR.Paths.Config.Provider.Interfaces.IConfigPathProvider.ConfigFilePath" />
         /// property from the system Registry has failed.
         /// </summary>
-        event ExceptionRaisedEventHandler ProfileCollectionFilePathLoadFailed;
+        event ExceptionRaisedEventHandler ConfigFilePathLoadFailed;
 
         /// <summary>
         /// Occurs when the value of the
         /// <see
-        ///     cref="P:MFR.Paths.Profiles.Provider.Interfaces.IProfileListPathProvider.ProfileCollectionFilePath" />
+        ///     cref="P:MFR.Paths.Config.Provider.Interfaces.IConfigPathProvider.ConfigFilePath" />
         /// property has been saved to the system Registry.
         /// </summary>
-        event EventHandler ProfileCollectionFilePathSaved;
+        event EventHandler ConfigFilePathSaved;
 
         /// <summary>
         /// Occurs when the process of saving the value of the
         /// <see
-        ///     cref="P:MFR.Paths.Profiles.Provider.Interfaces.IProfileListPathProvider.ProfileCollectionFilePath" />
+        ///     cref="P:MFR.Paths.Config.Provider.Interfaces.IConfigPathProvider.ConfigFilePath" />
         /// property to the system Registry has failed.
         /// </summary>
-        event ExceptionRaisedEventHandler ProfileCollectionFilePathSaveFailed;
+        event ExceptionRaisedEventHandler ConfigFilePathSaveFailed;
 
         /// <summary>
         /// Occurs when the process of saving the value of the
         /// <see
-        ///     cref="P:MFR.Paths.Profiles.Provider.Interfaces.IProfileListPathProvider.ProfileCollectionFilePath" />
+        ///     cref="P:MFR.Paths.Config.Provider.Interfaces.IConfigPathProvider.ConfigFilePath" />
         /// property to the system Registry is about to begin.
         /// </summary>
-        event CancelEventHandler SavingProfileCollectionFilePath;
+        event CancelEventHandler SavingConfigFilePath;
 
         /// <summary>
         /// Loads the value of the
         /// <see
-        ///     cref="P:MFR.Paths.Profiles.Provider.Interfaces.IProfileListPathProvider.ProfileCollectionFilePath" />
+        ///     cref="P:MFR.Paths.Config.Provider.Interfaces.IConfigPathProvider.ConfigFilePath" />
         /// property from the system Registry.
         /// </summary>
         /// <remarks>
         /// The
         /// <see
-        ///     cref="E:MFR.Paths.Profiles.Provider.Interfaces.IProfileListPathProvider.LoadingProfileCollectionFilePath" />
+        ///     cref="E:MFR.Paths.Config.Provider.Interfaces.IConfigPathProvider.LoadingConfigFilePath" />
         /// event is raised before this method begins its operations.  Setting the
         /// <see cref="P:System.ComponentModel.CancelEventArgs.Cancel" /> property to
         /// <see langword="true" /> on this event will prevent this method from executing
@@ -100,16 +98,16 @@ namespace MFR.Paths.Profiles.Provider.Interfaces
         /// <para />
         /// If the operation of obtaining the value of the
         /// <see
-        ///     cref="P:MFR.Paths.Profiles.Provider.Interfaces.IProfileListPathProvider.ProfileCollectionFilePath" />
+        ///     cref="P:MFR.Paths.Config.Provider.Interfaces.IConfigPathProvider.ConfigFilePath" />
         /// property from the system Registry was successful, then the
         /// <see
-        ///     cref="E:MFR.Paths.Profiles.Provider.Interfaces.IProfileListPathProvider.ProfileCollectionFilePathLoaded" />
+        ///     cref="E:MFR.Paths.Config.Provider.Interfaces.IConfigPathProvider.ConfigFilePathLoaded" />
         /// event is raised.
         /// <para />
         /// Otherwise, if a <see cref="T:System.Exception" /> is raised during the
         /// processing of the operation, then this method raises the
         /// <see
-        ///     cref="E:MFR.Paths.Profiles.Provider.Interfaces.IProfileListPathProvider.ProfileCollectionFilePathLoadFailed" />
+        ///     cref="E:MFR.Paths.Config.Provider.Interfaces.IConfigPathProvider.ConfigFilePathLoadFailed" />
         /// event.
         /// </remarks>
         void Load();
@@ -117,13 +115,13 @@ namespace MFR.Paths.Profiles.Provider.Interfaces
         /// <summary>
         /// Saves the value of the
         /// <see
-        ///     cref="P:MFR.Paths.Profiles.Provider.Interfaces.IProfileListPathProvider.ProfileCollectionFilePath" />
+        ///     cref="P:MFR.Paths.Config.Provider.Interfaces.IConfigPathProvider.ConfigFilePath" />
         /// property to the system Registry.
         /// </summary>
         /// <remarks>
         /// The
         /// <see
-        ///     cref="E:MFR.Paths.Profiles.Provider.Interfaces.IProfileListPathProvider.SavingProfileCollectionFilePath" />
+        ///     cref="E:MFR.Paths.Config.Provider.Interfaces.IConfigPathProvider.SavingConfigFilePath" />
         /// event is raised before this method begins its operations.  Setting the
         /// <see cref="P:System.ComponentModel.CancelEventArgs.Cancel" /> property to
         /// <see langword="true" /> on this event will prevent this method from executing
@@ -131,16 +129,16 @@ namespace MFR.Paths.Profiles.Provider.Interfaces
         /// <para />
         /// If the operation of storing the value of the
         /// <see
-        ///     cref="P:MFR.Paths.Profiles.Provider.Interfaces.IProfileListPathProvider.ProfileCollectionFilePath" />
+        ///     cref="P:MFR.Paths.Config.Provider.Interfaces.IConfigPathProvider.ConfigFilePath" />
         /// property to the system Registry was successful, then the
         /// <see
-        ///     cref="E:MFR.Paths.Profiles.Provider.Interfaces.IProfileListPathProvider.ProfileCollectionFilePathSaved" />
+        ///     cref="E:MFR.Paths.Config.Provider.Interfaces.IConfigPathProvider.ConfigFilePathSaved" />
         /// event is raised.
         /// <para />
         /// Otherwise, if a <see cref="T:System.Exception" /> is raised during the
         /// processing of the operation, then this method raises the
         /// <see
-        ///     cref="E:MFR.Paths.Profiles.Provider.Interfaces.IProfileListPathProvider.ProfileCollectionFilePathSaveFailed" />
+        ///     cref="E:MFR.Paths.Config.Provider.Interfaces.IConfigPathProvider.ConfigFilePathSaveFailed" />
         /// event.
         /// </remarks>
         void Save();
