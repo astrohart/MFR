cd3a807060947bd9f1d1a299633105b887e2b902
Brian Hart <brian.hart@xylogix.net>
2023-06-13 08:38:50 -0600

    Create the 'MFR.Paths.Profiles.Provider' module
    Provides objects and functionality to load and store the pathname of the profile list file to/from the system Registry.
    
    This module is a component of the xyLOGIX Project File Renamer software system.
    
    ---
    Copyright © 2023 by xyLOGIX, LLC.  All rights reserved.

---
+++ MFR.Paths.Profiles.Provider.Events/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("xyLOGIX Events Support Library for Project File Renamer's Providing Access to Pathname of the Profile List File Component")]
+[assembly: AssemblyDescription("Provides objects and functionality to load and store the pathname of the profile list file to/from the system Registry.  This particular library holds the C# event handler delegates and event argument types for this component.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("xyLOGIX, LLC")]
+[assembly: AssemblyProduct("xyLOGIX Support Library for Project File Renamer")]
+[assembly:
+    AssemblyCopyright(
+        "Copyright © 2023 by xyLOGIX, LLC.  All rights reserved.")]
+[assembly:
+    AssemblyTrademark(
+        "This software is a component of xyLOGIX Project File Renamer.  xyLOGIX Project File Renamer is a trademark of xyLOGIX, LLC.  All rights reserved, especially those granted by international treaties.")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7e37e6cc-fc5b-4879-aecd-941255c4503c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.*")]
