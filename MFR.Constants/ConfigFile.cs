de821d64ce6158ea61ecbc65fad04db8fb98331d
Brian Hart <brian.hart@xylogix.net>
2023-06-14 09:27:49 -0600

    Latest updates 09:27 06/14/2023

--- MFR.Constants/ConfigFile.cs
+++ MFR.Constants/ConfigFile.cs
@@ -2,15 +2,14 @@
 {
     /// <summary>
     /// String constants for the default names of files and folders relating to the
-    /// user's saved configuration profiles.
+    /// application configuration.
     /// </summary>
-    public static class ProfileFile
+    public static class ConfigFile
     {
         /// <summary>
         /// A <see cref="T:System.String" /> that contains the default filename to be used
-        /// for the file containing the saved collection of the user's previously-saved
-        /// configuration setting profiles, <c>profiles.json</c>.
+        /// for the file containing the application configuration, <c>config.json</c>.
         /// </summary>
-        public const string DefaultFilename = "profiles.json";
+        public const string DefaultFilename = "config.json";
     }
 }
