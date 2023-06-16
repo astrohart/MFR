69a248d4ac71286a9096e10e91569c719340dbf8
Brian Hart <brian.hart@xylogix.net>
2023-06-15 23:33:11 -0600

    Latest updates 23:33 06/15/2023

---
+++ MFR.Metadata.Registry.Validators.Factories/GetRegistryOperationMetadataValidator.cs
@@ -0,0 +1,32 @@
+using MFR.Metadata.Registry.Validators.Interfaces;
+
+namespace MFR.Metadata.Registry.Validators.Factories
+{
+    /// <summary>
+    /// Accesses the sole instance of an object that implements the
+    /// <see
+    ///     cref="T:MFR.Metadata.Registry.Validators.Interfaces.IRegOperationMetadataValidator{T}" />
+    /// interface.
+    /// </summary>
+    /// <typeparam name="T">
+    /// Name of the type of the data that is to be retrieved from the system Registry.
+    /// </typeparam>
+    public static class GetRegistryOperationMetadataValidator<T> where T : class
+    {
+        /// <summary>
+        /// Fluently retrieves the single instance of the object that implements
+        /// the
+        /// <see
+        ///     cref="T:MFR.Metadata.Registry.Validators.Interfaces.IRegOperationMetadataValidator{T}" />
+        /// interface.
+        /// </summary>
+        /// <returns>
+        /// Reference to an instance of an object that implements the
+        /// <see
+        ///     cref="T:MFR.Metadata.Registry.Validators.Interfaces.IRegOperationMetadataValidator{T}" />
+        /// interface.
+        /// </returns>
+        public static IRegOperationMetadataValidator<T> SoleInstance()
+            => RegOperationMetadataValidator<T>.Instance;
+    }
+}
