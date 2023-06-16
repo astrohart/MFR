256713259413ab94bde2ec6d35b2fb3d2d0cd2dc
Brian Hart <brian.hart@xylogix.net>
2023-06-15 22:26:15 -0600

    Create the 'MFR.Metadata.Registry.Validators' module
    Provides objects and functionality to validate the metadaa object isntances that are used to perform operations on the system Registry, such as saving data to it.
    
    This module is a component of the xyLOGIX Project File Renamer software system.
    
    ---
    Copyright © 2023 by xyLOGIX, LLC.  All rights reserved.

---
+++ MFR.Metadata.Registry.Validators.Interfaces/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("xyLOGIX Interfaces Support Library for Project File Renamer's Validators of Metadata for Registry Operations Component")]
+[assembly: AssemblyDescription("Provides objects and functionality to validate the metadaa object isntances that are used to perform operations on the system Registry, such as saving data to it.  This particular library holds the C# interfaces for this component.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("xyLOGIX, LLC")]
+[assembly: AssemblyProduct("xyLOGIX Support Library for Project File Renamer")]
+[assembly:
+    AssemblyCopyright(
+        "Copyright © 2023 by xyLOGIX, LLC.  All rights reserved.")]
+[assembly:
+    AssemblyTrademark(
+        "This software is a component of xyLOGIX Project File Renamer.  xyLOGIX Project File Renamer is a trademark of xyLOGIX, LLC.  All rights reserved, especially those granted by international treaties.")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("02f205af-3926-4a6d-85d7-a4d5b54cba4e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.*")]
