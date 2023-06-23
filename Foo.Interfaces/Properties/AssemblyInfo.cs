07c202a5e897b178fd304ffa0b357b8deb4ba64f
Brian Hart <brian.hart@xylogix.net>
2023-06-23 15:45:14 -0600

    Create the 'MFR.TextValues.Retrievers.Actions' module
    Provides a framework for the ingestion of text from files when we are doing a replace-in-files operation.
    
    This module is a component of the xyLOGIX Project File Renamer software system.
    
    ---
    Copyright © 2023 by xyLOGIX, LLC.  All rights reserved.

---
+++ Foo.Interfaces/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("xyLOGIX Interfaces Support Library for Project File Renamer's Actions for Text-Value Retrievers Component")]
+[assembly: AssemblyDescription("Provides a framework for the ingestion of text from files when we are doing a replace-in-files operation.  This particular library holds the C# interfaces for this component.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("xyLOGIX, LLC")]
+[assembly: AssemblyProduct("xyLOGIX Support Library for Project File Renamer")]
+[assembly:
+    AssemblyCopyright(
+        "Copyright © 2023 by xyLOGIX, LLC.  All rights reserved.")]
+[assembly:
+    AssemblyTrademark(
+        "This software is a component of xyLOGIX Project File Renamer.  xyLOGIX Project File Renamer is a trademark of xyLOGIX, LLC.  All rights reserved, especially those granted by international treaties.")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1bb1b75f-e35b-41c8-ba30-1e5f90e3d1b0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.*")]
