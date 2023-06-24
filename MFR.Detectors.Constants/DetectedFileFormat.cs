d1a4f7977cc4cea1720b08e69d217b0f83445f74
Brian Hart <brian.hart@xylogix.net>
2023-06-24 09:48:35 -0600

    Update DetectedFileFormat.cs

--- MFR.Detectors.Constants/DetectedFileFormat.cs
+++ MFR.Detectors.Constants/DetectedFileFormat.cs
@@ -20,7 +20,11 @@
         /// We were unable to determine the format of the file.
         /// </summary>
         /// <remarks>
-        /// When a file-format detection yields a value of 
+        /// When a file-format detection yields a value of
+        /// <see cref="F:MFR.Detectors.Constants.DetectedFileFormat.Unknown" />, the file
+        /// should be assumed by calling code to have a Binary file format.
+        /// <para/>
+        /// How this value is handled is application-specific.
         /// </remarks>
         Unknown = -1
     }
