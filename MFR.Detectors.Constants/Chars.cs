e9351f5a209b72c5bbaaa647374985f9f67e3945
Brian Hart <brian.hart@xylogix.net>
2023-06-24 10:06:52 -0600

    Latest updates 10:06 06/24/2023

---
+++ MFR.Detectors.Constants/Chars.cs
@@ -0,0 +1,32 @@
+﻿namespace MFR.Detectors.Constants
+{
+    /// <summary>
+    /// Values for special ASCII characters.
+    /// </summary>
+    public static class Chars
+    {
+        /// <summary>
+        /// The <c>BS</c> character (backspace).
+        /// </summary>
+        public static char BS = (char)8;
+
+        /// <summary>
+        /// The <c>CR</c> character (carriage return).
+        /// </summary>
+        public static char CR = (char)13;
+
+        /// <summary>
+        /// The <c>NUL</c> character (null).
+        /// </summary>
+        public static char NUL = (char)0; // Null char
+
+        // Back Space
+
+        // Carriage Return
+
+        /// <summary>
+        /// The <c>SUB</c> character (substitute).
+        /// </summary>
+        public static char SUB = (char)26; // Substitute
+    }
+}
