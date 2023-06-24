1a5e57ae4202aeada16cc6f3aa5fd7a4ae5f4288
Brian Hart <brian.hart@xylogix.net>
2023-06-24 09:43:34 -0600

    Latest updates 09:43 06/24/2023

---
+++ MFR.Detectors.Constants/DetectedFileFormat.cs
@@ -0,0 +1,24 @@
+﻿namespace MFR.Detectors.Constants
+{
+    /// <summary>
+    /// Values that specify the format of a file that we have ascertained from looking
+    /// at some of the bytes of the file.
+    /// </summary>
+    public enum DetectedFileFormat
+    {
+        /// <summary>
+        /// The file seems to be ASCII format.
+        /// </summary>
+        ASCII,
+
+        /// <summary>
+        /// The file has been detected to be a binary format.
+        /// </summary>
+        Binary,
+
+        /// <summary>
+        /// We were unable to determine the type of the file.
+        /// </summary>
+        Unknown = -1
+    }
+}
