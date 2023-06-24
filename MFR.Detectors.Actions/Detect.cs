76e0b1cff8e9ff4c53422c0daa84cfc673422303
Brian Hart <brian.hart@xylogix.net>
2023-06-24 10:01:39 -0600

    Latest updates 10:01 06/24/2023

---
+++ MFR.Detectors.Actions/Detect.cs
@@ -0,0 +1,39 @@
+﻿using MFR.Detectors.Constants;
+using MFR.FileSystem.Factories.Actions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using xyLOGIX.Core.Debug;
+
+namespace MFR.Detectors.Actions
+{
+    /// <summary>
+    /// Exposes static methods to do detection on files.
+    /// </summary>
+    public static class Detect
+    {
+        public static DetectedFileFormat FormatOfFile(string pathname)
+        {
+            var result = DetectedFileFormat.Unknown;
+
+            try
+            {
+                if (string.IsNullOrWhiteSpace(pathname)) return result;
+                if (!Does.FileExist(pathname)) return result;
+
+
+            }
+            catch (Exception ex)
+            {
+                // dump all the exception info to the log
+                DebugUtils.LogException(ex);
+
+                result = DetectedFileFormat.Unknown;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ MFR.Detectors.Models/FileFormatDetectionResult.cs
@@ -0,0 +1,45 @@
+﻿using MFR.Detectors.Constants;
+
+namespace MFR.Detectors.Models
+{
+    /// <summary>
+    /// Encapsulates the result of running detection on the format of a particular file
+    /// on the disk.
+    /// </summary>
+    public class FileFormatDetectionResult
+    {
+        /// <summary>
+        /// Gets or sets one of the
+        /// <see cref="T:MFR.Detectors.Constants.DetectedFileFormat" /> enumeration values
+        /// that indicates the detected format of the file.
+        /// </summary>
+        public DetectedFileFormat FileFormat
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether the file is a binary file, ASCII text file, or
+        /// an unknown format.
+        /// </summary>
+        public bool IsBinary
+            => FileFormat == DetectedFileFormat.Binary ||
+               FileFormat == DetectedFileFormat.Unknown;
+
+        /// <summary>
+        /// Gets or sets a <see cref="T:System.String" /> that contains the fully-qualified
+        /// pathname of a file on the disk.
+        /// </summary>
+        public string Pathname
+        {
+            get;
+            set;
+        }
+
+        /// <summary>Returns a string that represents the current object.</summary>
+        /// <returns>A string that represents the current object.</returns>
+        public override string ToString()
+            => $"File path: '{Pathname}', Format: '{FileFormat}'";
+    }
+}
