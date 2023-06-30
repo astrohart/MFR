625c79fada55e394a8e0fd3d39f15b31f63c775e
Brian Hart <brian.hart@xylogix.net>
2023-06-30 11:32:12 -0600

    Update Wait.cs

--- MFR.FileSystem.Factories.Actions/Wait.cs
+++ MFR.FileSystem.Factories.Actions/Wait.cs
@@ -1,22 +1,29 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using xyLOGIX.Core.Debug;
 
 namespace MFR.FileSystem.Factories.Actions
 {
+    /// <summary>
+    /// Exposes static methods to hold up components of a software system while we wait
+    /// for a certain action to be taken elsewhere, or for files and folders to become
+    /// accessible.
+    /// </summary>
     public static class Wait
     {
+        /// <summary>
+        /// Blocks the calling thread until the
+        /// <see cref="M:MFR.FileSystem.Factories.Actions.Is.FolderWriteable" /> method
+        /// returns <see langword="true" /> for the folder having the specified
+        /// <paramref name="pathname" />.
+        /// </summary>
+        /// <param name="pathname">
+        /// (Required.) A <see cref="T:System.String" /> containing
+        /// the fully-qualified pathname of the folder to be examined.
+        /// </param>
         public static async void ForFolderToBecomeWriteable(string pathname)
         {
             try
             {
-                while (!Is.FolderWriteable(pathname))
-                {
-                    await Task.Delay(500);
-                }
+                while (!Is.FolderWriteable(pathname)) await Task.Delay(500);
             }
             catch
             {
