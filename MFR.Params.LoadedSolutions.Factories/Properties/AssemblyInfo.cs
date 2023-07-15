deb041eb6b1debc808f2c9c1e8a1318849d8f0e3
Brian Hart <brian.hart@xylogix.net>
2023-07-15 07:04:59 -0600

    Create the 'MFR.Params.LoadedSolutions' module
    Provides POCOs that package a IVisualStudioSolution interface together with stats such as whether it is inside or out of the root directory (i.e., the directory that the Project File Renamer strats its operations in) directory tree, etc.
    
    This module is a component of the xyLOGIX Project File Renamer software system.
    
    ---
    Copyright © 2023 by xyLOGIX, LLC.  All rights reserved.

---
+++ MFR.Params.LoadedSolutions.Factories/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MFR.Params.LoadedSolutions.Factories")]
+[assembly: AssemblyDescription("Provides POCOs that package a IVisualStudioSolution interface together with stats such as whether it is inside or out of the root directory (i.e., the directory that the Project File Renamer strats its operations in) directory tree, etc.  This particular library holds the factories for this component.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("xyLOGIX, LLC")]
+[assembly: AssemblyProduct("xyLOGIX Project File Renamer")]
+[assembly:
+    AssemblyCopyright(
+        "Copyright © 2023 by xyLOGIX, LLC.  All rights reserved.")]
+[assembly:
+    AssemblyTrademark(
+        "This software is a component of xyLOGIX Project File Renamer.  xyLOGIX Project File Renamer is a trademark of xyLOGIX, LLC.  All rights reserved, especially those granted by international treaties.")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f444861f-818e-4393-aba1-33b1e09ef8a2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
