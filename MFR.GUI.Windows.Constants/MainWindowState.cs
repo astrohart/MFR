e355f953bc32a5b06b512e61e2dee8af67b989f9
Brian Hart <brian.hart@xylogix.net>
2023-07-21 19:27:02 -0600

    Create MainWindowState.cs

---
+++ MFR.GUI.Windows.Constants/MainWindowState.cs
@@ -0,0 +1,28 @@
+﻿namespace MFR.GUI.Windows.Constants
+{
+    /// <summary>
+    /// Values that describe the state of the main application window.
+    /// </summary>
+    public enum MainWindowState
+    {
+        /// <summary>
+        /// The main application window is waiting for user input.
+        /// </summary>
+        Idle,
+
+        /// <summary>
+        /// The application is busy performing user-requested operations.
+        /// </summary>
+        PerformingOperations,
+
+        /// <summary>
+        /// The user-requested operations have finished.
+        /// </summary>
+        OperationsFinished,
+
+        /// <summary>
+        /// Unknown state.
+        /// </summary>
+        Unknown = -1
+    }
+}
