da79f0187f2d972c9f91bb7d1f0fff60418fbea4
Brian Hart <brian.hart@xylogix.net>
2023-10-18 15:08:25 -0600

    Latest updates at 15:08 hours MDT on 10/18/2023

---
+++ MFR.GUI.Dialogs.Factories/MakeNewCancellableProgressDialog.cs
@@ -0,0 +1,27 @@
+using MFR.GUI.Dialogs.Interfaces;
+using PostSharp.Patterns.Diagnostics;
+using System;
+
+namespace MFR.GUI.Dialogs.Factories
+{
+    /// <summary>
+    /// Creates new instances of objects that implement the
+    /// <see
+    ///     cref="T:MFR.GUI.Dialogs.Interfaces.ICancellableProgressDialog" />
+    /// interface, and
+    /// returns references to them.
+    /// </summary>
+    [Log(AttributeExclude = true)]
+    public static class MakeNewCancellableProgressDialog
+    {
+        /// <summary>
+        /// Creates a new instance of an object that implements the
+        /// <see
+        ///     cref="T:MFR.GUI.Dialogs.Interfaces.ICancellableProgressDialog" />
+        /// interface and
+        /// returns a reference to it.
+        /// </summary>
+        public static ICancellableProgressDialog FromScratch()
+            => new CancellableProgressDialog();
+    }
+}
