f71440d127b8db373fb44c6d5d1ac801062e9f1b
Brian Hart <brian.hart@xylogix.net>
2024-01-09 11:17:39 -0700

    Latest updates at 11:17 hours MDT on 01/09/2024

--- MFR.GUI.Windows/MainWindow.cs
+++ MFR.GUI.Windows/MainWindow.cs
@@ -85,6 +85,9 @@ namespace MFR.GUI.Windows
         /// </summary>
         private MainWindowState _state;
 
+        private bool enableFormLevelDoubleBuffering = true;
+        private int originalExStyle = -1;
+
         /// <summary>
         /// Empty, static constructor to prohibit direct allocation of this class.
         /// </summary>
@@ -145,8 +148,15 @@ namespace MFR.GUI.Windows
         protected override CreateParams CreateParams
         {
             get {
+                if (originalExStyle == -1)
+                    originalExStyle = base.CreateParams.ExStyle;
+
                 var cp = base.CreateParams;
-                cp.ExStyle |= 0x02000000; // Turn on WS_EX_COMPOSITED
+                if (enableFormLevelDoubleBuffering)
+                    cp.ExStyle |= 0x02000000; // WS_EX_COMPOSITED
+                else
+                    cp.ExStyle = originalExStyle;
+
                 return cp;
             }
         }
@@ -269,14 +279,14 @@ namespace MFR.GUI.Windows
                 {
                     if (_operationEngine == null)
                         result = _operationEngine = GetOperationEngine
-                                                    .OfType<
-                                                        IFullGuiOperationEngine>(
-                                                        OperationEngineType
-                                                            .FullGUI
-                                                    )
-                                                    .AndAttachConfiguration(
-                                                        CurrentConfiguration
-                                                    ) as
+                            .OfType<
+                                IFullGuiOperationEngine>(
+                                OperationEngineType
+                                    .FullGUI
+                            )
+                            .AndAttachConfiguration(
+                                CurrentConfiguration
+                            ) as
                             IFullGuiOperationEngine;
                 }
                 catch (Exception ex)
@@ -482,6 +492,18 @@ namespace MFR.GUI.Windows
         public void SetState(MainWindowState newState)
             => State = newState;
 
+        public void TurnOffFormLevelDoubleBuffering()
+        {
+            enableFormLevelDoubleBuffering = DoubleBuffered = false;
+            MinimizeBox = true;
+        }
+
+        public void TurnOnFormLevelDoubleBuffering()
+        {
+            enableFormLevelDoubleBuffering = DoubleBuffered = true;
+            MinimizeBox = true;
+        }
+
         /// <summary>
         /// Raises the <see cref="E:System.Windows.Forms.Form.FormClosing" />
         /// event.
@@ -495,6 +517,33 @@ namespace MFR.GUI.Windows
             base.OnFormClosing(e);
 
             SaveUserSettingsOnExit();
+        }
+
+        /// <summary>
+        /// Raises the <see cref="E:System.Windows.Forms.Form.ResizeBegin" />
+        /// event.
+        /// </summary>
+        /// <param name="e">
+        /// A <see cref="T:System.EventArgs" /> that contains the event
+        /// data.
+        /// </param>
+        protected override void OnResizeBegin(EventArgs e)
+        {
+            base.OnResizeBegin(e);
+
+            TurnOnFormLevelDoubleBuffering();
+        }
+
+        /// <summary>Raises the <see cref="E:System.Windows.Forms.Form.ResizeEnd" /> event.</summary>
+        /// <param name="e">
+        /// A <see cref="T:System.EventArgs" /> that contains the event
+        /// data.
+        /// </param>
+        protected override void OnResizeEnd(EventArgs e)
+        {
+            base.OnResizeEnd(e);
+
+            TurnOffFormLevelDoubleBuffering();
         }
 
         /// <summary>Raises the <see cref="E:System.Windows.Forms.Form.Shown" /> event.</summary>
@@ -1942,7 +1991,7 @@ namespace MFR.GUI.Windows
             }
 
             /* OKAY, scan the all the folders within the directory tree of the
-                starting folder.  If even one of the folders has a name and contained 
+                starting folder.  If even one of the folders has a name and contained
                 <c>.csproj</c> name matching the replaceWith text identically, stop,
                 and throw a warning to the user.
 
@@ -1962,8 +2011,7 @@ namespace MFR.GUI.Windows
                 ) && DialogResult.No ==
                 xyLOGIX.Win32.Interact.Messages.ConfirmWithYesNo(
                     this,
-                    Resources
-                        .Confirm_ReplaceTextThatWouldOverwriteExistingFiles
+                    Resources.Confirm_ReplaceTextThatWouldOverwriteExistingFiles
                 ))
             {
                 hiddenFocusLabel.Focus();
