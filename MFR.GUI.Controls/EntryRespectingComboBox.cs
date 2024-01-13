83c2096376b81eb180fdf88cc41ce019714d7974
Brian Hart <brian.hart@xylogix.net>
2024-01-13 13:51:48 -0700

    Update EntryRespectingComboBox.cs
    * Make it less likely combo boxes will flicker on the main window

--- MFR.GUI.Controls/EntryRespectingComboBox.cs
+++ MFR.GUI.Controls/EntryRespectingComboBox.cs
@@ -1,6 +1,5 @@
 using MFR.GUI.Controls.Interfaces;
 using PostSharp.Patterns.Diagnostics;
-using System;
 using System.Windows.Forms;
 using xyLOGIX.UI.Dark.Controls;
 
@@ -18,22 +17,14 @@ namespace MFR.GUI.Controls
     /// engineered in the hopes of making that behavior go away.
     /// </remarks>
     [Log(AttributeExclude = true)]
-    public class EntryRespectingComboBox : DarkComboBox, IEntryRespectingComboBox
+    public class EntryRespectingComboBox : DarkComboBox,
+        IEntryRespectingComboBox
     {
         /// <summary>
         /// String containing the text most recently typed into the editing
         /// field of the ComboBox by the user.
         /// </summary>
         private string _enteredText;
-
-        /// <summary>Raises the <see cref="E:System.Windows.Forms.Control.TextChanged" /> event.</summary>
-        /// <param name="e">An <see cref="T:System.EventArgs" /> that contains the event data.</param>
-        protected override void OnTextChanged(EventArgs e)
-        {
-            base.OnTextChanged(e);
-
-            _enteredText = Text;
-        }
 
         /// <summary>
         /// Gets the text, verbatim, that was typed in the Combo Box's editing
@@ -60,68 +51,6 @@ namespace MFR.GUI.Controls
                     ? Text
                     : _enteredText;
             set => Text = _enteredText = value;
-        }
-
-        /// <summary>
-        /// Raises the <see cref="E:System.Windows.Forms.Control.Leave" /> event.
-        /// </summary>
-        /// <param name="e">
-        /// An <see cref="T:System.EventArgs" /> that contains the event data.
-        /// </param>
-        /// <remarks>
-        /// This method responds by saving off any text directly typed by the
-        /// user into the
-        /// <see
-        ///     cref="P:MFR.GUI.Controls.EntryRespectingComboBox.EnteredText" />
-        /// property and adding that text to the list.
-        /// </remarks>
-        protected override void OnLeave(EventArgs e)
-        {
-            base.OnLeave(e);
-
-            //EnteredText = Text; // save off the value of the Text property.
-
-            if (DropDownStyle != ComboBoxStyle.DropDown ||
-                Items.Contains(EnteredText)) return;
-            Items.Insert(
-                0, /* list goes by reverse chronological order of entry */
-                EnteredText
-            ); // add the text typed by the user to the list of items
-
-            //ClearSelection(); // remove the highlight
-        }
-
-        /// <summary>
-        /// Raises the
-        /// <see
-        ///     cref="E:System.Windows.Forms.ComboBox.SelectedIndexChanged" />
-        /// event.
-        /// </summary>
-        /// <param name="e">
-        /// An <see cref="T:System.EventArgs" /> that contains the event data.
-        /// </param>
-        /// <remarks>
-        /// This method responds by removing the highlight that persists in the
-        /// edit portion of the control after the selection has been changed.
-        /// </remarks>
-        protected override void OnSelectedIndexChanged(EventArgs e)
-        {
-            base.OnSelectedIndexChanged(e);
-
-            _enteredText = Text;
-
-            ClearSelection();
-        }
-
-        /// <summary>
-        /// Called to remove the selection highlight that persists on the text
-        /// in the edit portion of the control even after the selection is
-        /// changed or input focus leaves the control.
-        /// </summary>
-        private void ClearSelection()
-        {
-            SelectionStart = Text.Length;
-            SelectionLength = 0;
         }
     }
 }
