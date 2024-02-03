df42f36f6adcb7b9f1b3cf9a2092efc5bf440c68
Brian Hart <brian.hart@xylogix.net>
2024-02-03 15:27:53 -0700

    Update ComboBoxInitializer.cs

--- MFR.GUI.Initializers/ComboBoxInitializer.cs
+++ MFR.GUI.Initializers/ComboBoxInitializer.cs
@@ -14,6 +14,20 @@ namespace MFR.GUI.Initializers
     public static class ComboBoxInitializer
     {
         /// <summary>
+        /// Initializes static data or performs actions that need to be performed once only
+        /// for the <see cref="T:MFR.GUI.Initializers.ComboBoxInitializer" /> class.
+        /// </summary>
+        /// <remarks>
+        /// This constructor is called automatically prior to the first instance being
+        /// created or before any static members are referenced.
+        /// <para />
+        /// We've decorated this constructor with the <c>[Log(AttributeExclude = true)]</c>
+        /// attribute in order to simplify the logging output.
+        /// </remarks>
+        [Log(AttributeExclude = true)]
+        static ComboBoxInitializer() { }
+
+        /// <summary>
         /// Initializes a <see cref="T:System.Windows.Forms.ComboBox" /> to have
         /// the specified <paramref name="itemList" /> as the items in its
         /// drop-down, and <paramref name="currentItem" /> as the
@@ -81,7 +95,8 @@ namespace MFR.GUI.Initializers
                 foreach (var item in itemList)
                     comboBox.Items.Add(item);
 
-                comboBox.SelectedIndex = 0;
+                if (string.IsNullOrWhiteSpace(currentItem))
+                    comboBox.SelectedIndex = 0;
 
                 if (!string.IsNullOrWhiteSpace(currentItem) && !currentItem
                         .Trim()
