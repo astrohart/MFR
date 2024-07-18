305a6e0efcb6fe5137310b825769f686f2d1e5bb
Brian Hart <brian.hart@xylogix.net>
2024-07-18 12:40:33 -0600

    Create ErrantProcessInfo.cs
    * This object encapsulates the pathname of a .exe file that represents one of the errant process(es) that are to be forcibly terminated before either of the Rename Files in Folder or Rename Sub Folders operations are to be processed.

---
+++ MFR.GUI.Models/ErrantProcessInfo.cs
@@ -0,0 +1,61 @@
+﻿using System.IO;
+using xyLOGIX.Files.Actions;
+
+namespace MFR.GUI.Models
+{
+    /// <summary>
+    /// Encapsulates data about an errant process that is to be forcibly terminated
+    /// prior to the processing of either of the <c>Rename Files In Folder</c> or
+    /// <c>Rename Sub Folder(s)</c> operations.
+    /// </summary>
+    public class ErrantProcessInfo
+    {
+        /// <summary>
+        /// Constructs a new instance of <see cref="T:MFR.GUI.Models.ErrantProcessInfo" />
+        /// and returns a reference to it.
+        /// </summary>
+        /// <param name="pathname">
+        /// (Required.) A <see cref="T:System.String" /> that contains the fully-qualified
+        /// pathname of a <c>.exe</c> file representing a process that is to be forcibly
+        /// terminated.
+        /// </param>
+        public ErrantProcessInfo(string pathname)
+        {
+            Pathname = pathname;
+        }
+
+        /// <summary>
+        /// Gets a <see cref="T:System.String" /> containing just the filename only, of the
+        /// <c>.exe</c> file representing a process that is to be forcibly terminated.
+        /// </summary>
+        public string FileName
+        {
+            get {
+                var result = string.Empty;
+
+                try
+                {
+                    if (!Does.FileExist(Pathname)) return result;
+
+                    result = Path.GetFileName(Pathname);
+                }
+                catch
+                {
+                    result = string.Empty;
+                }
+
+                return result;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets a <see cref="T:System.String" /> that contains the fully-qualified
+        /// pathname of a <c>.exe</c> file that is to be forcibly terminated.
+        /// </summary>
+        public string Pathname
+        {
+            get;
+            set;
+        }
+    }
+}
