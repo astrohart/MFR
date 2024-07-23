92d05047555d912464e1569aeb7d92ac749f1dbf
Brian Hart <brian.hart@xylogix.net>
2024-07-23 13:36:11 -0600

    Update FileSystemEntryValidator.cs
    * Added the IsValid() method.

--- MFR.FileSystem.Validators/FileSystemEntryValidator.cs
+++ MFR.FileSystem.Validators/FileSystemEntryValidator.cs
@@ -1,6 +1,8 @@
 using MFR.FileSystem.Interfaces;
 using PostSharp.Patterns.Diagnostics;
 using System;
+using xyLOGIX.Core.Debug;
+using xyLOGIX.Files.Actions;
 
 namespace MFR.FileSystem.Validators
 {
@@ -12,6 +14,42 @@ namespace MFR.FileSystem.Validators
     [Log(AttributeExclude = true)]
     public static class FileSystemEntryValidator
     {
+        /// <summary>
+        /// Determines whether the specified file system <paramref name="entry" /> object
+        /// is valid or not.
+        /// </summary>
+        /// <param name="entry">
+        /// (Required.) Reference to an instance of an object that implements the
+        /// <see cref="T:MFR.FileSystem.Interfaces.IFileSystemEntry" /> interface.
+        /// </param>
+        /// <remarks>
+        /// To count as valid, the specified <paramref name="entry" /> must not be
+        /// set to a <see langword="null" /> reference, and it must refer to a file or
+        /// folder that actually exists on the file system.
+        /// </remarks>
+        /// <returns>
+        /// <see langword="true" /> if the specified file-system
+        /// <paramref name="entry" /> is valid; <see langword="false" /> otherwise.
+        /// </returns>
+        public static bool IsValid(IFileSystemEntry entry)
+        {
+            bool result;
+
+            try
+            {
+                result = entry != null && Does.FileSystemEntryExist(entry.Path);
+            }
+            catch (Exception ex)
+            {
+                // dump all the exception info to the log
+                DebugUtils.LogException(ex);
+
+                result = false;
+            }
+
+            return result;
+        }
+
         /// <summary>
         /// Validates the <paramref name="entry" /> specified.
         /// </summary>
