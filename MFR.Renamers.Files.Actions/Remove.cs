c68469399cab33b666808d2b69b2f0eae3853438
Brian Hart <brian.hart@xylogix.net>
2024-10-11 11:39:36 -0600

    Update Remove.cs

--- MFR.Renamers.Files.Actions/Remove.cs
+++ MFR.Renamers.Files.Actions/Remove.cs
@@ -1,9 +1,12 @@
 using PostSharp.Patterns.Diagnostics;
+using System;
+using xyLOGIX.Core.Debug;
+using xyLOGIX.Interop.Processes.Actions;
 
 namespace MFR.Renamers.Files.Actions
 {
     /// <summary>
-    /// TODO: Add class XML documentation here
+    /// Exposes static methods to remove data from data sources or the file system.
     /// </summary>
     public static class Remove
     {
@@ -19,8 +22,29 @@ namespace MFR.Renamers.Files.Actions
         /// attribute in order to simplify the logging output.
         /// </remarks>
         [Log(AttributeExclude = true)]
-        static Remove()
+        static Remove() { }
+
+        /// <summary>
+        /// Attempts to remove the directory having the specified
+        /// <paramref name="pathname" />, and all of its contents, from the file system.
+        /// </summary>
+        /// <param name="pathname">
+        /// (Required.) A <see cref="T:System.String" /> containing
+        /// the fully-qualified pathname of the directory that is to be removed.
+        /// </param>
+        public static void Directory(string pathname)
         {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(pathname)) return;
+
+                Run.SystemCommand($"RD /S /Q \"{pathname}\"", "", true);
+            }
+            catch (Exception ex)
+            {
+                // dump all the exception info to the log
+                DebugUtils.LogException(ex);
+            }
         }
     }
 }
