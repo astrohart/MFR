c1a7cb3f51e7004cf7f6cdeec5e869e0c4d5a864
Brian Hart <brian.hart@xylogix.net>
2024-10-11 11:34:47 -0600

    Update Stop.cs
    * Created the 'Stop.Service' method

--- MFR.Renamers.Files.Actions/Stop.cs
+++ MFR.Renamers.Files.Actions/Stop.cs
@@ -1,9 +1,13 @@
 using PostSharp.Patterns.Diagnostics;
+using System;
+using System.Threading;
+using xyLOGIX.Core.Debug;
+using xyLOGIX.Interop.Processes.Actions;
 
 namespace MFR.Renamers.Files.Actions
 {
     /// <summary>
-    /// TODO: Add class XML documentation here
+    /// Exposes static methods to stop processes and services from the command line.
     /// </summary>
     public static class Stop
     {
@@ -19,8 +23,38 @@ namespace MFR.Renamers.Files.Actions
         /// attribute in order to simplify the logging output.
         /// </remarks>
         [Log(AttributeExclude = true)]
-        static Stop()
+        static Stop() { }
+
+        /// <summary>
+        /// Stops a Windows Service by using the command line <c>sc stop</c> command.
+        /// </summary>
+        /// <param name="serviceName">
+        /// (Required.) A <see cref="T:System.String" /> containing the name of the service
+        /// (must have no spaces).
+        /// </param>
+        /// <remarks>
+        /// If a blank value is passed as the argument of the
+        /// <paramref name="serviceName" /> parameter, then this method does nothing.
+        /// </remarks>
+        public static void Service(string serviceName)
         {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(serviceName)) return;
+                if (serviceName.Contains(" "))
+                    return; // service name can't have spaces
+
+                Run.SystemCommand($"sc stop {serviceName}", "", true);
+
+                Thread.Sleep(
+                    10000
+                ); // sleep for 10 seconds to allow the action to be processed.
+            }
+            catch (Exception ex)
+            {
+                // dump all the exception info to the log
+                DebugUtils.LogException(ex);
+            }
         }
     }
 }
