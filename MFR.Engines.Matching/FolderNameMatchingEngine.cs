a5b9e29dd796865618015f5b11e19c7deff9904d
Brian Hart <brian.hart@xylogix.net>
2025-02-16 20:00:39 -0700

    Update FolderNameMatchingEngine.cs
    * Modified string matcher retrieval method.
      - Changed variable name from `CurrentConfiguration` to `CurrentConfig` for consistency and clarity.

--- MFR.Engines.Matching/FolderNameMatchingEngine.cs
+++ MFR.Engines.Matching/FolderNameMatchingEngine.cs
@@ -107,7 +107,7 @@ namespace MFR.Engines.Matching
                     return result;
 
                 var matcher = Get.StringMatcherForOperation(
-                    OperationType, CurrentConfiguration
+                    OperationType, CurrentConfig
                 );
                 if (matcher == null) return result;
 
