24915899adba2a7954ccb5351555400650339d0d
Brian Hart <brian.hart@xylogix.net>
2025-02-19 14:36:03 -0700

    Update Program.cs
    * Remove automatic debugger start in Main method
    - Eliminates the call to `ProgramFlowHelper.StartDebugger();`
    - This change may impact debugging behavior during development or testing.

--- MFR.GUI/Program.cs
+++ MFR.GUI/Program.cs
@@ -78,8 +78,6 @@ namespace MFR.GUI
         [STAThread]
         public static void Main([NotLogged] string[] args)
         {
-            ProgramFlowHelper.StartDebugger();
-
             SetUpLogging(); // has to be called here for the log file to be stored in the proper location.
 
             if (!Register.WindowsMessageFilter())
