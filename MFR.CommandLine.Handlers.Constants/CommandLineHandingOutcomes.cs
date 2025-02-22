c5b7c79585fa654922bf8325f7234eb9864e8a3d
Brian Hart <brian.hart@xylogix.net>
2025-02-22 10:22:51 -0700

    Update CommandLineHandingOutcomes.cs
    * Added a new file `CommandLineHandingOutcomes.cs`
      - Defines an enumeration `CommandLineHandingOutcomes`
      - Enum includes options: `DoNothing`, `FailFatalExit`, `RunCodeLocatorWizard`
      - Default value `Unknown` set to -1
    * Updated project file `MFR.CommandLine.Handlers.Constants.csproj`
      - Included `CommandLineHandingOutcomes.cs` in the compilation process

---
+++ MFR.CommandLine.Handlers.Constants/CommandLineHandingOutcomes.cs
@@ -0,0 +1,28 @@
+﻿namespace MFR.CommandLine.Handlers.Constants
+{
+    /// <summary>
+    /// Possible outcomes of handling invalid command-line arguments.
+    /// </summary>
+    public enum CommandLineHandingOutcomes
+    {
+        /// <summary>
+        /// Do nothing but allow the application to continue loading.
+        /// </summary>
+        DoNothing,
+
+        /// <summary>
+        /// Display an error message to the user stating that the application cannot
+        /// continue loading due to an unrecoverable error, and then terminate the
+        /// execution of the application.
+        /// </summary>
+        FailFatalExit,
+
+        /// <summary>
+        /// Launch the <b>Code Locator Wizard</b> to guide the user through the process of
+        /// determining what code base is to be acted upon.
+        /// </summary>
+        RunCodeLocatorWizard,
+
+        Unknown = -1
+    }
+}
