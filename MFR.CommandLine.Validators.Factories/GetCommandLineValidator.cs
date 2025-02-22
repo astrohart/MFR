9663d2eb410a613b00af6cd8c335189a0b8bcfcd
Brian Hart <brian.hart@xylogix.net>
2025-02-22 10:41:17 -0700

    Update GetCommandLineValidator.cs
    * Updated license information for clarity and consistency.
    * Added a static constructor to `GetCommandLineValidator`:
      - Decorated with `[Log(AttributeExclude = true)]` for simplified logging.
      - Initializes static data or performs one-time actions.
    * Modified `SoleInstance` method:
      - Added `[DebuggerStepThrough]` attribute for improved debugging.
      - Simplified implementation by removing root directory path association, returning only `CommandLineValidator.Instance`.

--- MFR.CommandLine.Validators.Factories/GetCommandLineValidator.cs
+++ MFR.CommandLine.Validators.Factories/GetCommandLineValidator.cs
@@ -1,44 +1,44 @@
 /*71b451a5-ec2a-4538-a74d-a520fd3c98ef
  * # xyLOGIX Project File Renamer License
- * 
- * This software and associated documentation files (the "Software") are the exclusive property of xyLOGIX, LLC ("xyLOGIX"), a Tennessee Limited Liability Company. Copyright © 2025 by 
+ *
+ * This software and associated documentation files (the "Software") are the exclusive property of xyLOGIX, LLC ("xyLOGIX"), a Tennessee Limited Liability Company. Copyright © 2025 by
  * xyLOGIX, LLC.  All rights reserved.
- * 
+ *
  * ## License Grant
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this Software, to use the Software solely for the purpose of evaluating its functionality and performance.
  * This license does not grant the right to modify, distribute, sublicense, or sell copies of the Software, nor to use the Software for any commercial purpose.
- * 
+ *
  * ## Restrictions
- * 
+ *
  * 1.  No Distribution: You may not distribute, sublicense, or sell copies of the Software, in whole or in part, to any third party.
  * 2.  No Modification:  You may not modify, adapt, or create derivative works of the Software.
  * 3.  No Reverse Engineering:  You may not reverse engineer, decompile, or disassemble the Software.
  * 4.  No Commercial Use:  You may not use the Software for any commercial purpose without the prior written consent of xyLOGIX.
  * 5.  No Removal of Notices:  You may not remove or alter any proprietary notices, labels, or marks on the Software.
- * 
+ *
  * ## Intellectual Property Rights
- * 
+ *
  * All intellectual property rights, including but not limited to copyrights, patents, trademarks, and trade secrets, in and to the Software are and shall remain the exclusive property of xyLOGIX.
  * This license does not transfer any ownership rights in the Software to you.
- * 
+ *
  * ## Disclaimer of Warranty
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND NON-INFRINGEMENT. 
- * IN NO EVENT SHALL XYLOGIX BE LIABLE FOR ANY CLAIM, DAMAGES, OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT, OR OTHERWISE, ARISING FROM, OUT OF, OR IN CONNECTION WITH THE SOFTWARE OR THE USE 
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND NON-INFRINGEMENT.
+ * IN NO EVENT SHALL XYLOGIX BE LIABLE FOR ANY CLAIM, DAMAGES, OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT, OR OTHERWISE, ARISING FROM, OUT OF, OR IN CONNECTION WITH THE SOFTWARE OR THE USE
  * OR OTHER DEALINGS IN THE SOFTWARE.
- * 
+ *
  * ## Limitation of Liability
- * 
+ *
  * IN NO EVENT SHALL XYLOGIX BE LIABLE FOR ANY INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
  * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THE SOFTWARE
  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
+ *
  * ## Termination
- * 
+ *
  * This license is effective until terminated.  Your rights under this license will terminate automatically without notice from xyLOGIX if you fail to comply with any term(s) of this license. Upon termination, you must
  * cease all use of the Software and destroy all copies, full or partial, of the Software.
- * 
+ *
  * ## Governing Law
  *
  * This license shall be governed by and construed in accordance with the laws of the State of Washington, without regard to its conflict of laws principles and without regard to the principle of contra proferentem.
@@ -52,8 +52,8 @@
  */
 
 using MFR.CommandLine.Validators.Interfaces;
-using MFR.Directories.Validators.Factories;
 using PostSharp.Patterns.Diagnostics;
+using System.Diagnostics;
 
 namespace MFR.CommandLine.Validators.Factories
 {
@@ -63,9 +63,24 @@ namespace MFR.CommandLine.Validators.Factories
     ///     cref="T:MFR.CommandLine.Validators.Interfaces.ICommandLineValidator" />
     /// interface.
     /// </summary>
-    [Log(AttributeExclude = true)]
     public static class GetCommandLineValidator
     {
+        /// <summary>
+        /// Initializes static data or performs actions that need to be performed once only
+        /// for the
+        /// <see cref="T:MFR.CommandLine.Validators.Factories.GetCommandLineValidator" />
+        /// class.
+        /// </summary>
+        /// <remarks>
+        /// This constructor is called automatically prior to the first instance being
+        /// created or before any static members are referenced.
+        /// <para />
+        /// We've decorated this constructor with the <c>[Log(AttributeExclude = true)]</c>
+        /// attribute in order to simplify the logging output.
+        /// </remarks>
+        [Log(AttributeExclude = true)]
+        static GetCommandLineValidator() { }
+
         /// <summary>
         /// Provides access to the sole instance of the object that implements
         /// the
@@ -82,10 +97,8 @@ namespace MFR.CommandLine.Validators.Factories
         /// <exception cref="T:System.ArgumentOutOfRangeException">
         /// Thrown if the object type requested is not supported.
         /// </exception>
+        [DebuggerStepThrough]
         public static ICommandLineValidator SoleInstance()
-            => CommandLineValidator.Instance
-                                   .AssociateWithRootDirectoryPathValidator(
-                                       GetRootDirectoryPathValidator.SoleInstance()
-                                   );
+            => CommandLineValidator.Instance;
     }
 }
