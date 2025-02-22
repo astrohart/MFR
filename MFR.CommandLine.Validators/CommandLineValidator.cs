706094220f9ab92dd1a05db9da215ab88c03c124
Brian Hart <brian.hart@xylogix.net>
2025-02-22 10:41:09 -0700

    Update CommandLineValidator.cs
    * Added a static property `RootDirectoryPathValidator`
      - Provides a reference to an instance of `IRootDirectoryPathValidator`
      - Replaces the previous instance variable `_rootDirectoryPathValidator`
    
    * Modified the `AssociateWithRootDirectoryPathValidator` method
      - Removed the need for an instance variable
    
    * Renamed the `Validate` method to `IsValid`
      - Updated its implementation to use the new static property
    
    * Updated comments throughout the file for clarity
    
    * Added a new project reference to `MFR.Directories.Validators.Factories` in `MFR.CommandLine.Validators.csproj`
      - Indicates a new dependency for the project

--- MFR.CommandLine.Validators/CommandLineValidator.cs
+++ MFR.CommandLine.Validators/CommandLineValidator.cs
@@ -1,44 +1,44 @@
 /*71b451a5-ec2a-4538-a74d-a520fd3c98ef
  * # xyLOGIX Project File Renamer License
- * 
- * This software and associated documentation files (the "Software") are the exclusive property of xyLOGIX, LLC ("xyLOGIX"), a Tennessee Limited Liability Company. Copyright © 2025 by 
+ *
+ * This software and associated documentation files (the "Software") are the exclusive property of xyLOGIX, LLC ("xyLOGIX"), a Tennessee Limited Liability Company. Copyright © 2025 by
  * xyLOGIX, LLC.  All rights reserved.
- * 
+ *
  * ## License Grant
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this Software, to use the Software solely for the purpose of evaluating its functionality and performance.
  * This license does not grant the right to modify, distribute, sublicense, or sell copies of the Software, nor to use the Software for any commercial purpose.
- * 
+ *
  * ## Restrictions
- * 
+ *
  * 1.  No Distribution: You may not distribute, sublicense, or sell copies of the Software, in whole or in part, to any third party.
  * 2.  No Modification:  You may not modify, adapt, or create derivative works of the Software.
  * 3.  No Reverse Engineering:  You may not reverse engineer, decompile, or disassemble the Software.
  * 4.  No Commercial Use:  You may not use the Software for any commercial purpose without the prior written consent of xyLOGIX.
  * 5.  No Removal of Notices:  You may not remove or alter any proprietary notices, labels, or marks on the Software.
- * 
+ *
  * ## Intellectual Property Rights
- * 
+ *
  * All intellectual property rights, including but not limited to copyrights, patents, trademarks, and trade secrets, in and to the Software are and shall remain the exclusive property of xyLOGIX.
  * This license does not transfer any ownership rights in the Software to you.
- * 
+ *
  * ## Disclaimer of Warranty
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND NON-INFRINGEMENT. 
- * IN NO EVENT SHALL XYLOGIX BE LIABLE FOR ANY CLAIM, DAMAGES, OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT, OR OTHERWISE, ARISING FROM, OUT OF, OR IN CONNECTION WITH THE SOFTWARE OR THE USE 
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND NON-INFRINGEMENT.
+ * IN NO EVENT SHALL XYLOGIX BE LIABLE FOR ANY CLAIM, DAMAGES, OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT, OR OTHERWISE, ARISING FROM, OUT OF, OR IN CONNECTION WITH THE SOFTWARE OR THE USE
  * OR OTHER DEALINGS IN THE SOFTWARE.
- * 
+ *
  * ## Limitation of Liability
- * 
+ *
  * IN NO EVENT SHALL XYLOGIX BE LIABLE FOR ANY INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
  * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THE SOFTWARE
  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
+ *
  * ## Termination
- * 
+ *
  * This license is effective until terminated.  Your rights under this license will terminate automatically without notice from xyLOGIX if you fail to comply with any term(s) of this license. Upon termination, you must
  * cease all use of the Software and destroy all copies, full or partial, of the Software.
- * 
+ *
  * ## Governing Law
  *
  * This license shall be governed by and construed in accordance with the laws of the State of Washington, without regard to its conflict of laws principles and without regard to the principle of contra proferentem.
@@ -55,10 +55,12 @@ using MFR.CommandLine.Models.Interfaces;
 using MFR.CommandLine.Validators.Constants;
 using MFR.CommandLine.Validators.Events;
 using MFR.CommandLine.Validators.Interfaces;
+using MFR.Directories.Validators.Factories;
 using MFR.Directories.Validators.Interfaces;
 using PostSharp.Patterns.Diagnostics;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using xyLOGIX.Core.Debug;
 
@@ -70,14 +72,6 @@ namespace MFR.CommandLine.Validators
     /// </summary>
     public class CommandLineValidator : ICommandLineValidator
     {
-        /// <summary>
-        /// Reference to an instance of an object that implements the
-        /// <see
-        ///     cref="T:MFR.Directories.Validators.Interfaces.IRootDirectoryPathValidator" />
-        /// interface.
-        /// </summary>
-        private IRootDirectoryPathValidator _rootDirectoryPathValidator;
-
         /// <summary>
         /// Empty, static constructor to prohibit direct allocation of this class.
         /// </summary>
@@ -110,6 +104,16 @@ namespace MFR.CommandLine.Validators
         } = new CommandLineValidator();
 
         /// <summary>
+        /// Gets a reference to an instance of an object that implements the
+        /// <see cref="T:MFR.Directories.Validators.Interfaces.IRootDirectoryPathValidator" />
+        /// interface.
+        /// </summary>
+        private static IRootDirectoryPathValidator RootDirectoryPathValidator
+        {
+            [DebuggerStepThrough] get;
+        } = GetRootDirectoryPathValidator.SoleInstance();
+
+        /// <summary>
         /// Gets a count of validation failures that occurred the last time the
         /// <see
         ///     cref="M:MFR.CommandLine.Validators.Interfaces.ICommandLineValidator.IsValid" />
@@ -133,41 +137,6 @@ namespace MFR.CommandLine.Validators
         /// Occurs once for each validation failure.
         /// </summary>
         public event CommandLineInfoInvalidEventHandler CommandLineInfoInvalid;
-
-        /// <summary>
-        /// Associates an instance of an object that implements the
-        /// <see
-        ///     cref="T:MFR.Directories.Validators.Interfaces.IRootDirectoryPathValidator" />
-        /// interface with this validator object.
-        /// </summary>
-        /// <param name="rootDirectoryPathValidator">
-        /// (Required.) Reference to an instance of an object that implements
-        /// the
-        /// <see
-        ///     cref="T:MFR.Directories.Validators.Interfaces.IRootDirectoryPathValidator" />
-        /// interface.
-        /// </param>
-        /// <returns>
-        /// Reference to the same instance of the object that called this
-        /// method, for fluent use.
-        /// </returns>
-        /// <exception cref="T:System.ArgumentNullException">
-        /// Thrown if the required parameter,
-        /// <paramref
-        ///     name="rootDirectoryPathValidator" />
-        /// , is passed a <see langword="null" /> value.
-        /// </exception>
-        public ICommandLineValidator AssociateWithRootDirectoryPathValidator(
-            IRootDirectoryPathValidator rootDirectoryPathValidator
-        )
-        {
-            _rootDirectoryPathValidator = rootDirectoryPathValidator ??
-                                          throw new ArgumentNullException(
-                                              nameof(rootDirectoryPathValidator)
-                                          );
-
-            return this;
-        }
 
         /// <summary>
         /// Gets a value indicating whether the specified
@@ -205,7 +174,7 @@ namespace MFR.CommandLine.Validators
         ///     cref="P:MFR.CommandLine.Validators.Interfaces.IValidator.ValidationFailures" />
         /// property's value is set to zero.
         /// </remarks>
-        public bool Validate(ICommandLineInfo cmdInfo)
+        public bool IsValid([NotLogged] ICommandLineInfo cmdInfo)
         {
             var result = false;
 
@@ -215,12 +184,12 @@ namespace MFR.CommandLine.Validators
             {
                 DebugUtils.WriteLine(
                     DebugLevel.Info,
-                    "CommandLineValidator.Validate: Attempting to validate the values passed by the user on the command line..."
+                    "CommandLineValidator.IsValid: Attempting to validate the values passed by the user on the command line..."
                 );
 
                 DebugUtils.WriteLine(
                     DebugLevel.Info,
-                    "CommandLineValidator.Validate: Checking whether the 'cmdInfo' method parameter has a null reference for a value..."
+                    "CommandLineValidator.IsValid: Checking whether the 'cmdInfo' method parameter has a null reference for a value..."
                 );
 
                 // Check to see if the required parameter, cmdInfo, is null. If it is, send an
@@ -231,7 +200,7 @@ namespace MFR.CommandLine.Validators
                     // the parameter cmdInfo is required.
                     DebugUtils.WriteLine(
                         DebugLevel.Error,
-                        "CommandLineValidator.Validate: *** ERROR *** A null reference was passed for the 'cmdInfo' method parameter.  Stopping."
+                        "CommandLineValidator.IsValid: *** ERROR *** A null reference was passed for the 'cmdInfo' method parameter.  Stopping."
                     );
 
                     OnCommandLineInfoInvalid(
@@ -246,18 +215,18 @@ namespace MFR.CommandLine.Validators
 
                 DebugUtils.WriteLine(
                     DebugLevel.Info,
-                    "CommandLineValidator.Validate: *** SUCCESS *** We have been passed a valid object reference for the 'cmdInfo' method parameter."
+                    "CommandLineValidator.IsValid: *** SUCCESS *** We have been passed a valid object reference for the 'cmdInfo' method parameter."
                 );
 
                 // Dump the value of the property, cmdInfo.StartingFolder, to the log
                 DebugUtils.WriteLine(
                     DebugLevel.Debug,
-                    $"CommandLineValidator.Validate: cmdInfo.StartingFolder = '{cmdInfo.StartingFolder}'"
+                    $"CommandLineValidator.IsValid: cmdInfo.StartingFolder = '{cmdInfo.StartingFolder}'"
                 );
 
                 DebugUtils.WriteLine(
                     DebugLevel.Info,
-                    "CommandLineValidator.Validate: Validating the starting folder that we were passed..."
+                    "CommandLineValidator.IsValid: Validating the starting folder that we were passed..."
                 );
 
                 /*
@@ -267,13 +236,13 @@ namespace MFR.CommandLine.Validators
                  * Therefore, if the validation fails here, we simply increment
                  * the count of validation failures but otherwise do nothing.
                  */
-                if (!_rootDirectoryPathValidator?.Validate(
+                if (!RootDirectoryPathValidator?.IsValid(
                         cmdInfo.StartingFolder
                     ) ?? false)
                 {
                     ValidationFailures +=
-                        _rootDirectoryPathValidator?.ValidationFailures ?? 1;
-                    Errors = Errors.Concat(_rootDirectoryPathValidator.Errors)
+                        RootDirectoryPathValidator?.ValidationFailures ?? 1;
+                    Errors = Errors.Concat(RootDirectoryPathValidator.Errors)
                                    .ToList();
                 }
 
@@ -297,7 +266,7 @@ namespace MFR.CommandLine.Validators
 
             DebugUtils.WriteLine(
                 DebugLevel.Debug,
-                $"CommandLineValidator.Validate: Result = {result}"
+                $"CommandLineValidator.IsValid: Result = {result}"
             );
 
             return result;
